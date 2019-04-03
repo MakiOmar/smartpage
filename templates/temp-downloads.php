26ac1ecc272227e6a34cbd4660ddcdd4ceb253c1
makiomar <prosentra@gmail.com>
2019-04-03 13:19:53 +0300

    renamed

---
+++ templates/temp-downloads.php
@@ -0,0 +1,18 @@
+<?php
+	$tcp= new Smpg_Generate_Posts_View(
+						array('post_type' => 'smpg_download','posts_per_page'=>5),
+						'download-loop',
+						true
+					);
+
+	$tcp->sectionWrapperOpen ='
+	<div class="section">
+		<div><h4 class="section_title clearfix">'.esc_html__('Suggested downloads','smartpage').'</h4></div>
+			<div class="posts-wrapper">
+				<div id="download">';
+
+	$tcp->sectionWrapperClose ='</div></div></div>';
+
+	$tcp->postsView();
+
+?>
