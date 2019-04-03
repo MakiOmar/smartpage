a2e25c895ecbe4ef3cf2819b7303a40d859d6922
makiomar <prosentra@gmail.com>
2019-04-03 11:05:50 +0300

    Get posts view by class

--- templates/temp-category-posts-masonary.php
+++ templates/temp-category-posts-masonary.php
@@ -1,20 +1,22 @@
-<?php 
-	$args = array('post_type'=>'post');
-	$p = new WP_Query($args);
-	if ( $p->have_posts() ) {
-		if(is_front_page() or is_home()){?>
-			<div class="section"><div><h4 class="section_title clearfix"><?php _e('Recent Posts','smartpage');?></h4></div><div id="masonary">
-		<?php }else{?>
-		<div><div id="masonary">
-		<?php }
-		while ($p->have_posts() ) {
-			$p->the_post();
-			get_template_part('templates/temp','blog-post') ;
-		}
-		wp_reset_postdata();
-		?>
-   </div>
-	   </div>
-<?php  }
+<?php
+	$tcp= new Smpg_Generate_Posts_View(
+			array('post_type'=>'post'),
+			'blog-post',
+			true
+		);
+	if(is_front_page() or is_home()){
+		$tcp->sectionWrapperOpen = 
+		'<div class="section">
+			<div>
+				<h4 class="section_title clearfix">'.esc_html__('Recent Posts','smartpage').'
+				</h4>
+		</div><div id="masonary">';
+	}else{
+		$tcp->sectionWrapperOpen ='<div><div id="masonary">';
+	}
 
+	$tcp->sectionWrapperClose ='</div></div>';
+
+	$tcp->postsView();
+						
 ?>
--- templates/temp-category-posts.php
+++ templates/temp-category-posts.php
@@ -1,19 +1,60 @@
 <?php 
-	$args = array('post_type'=>'post');
-	$p = new WP_Query($args);
-	if ( $p->have_posts() ) {
-		if(is_front_page() or is_home()){?>
-			<div class="section"><div><h4 class="section_title clearfix"><?php _e('Recent Posts','smartpage');?></h4></div>
-		<?php }else{?>
-		<div>
-		<?php }
-		while ($p->have_posts() ) {
-			$p->the_post();
-			get_template_part('templates/temp','blog-post') ;
-		}
-		wp_reset_postdata();
-		?>
-	   </div>
-<?php  }
+/*
+	$args = array();
 
+	$p = new WP_Query($args);
+
+	if ( $p->have_posts() ) {
+		
+		if(is_front_page() or is_home()){?>
+		
+			<div class="section">
+				<div>
+					<h4 class="section_title clearfix">
+						<?php esc_html_e('Recent Posts','smartpage');?>
+					</h4>
+				</div>
+			
+		<?php }else{?>
+		
+		<div>
+		
+			<?php }
+
+			while ($p->have_posts() ) {
+
+				$p->the_post();
+
+				get_template_part('templates/temp','blog-post') ;
+
+			}
+
+			wp_reset_postdata();?>
+			
+	   </div>
+	   
+<?php  }*/?>
+
+
+<?php
+		$tcp= new Smpg_Generate_Posts_View(
+				array('post_type'=>'post'),
+				'blog-post',
+				true
+			);
+		if(is_front_page() or is_home()){
+			$tcp->sectionWrapperOpen = 
+			'<div class="section">
+				<div>
+					<h4 class="section_title clearfix">'.esc_html__('Recent Posts','smartpage').'
+					</h4>
+			</div>';
+		}else{
+			$tcp->sectionWrapperOpen ='<div>';
+		}
+		
+		$tcp->sectionWrapperClose ='</div>';
+
+		$tcp->postsView();
+						
 ?>
