763edf6f6970974f8273212ce6133fb872c4d10a
makiomar <prosentra@gmail.com>
2019-04-03 11:05:09 +0300

    add helper class

--- functions/helper/php-helpers.php
+++ functions/helper/php-helpers.php
@@ -197,3 +197,21 @@ function hfx_calculate_transient($time, $time_type){
 	return $period;
 }
 
+/*
+*Checks if a url exists
+*/
+function is_url_exist($url){
+    $ch = curl_init($url);    
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if($code == 200){
+       $status = true;
+    }else{
+      $status = false;
+    }
+    curl_close($ch);
+   return $status;
+}
+
