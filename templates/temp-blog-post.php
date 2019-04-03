0765456b22329917057814e48d00652b50711b94
makiomar <prosentra@gmail.com>
2019-04-03 10:59:15 +0300

    replace a function with variable

--- templates/temp-blog-post.php
+++ templates/temp-blog-post.php
@@ -1,10 +1,15 @@
-<?php setPostViews(get_the_ID());?>
+<?php 
 
-<div id="post-<?php echo get_the_ID()?>" class="post-wrapper grid-col-max-480-12 grid-col-av-12 grid-col-md-6 grid-col">
+$tbp_post_id = get_the_ID();
+
+setPostViews($tbp_post_id);?>
+
+<div id="post-<?php echo $tbp_post_id?>" class="post-wrapper grid-col-max-480-12 grid-col-av-12 grid-col-md-6 grid-col">
 	<div class="post-contents blog-post grid-col">
+  
 	  <div class="post-info">
 	  
-	   <?php if( has_post_thumbnail() && is_url_exist(get_the_post_thumbnail_url(get_the_ID()))){
+	   <?php if( has_post_thumbnail() && is_url_exist(get_the_post_thumbnail_url($tbp_post_id))){
 	
 			get_template_part('templates/post-layout/post','with-thumb') ;
 	
@@ -23,20 +28,28 @@
 			</div>
 			
 			<div class="author-name">
+			
 				<span>
+				
 					<?php 
 						_e('By','smartpage');
 						echo ' '.get_the_author(); 
 					?>
+					
 				</span>
+				
 			</div>
 			
 			<div>
+			
 				<a class="button" href="<?php echo get_the_permalink() ?>"><?php _e('Read more','smartpage') ?></a>
+				
 			</div>
 			
 		</div>
 		
 	</div>
+	
   </div>
+  
 </div>
