9ed095e81e20950f936b69ccc4c7dd205e4687da
makiomar <prosentra@gmail.com>
2019-04-03 14:36:52 +0300

    Replace seach template

--- search.php
+++ search.php
@@ -5,7 +5,29 @@ get_header();
   	<div class="grid-row">
       <div class="grid-col">
         <div class="grid-col-sm-9-5">
-        <?php get_template_part('templates/temp','blog') ;?>	
+        <?php 
+
+			if ( have_posts() ) {
+				while (have_posts() ) { the_post();
+				get_template_part('templates/temp','blog-post') ;
+
+				} 
+			}
+			if(is_rtl()){
+				$prev_text = '<i class="fa fa-arrow-right"></i>';
+				$next_text = '<i class="fa fa-arrow-left"></i>';
+			}else{
+				$prev_text = '<i class="fa fa-arrow-left"></i>';
+				$next_text = '<i class="fa fa-arrow-right"></i>';
+			}
+        	the_posts_pagination( array(
+					'type' => 'list',
+					'prev_text' => $prev_text,
+					'next_text' => $next_text,
+					'screen_reader_text'=>' ',
+
+				) );
+		?>	
         </div>
        <?php get_sidebar();?>
 	  </div>
