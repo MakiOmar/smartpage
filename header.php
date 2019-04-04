a783f9f1b259d0ab8ab34598bcaf1dd4c368cca0
makiomar <prosentra@gmail.com>
2019-04-04 12:27:42 +0300

    fix meta tag content and character set

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ ob_start('ob_gzhandler');
 <!doctype html>
 <html itemscope itemtype="http://schema.org/WebPage" <?php language_attributes()?>>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>"/>
+<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>"/>
 <meta http-equiv="x-ua-compatible" content="IE=edge" >
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 <?php wp_head();?>
