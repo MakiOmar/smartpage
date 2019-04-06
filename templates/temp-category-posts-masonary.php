60e33cacd75f1501c3c74a0b49f851c5a10633bc
makiomar <prosentra@gmail.com>
2019-04-06 18:29:28 +0300

    remove featured posts from blog posts section

--- templates/temp-category-posts-masonary.php
+++ templates/temp-category-posts-masonary.php
@@ -1,6 +1,11 @@
 <?php
+$FreaturedCat = get_category_by_slug('featured-posts');
+
 	$tcp= new Smpg_Generate_Posts_View(
-			array('post_type'=>'post'),
+			array(
+				'post_type'=>'post',
+				'category__not_in' => $FreaturedCat->term_id,
+			),
 			'blog-post',
 			true
 		);
