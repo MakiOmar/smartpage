f49aadfac6b55c2c5de13821c4a766c4e2406929
makiomar <prosentra@gmail.com>
2019-04-07 09:54:04 +0300

    add ajax input feld

--- footer.php
+++ footer.php
@@ -1,14 +1,27 @@
 <footer class="grid-col-md-12 grid-col">
+
 	<p><?php printf(__('All rights reserved for %s 2018',TEXTDOM),get_bloginfo()) ?></p>
+	
 </footer>
+
 <div id="page-scroll-wrapper">
+
 	<div id="page-scroll-bg"></div>
+	
 	<a href="#" id="page-scroll"><i class="fa fa-angle-down fa-3x"></i></a>
+	
 </div>
+
+</div>
+
+<input type="hidden" id="smpg_ajax_url" value="<?php echo smpg_get_ajax_url(); ?>" />
+
 <?php wp_footer();?>
-</div>
+
 </body>
+
 </html>
+
 <?php
 // end output buffering and send our HTML to the browser as a whole
 ob_end_flush();
