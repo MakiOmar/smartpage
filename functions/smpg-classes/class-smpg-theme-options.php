56a61a130fb9b2e2d0d3a6661dbac5420b0d7a31
makiomar <prosentra@gmail.com>
2019-04-11 12:50:38 +0300

    create theme options class

---
+++ functions/smpg-classes/class-smpg-theme-options.php
@@ -0,0 +1,217 @@
+<?php
+if (!class_exists('Smpg_Theme_Options')) {
+	class Smpg_Theme_Options{
+		public $page = '';
+		public $OptionGroup = '';
+		public $sections = array();
+		public $args = array();
+		/**
+		 * Class Constructor. Defines the args for the theme options class
+		*/
+		public function __construct($menu = array(), $sections = array()){
+			
+			$defaults = array();
+			
+			$defaults['opt_name'] = SMPG_OPTIONS;
+			
+			//$defaults['menu_icon'] = MFN_OPTIONS_URI.'/img/menu_icon.png';
+			$defaults['menu_title'] = __('SmartPage Theme Options', TEXTDOM);
+			//$defaults['page_icon'] = 'icon-themes';
+			$defaults['page_title'] = __('SmartPage Theme Options', TEXTDOM);
+			$defaults['page_slug'] = 'smpg_options';
+			$defaults['page_cap'] = 'manage_options';
+			$defaults['page_type'] = 'menu';
+			$defaults['page_parent'] = '';
+			$defaults['page_position'] = 100;
+			
+			//get args
+			$this->args = $defaults;
+			
+			//Set option groups
+			$this->OptionGroup = $this->args['opt_name'].'_group';
+			
+			//get sections
+			$this->sections = $sections;
+			
+			//options page
+			add_action('admin_menu', array(&$this, '_options_page'));
+			
+			/**
+			 * register smpg_settings_init to the admin_init action hook
+			 */
+			add_action('admin_init', array(&$this, 'smpg_settings_init'));
+			
+
+		}
+		
+		/**
+		 * Class Theme Options Page Function, creates main options page.
+		*/
+		function _options_page(){
+			
+			$this->page = add_theme_page(
+				$this->args['page_title'], 
+				$this->args['menu_title'], 
+				$this->args['page_cap'], 
+				$this->args['page_slug'], 
+				array(&$this, '_options_page_html')
+			);
+			/*
+			*Load page scripts
+			*/
+			//add_action('admin_print_styles-'.$this->page, array(&$this, '_enqueue'));
+		}
+		function smpg_settings_init(){
+			// register a new setting for "smartpage" page
+			register_setting(
+				$this->OptionGroup,
+				$this->args['opt_name'],
+				array(
+					'sanitize_callback',
+					array(&$this,'smpg_options_validate_cd')
+				)
+			);
+			
+			foreach($this->sections as $secKey => $section){
+				
+				add_settings_section(
+					'smpg_'.$secKey.'_section',
+					$section['title'],
+					array(&$this,'smpg__section_cb'),
+					//You should pass the page passed added by add_theme_page
+					$this->args['opt_name']
+				);
+				
+				if(isset($section['fields'])){
+
+					foreach($section['fields'] as $fieldKey => $field){
+
+							if(isset($field['title'])){
+
+								$fieldTitle = (isset($field['sub_desc'])) ? $field['title'].'<span class="description">'.$field['sub_desc'].'</span>' : $field['title'];
+
+							}else{
+								
+								$fieldTitle = '';
+								
+							}
+							
+							add_settings_field(
+								$fieldKey.'_field',
+								$fieldTitle,
+								array(&$this,'smpg__field_input'),
+								//You should pass the page passed to add_settings_section
+								$this->args['opt_name'],
+								'smpg_'.$secKey.'_section',
+								$field
+							);
+
+					}
+
+				}
+			}
+
+		}
+
+		function smpg__section_cb($section){
+			
+		}
+		function smpg__field_input($field){
+
+			if(isset($field['callback']) && function_exists($field['callback'])){
+
+			}?>
+			
+			<select>
+				<option>1</option>
+				<option>1</option>
+				<option>1</option>
+				<option>1</option>
+				<option>1</option>
+				<option>1</option>
+			</select>
+			
+		<?php }
+		
+
+		function smpg_options_validate_cd($smpg_options){
+			$Smpg_Options = Smpg_Options::get_instance();
+			$message = null;
+			$type = null;
+
+			if ( null != $smpg_options ) {
+
+				if ( false === $Smpg_Options->get_option( 'myOption' ) ) {
+
+					$Smpg_Options->add_option( 'myOption', $smpg_options );
+
+					$type = 'updated';
+
+					$message = esc_html__( 'Successfully saved', TEXTDOM );
+
+
+				} else {
+
+					$Smpg_Options->update_option( 'myOption', $smpg_options );
+
+					$type = 'updated';
+
+					$message = esc_html__( 'Successfully updated', TEXTDOM );
+
+				}
+
+			} else {
+
+				$type = 'error';
+				$message = esc_html__( 'Data can not be added', TEXTDOM );
+
+			}
+			/*add error/update messages
+			*check if the user have submitted the settings
+			*wordpress will add the "settings-updated" $_GET parameter to the url
+			*/
+			if ( isset( $_GET['settings-updated'] ) ) {
+				 // add settings saved message with the class of "updated"
+				 add_settings_error( 'smpg_messages', esc_attr( 'smpg_settings_updated' ), $message, $type );
+			 }
+
+			// show error/update messages
+			settings_errors( 'smpg_messages' );
+		}
+		
+		/**
+		 * HTML OUTPUT.
+		*/
+		function _options_page_html(){
+			global $wp_settings_sections;
+			// check user capabilities
+			if ( ! current_user_can( 'manage_options' ) ) return;?>
+
+			<div class="wrap">
+				<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+
+				<form action="options.php" method="post">
+
+					<?php
+
+					// output security fields for the registered setting "smpg_options"
+					settings_fields( $this->OptionGroup );
+
+					// output setting sections and their fields
+					// (sections are registered for "smpg_options", each field is registered to a specific section)
+					//You should pass the page passed to add_settings_section
+					do_settings_sections( $this->args['opt_name'] );
+				
+					//neat_print_r($wp_settings_sections);
+
+					// output save settings button
+					submit_button( 'Save Settings' );
+					?>
+
+				</form>
+			</div>
+<?php
+}
+	}
+}
+?>
