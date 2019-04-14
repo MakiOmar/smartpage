00f48ae08c9d4093b36b3c0dfbfbb354268056b5
makiomar <prosentra@gmail.com>
2019-04-15 00:57:50 +0300

    rename file

--- functions/options/fields/select/f-select.php
+++ functions/options/fields/select/f-select.php
@@ -1,11 +1,11 @@
 <?php
-class MFN_Options_select extends MFN_Options{	
+class Options__Fields__Select__F_Select extends Options__Theme_Settings{	
 	
 	/**
 	 * Field Constructor.
 	*/
 	function __construct( $field = array(), $value ='', $parent = NULL ){
-		if( is_object($parent) ) parent::__construct($parent->sections, $parent->args, $parent->extra_tabs);
+		if( is_object($parent) ) parent::__construct($parent->sections, $parent->args, $parent->extraTabs);
 		$this->field = $field;
 		$this->value = $value;
 	}
@@ -14,11 +14,22 @@ class MFN_Options_select extends MFN_Options{
 	 * Field Render Function.
 	*/
 	function render( $meta = false ){
-		
+
 		$class = ( isset( $this->field['class']) ) ? 'class="'.$this->field['class'].'" ' : '';
+		
 		$name = ( ! $meta ) ? ( $this->args['opt_name'].'['.$this->field['id'].']' ) : $this->field['id'];
 		
-		echo '<select name="'. $name .'" '.$class.'rows="6" >';
+		if( get_transient( $this->field['id'] ) ){ ?>
+		
+			<p class="error"><?php echo  get_transient( $this->field['id'] )?></p>
+			
+		<?php 
+			
+			delete_transient( $this->field['id'] );
+												  
+		}
+		
+		echo '<select name="'. $name .'" '.$class.'rows="6" autocomplete="off">';
 			if( is_array( $this->field['options'] ) ){
 				foreach( $this->field['options'] as $k => $v ){
 					echo '<option value="'.$k.'" '.selected($this->value, $k, false).'>'.$v.'</option>';
