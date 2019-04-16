5badccdef619eb70ed9cb2a93bbc29be58d37f6f
makiomar <prosentra@gmail.com>
2019-04-16 13:36:37 +0300

    navigate with jquery

--- functions/options/js/options.js
+++ functions/options/js/options.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+jQuery(document).ready(function($){
+	"use strict";
+	
+	$(document).on('click','.smpg-nav-link', function(e){
+		e.preventDefault;
+		var linkId = $(this).attr('id');
+		
+		$('.smpg-section-group').each(function(){
+			if($(this).hasClass('smpg-show-section')){
+				$(this).removeClass('smpg-show-section');
+			}
+		});
+		
+		$('#smpg-'+linkId+'-section-group').addClass('smpg-show-section');
+		
+	});
+});
