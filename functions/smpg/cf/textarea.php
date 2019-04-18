bb3d64ede8ed89ec6fa5718bb7fc96e176da3977
makiomar <prosentra@gmail.com>
2019-04-19 01:25:04 +0300

    add textarea input class

---
+++ functions/smpg/cf/textarea.php
@@ -0,0 +1,30 @@
+<?php 
+
+class Smpg__Cf__Textarea extends Smpg__Custom_Field{
+	public $post, $metaBox, $id;
+	public function __construct($post, $metaBox){
+		parent::__construct();
+		$this->post = $post;
+		$this->metaBox= $metaBox;
+		$this->id= $this->metaBox['id'];
+		$this->idAttr= str_replace('_', '-', $this->id);
+	}
+	
+	public function render(){
+		
+		wp_nonce_field( $this->id.'_action', $this->id.'_nonce' );
+		
+		if(get_post_meta( $this->post->ID, $this->id, true )){
+			
+			$value = get_post_meta( $this->post->ID, $this->id, true );
+			
+		}else{
+			
+			$value = '';
+			
+		}
+		
+		echo '<textarea rows="4" cols="20" id="'.$this->idAttr.'" name="'.$this->id.'">'.$value.'</textarea> ';
+	}
+} 
+
