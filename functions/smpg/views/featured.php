610625378dd7b9b654a6744bbe83494da3ca24c2
makiomar <prosentra@gmail.com>
2019-04-26 14:13:50 +0300

    implement featured post views class

---
+++ functions/smpg/views/featured.php
@@ -0,0 +1,91 @@
+<?php
+class Smpg__Views__Featured extends Smpg__Generate_Posts_View{
+	
+	public $IfNot = '';
+	
+	public $child;
+	
+	public $featuredIDs = array();
+	
+	public function __construct($parent){
+		parent::__construct($parent->args, $parent->postsTemplate, $parent->resetLoop);
+		
+		$this->child = $parent;
+		
+		$this->IfNot();
+	}
+	
+	public function render(){?>
+		
+		<div id="slider-wrapper">
+	
+		<div id="featured">
+			<div id="active-slide">
+				<?php 
+					 while($this->child->post->have_posts()){
+
+						$this->child->post->the_post();
+						if (has_post_thumbnail()){
+
+						$this->featuredIDs[]= get_the_ID();
+
+						$link = get_the_permalink();?>
+
+						<div class="view">
+
+						  <?php the_post_thumbnail('full');?>
+
+						  <div class="title-readmore">
+
+							  <h2 class="slide-title"><a href="<?php echo $link ?>"><?php the_title() ;?></a></h2>
+
+							  <a class="featured-button" href="<?php echo $link?>"><?php esc_html_e('Read more', TEXTDOM)?></a>
+
+						  </div>
+						</div>
+					<?php }
+				} 
+				if($this->child->resetLoop === true){
+					wp_reset_postdata();
+				};?>
+			</div>
+
+			<?php if(count($this->featuredIDs) > 0){ ?>
+			
+			<h3 class="featured-posts-title">
+			
+				<a href="<?php echo get_category_link($this->child->args['cat']) ?>">
+				
+					<?php echo get_cat_name( $this->child->args['cat'] ) ?>
+					
+				</a>
+				
+			</h3>
+				
+		
+			<div id="slides-list" class="">
+
+			<?php foreach($this->featuredIDs as $pID) {?>
+
+				<a href="<?php echo get_the_permalink($pID) ?>" class="<?php if(array_search($pID,$this->featuredIDs) == 0) echo 'active-slide'?> slide-item grid-col-<?php echo count($this->featuredIDs)?>">
+				
+					<img src="<?php echo get_the_post_thumbnail_url($pID,'thumbnail') ?>" alt="<?php echo get_the_title($pID) ?>"/>
+					
+				</a>
+
+				<?php }?>
+
+			</div>
+		
+		<?php }?>
+
+			</div>
+		</div>
+		
+	<?php }
+	
+	public function IfNot(){
+		echo $this->IfNot;
+	}
+}
+?>
--- templates/temp-featured.php
+++ templates/temp-featured.php
@@ -17,82 +17,12 @@
 			'cat'            =>$FreaturedCat->term_id
 		);
 		
-		$FreaturedQuery = new WP_Query($args);
-		
-		if ( $FreaturedQuery->have_posts() ) {
-			
-			$FeaturedIDs = array();
-			
-	?>
-	<div id="slider-wrapper">
-	
-		<div id="featured">
-	
-			<div id="active-slide">
-				<?php
-
-				while ($FreaturedQuery->have_posts() ) { 
-
-					$FreaturedQuery->the_post();
-
-					if (has_post_thumbnail()){
-
-						$FeaturedIDs[]= get_the_ID();
-
-						$link = get_the_permalink();?>
-
-						<div class="view">
-
-						  <?php the_post_thumbnail('full');?>
-
-						  <div class="title-readmore">
-
-							  <h2 class="slide-title"><a href="<?php echo $link ?>"><?php the_title() ;?></a></h2>
-
-							  <a class="featured-button" href="<?php echo $link?>"><?php esc_html_e('Read more', TEXTDOM)?></a>
-
-						  </div>
-						</div>
-					<?php }
-				}?>
-			</div>
-		
-		<?php if(count($FeaturedIDs) > 0){ ?>
-			
-			<h3 class="featured-posts-title">
-			
-				<a href="<?php echo get_category_link($FreaturedCat->term_id) ?>">
-				
-					<?php echo get_cat_name( $FreaturedCat->term_id ) ?>
-					
-				</a>
-				
-			</h3>
-				
-		
-			<div id="slides-list" class="">
-
-			<?php foreach($FeaturedIDs as $pID) {?>
-
-				<a href="<?php echo get_the_permalink($pID) ?>" class="<?php if(array_search($pID,$FeaturedIDs) == 0) echo 'active-slide'?> slide-item grid-col-<?php echo count($FeaturedIDs)?>">
-				
-					<img src="<?php echo get_the_post_thumbnail_url($pID,'thumbnail') ?>" alt="<?php echo get_the_title($pID) ?>"/>
-					
-				</a>
-
-				<?php }?>
-
-			</div>
-		
-		<?php }?>
-		
-	 </div>
-	 
-</div>
-
-<?php }
-		
-	wp_reset_postdata();
+		$fc= new Smpg__Generate_Posts_View(
+						$args,
+						'featured',
+						true
+					);
+		$fc->postsView();
 		
 	}; 
 ?>
