5b8ae0b2f8cdfcae88845b42b1264e95a494dcc7
makiomar <prosentra@gmail.com>
2019-04-27 17:57:53 +0300

    exclude featured posts from query dependin on both featured cat and featured post

--- templates/category-posts-masonary.php
+++ templates/category-posts-masonary.php
@@ -1,18 +1,28 @@
 <?php
 	$smpgOptions = Smpg__Options_Model::get_instance();
 	
-	if(isset($smpgOptions->smpg_slider_settings ) && $smpgOptions->smpg_slider_settings == 'featured-cat'){
-		$FreaturedCat = get_term_by( 
+	$args = array('post_type' => 'post');
+
+	if(isset($smpgOptions->smpg_slider_settings ) ){
+		if($smpgOptions->smpg_slider_settings == 'featured-cat'){
+			$FreaturedCat = get_term_by( 
 							'id', 
 							$smpgOptions->smpg_featured_cat_settings,
 							$smpgOptions->smpg_featured_tax_settings
 						);
+		
+			$args['category__not_in'] = $FreaturedCat->term_id;
+			
+		}elseif($smpgOptions->smpg_slider_settings == 'featured-post'){
+
+			$args['posts__not_in'] =  get_posts_ids_by_meta('smpg_set_featured', 'on');
+			
+		}
+		
 	}
 
 	$tcp= new Smpg__Generate_Posts_View(
-			array(
-				'category__not_in' => $FreaturedCat->term_id,
-			),
+			$args,
 			'masonary',
 			true
 		);
