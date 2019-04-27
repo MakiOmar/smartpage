4a7267c4a1489cf1837160592823cf225a26d827
makiomar <prosentra@gmail.com>
2019-04-27 13:43:47 +0300

    rename object

--- templates/news.php
+++ templates/news.php
@@ -1,9 +1,9 @@
 <?php
-	$tn= new Smpg__Generate_Posts_View(
+	$news= new Smpg__Generate_Posts_View(
 						array('post_type' => 'smpg_news','posts_per_page'=>5),
 						'news',
 						true
 					);
 
-	$tn->postsView();
+	$news->postsView();
 ?>
