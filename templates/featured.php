b0a977fb9f5d7af409a939044f46dffcd8ac1853
makiomar <prosentra@gmail.com>
2019-04-28 20:44:40 +0300

    add ability to check for rev slider

--- templates/featured.php
+++ templates/featured.php
@@ -1,35 +1,42 @@
 <?php
 
-	$smpgOptions = Smpg__Options_Model::get_instance();
-	$args = array(
-			'posts_per_page' => 5,
-			'orderby'        => 'rand',
-		);
+$smpgOptions = Smpg__Options_Model::get_instance();
 
-	if(isset($smpgOptions->smpg_slider_settings )){
+if(isset($smpgOptions->smpg_slider_settings )){
+	
+	if(smpg_is_active_plugin('revslider/revslider.php') && $smpgOptions->smpg_slider_settings == 'rev-slider'){
+		echo 'revolution slider';
+	}else{
+		
+		$args = array(
+					'posts_per_page' => 5,
+					'orderby'        => 'rand',
+				);
+		
 		if($smpgOptions->smpg_slider_settings == 'featured-cat'){
 			$FreaturedCat = get_term_by( 
 							'id', 
 							$smpgOptions->smpg_featured_cat_settings,
 							$smpgOptions->smpg_featured_tax_settings
 						);
-		
+
 		$args['cat'] = $FreaturedCat->term_id;
-			
+
 		}elseif($smpgOptions->smpg_slider_settings == 'featured-post'){
 			$args['meta_key'] = 'smpg_set_featured';
 		}
-		
+
 		$fc= new Smpg__Generate_Posts_View(
-						$args,
-						'featured',
-						true
-					);
+					$args,
+					'featured',
+					true
+				);
 		$fc->postsView();
-		
 	}
-		
-		
-		
+
+}
+
+
+
 
 ?>
