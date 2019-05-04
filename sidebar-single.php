39ecbb6d2cc51a1f55707de8e195775b4ef566df
makiomar <prosentra@gmail.com>
2019-05-04 13:03:54 +0300

    Refactor sidebar single

--- sidebar-single.php
+++ sidebar-single.php
@@ -1,30 +1,45 @@
- <span class="toggle-sidebar"><i class="fa fa-arrow-down"></i></span>
- <div class="grid-col-sm-2-5 asidebar single-sidebar grid-col">
-	<?php if ( is_single() ) {
-		  $cats = wp_get_post_categories($post->ID);
+ <?php 
+if ( is_single() ) {
+	$p_ID = get_The_ID();
 	
-		  if ($cats) {
-			$first_cat = $cats[0];
-			  
-			$catpostcount= number_postpercat($first_cat);
+	$cats = wp_get_post_categories($p_ID);
+	
+	if (!is_wp_error($cats) && !empty($cats)) {
+		
+		$first_cat = $cats[0];
 
-			if ($catpostcount>=2){
-				echo '<h3 class="widgeted_title"><a href="'.get_category_link( $first_cat ).'">'.get_cat_name( $first_cat ).'</a></h3>';
-			}
-			$args=array(
-			  'cat' => $first_cat,
-			  'post__not_in' => array($post->ID),
-			  'order' => 'ASC',
-			  'showposts'=>-1,
-			);
-			$same_cat_posts = new WP_Query($args);
-			if( $same_cat_posts->have_posts() ) {
-			  while ($same_cat_posts->have_posts()) : $same_cat_posts->the_post(); ?>
-				<h5 class="same_cat_post"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h5>
-			   <?php
-			  endwhile;
-			} //if ($my_query)
-		  } //if ($cats)
-		  wp_reset_query();  // Restore global post data stomped by the_post().
-		}?>	
-</div>
+		$catpostcount = number_postpercat($first_cat);
+
+		if ($catpostcount >= 2){
+				$args=array(
+				  'cat' => $first_cat,
+				  'post__not_in' => array($p_ID),
+				  'order' => 'ASC',
+				  'posts_per_page'=>-1,
+				);
+
+				$same_cat_posts = new WP_Query($args);
+
+				if( $same_cat_posts->have_posts() ) {?>
+
+					<span class="toggle-sidebar"><i class="fa fa-arrow-down"></i></span>
+
+					<div class="grid-col-sm-2-5 asidebar single-sidebar grid-col">
+
+						<?php 
+						echo '<h3 class="widgeted_title"><a href="'.get_category_link( $first_cat ).'">'.get_cat_name( $first_cat ).'</a></h3>';
+						while ($same_cat_posts->have_posts()) : $same_cat_posts->the_post(); ?>
+
+							<h5 class="same_cat_post"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title();?></a></h5>
+
+					   <?php 
+							endwhile;
+							wp_reset_postdata();  // Restore global post data stomped by the_post().
+						?>
+
+				  </div>
+				<?php } //if ($same_cat_posts)
+		}
+	} //if ($cats)
+	
+}?>	
