7bfd0c55e0ff4086c99fafe1071388a2f55f740a
makiomar <prosentra@gmail.com>
2019-05-04 11:35:02 +0300

    replace description with note

--- functions/options/fields/select/f-select.php
+++ functions/options/fields/select/f-select.php
@@ -19,8 +19,8 @@ class Options__Fields__Select__F_Select extends Options__Theme_Settings{
 		
 		$name = ( ! $meta ) ? ( $this->args['opt_name'].'['.$this->field['id'].']' ) : $this->field['id'];
 		
-		if(isset($this->field['desc'])){
-			echo '<p class=smpg-warning>'.$this->field['desc'].'<p>';
+		if(isset($this->field['note'])){
+			echo '<p class=smpg-warning>'.$this->field['note'].'<p>';
 		}
 		echo '<select name="'. $name .'" '.$class.'rows="6" autocomplete="off">';
 			if( is_array( $this->field['options'] ) ){
