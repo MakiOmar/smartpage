f8670b4fe68e8400552733e8a03398e7d5ff9bfb
makiomar <prosentra@gmail.com>
2019-05-07 10:47:25 +0300

    add ability to toggle slider options

--- functions/options/js/options.js
+++ functions/options/js/options.js
@@ -14,4 +14,31 @@ jQuery(document).ready(function($){
 		$('#smpg-'+linkId+'-section-group').addClass('smpg-show-section');
 		
 	});
+	
+	$('.smpg-radio-smpg_slider_settings').find('input[type="radio"]').change(function(){
+		var clicked = $(this);
+
+		$('.smpg_slider_settings_show').each(function(){
+			$(this).removeClass('smpg_slider_settings_show');
+		});
+
+		$('.' + clicked.val()).addClass('smpg_slider_settings_show');
+		
+	});
+	
+	$("#smpg_home_slider_settings").change(function() {
+		
+			$('.smpg_rev_slider_settings').each(function(){
+				
+				if($(this).hasClass('smpg_home_slider_settings')){
+					
+					$(this).removeClass('smpg_home_slider_settings');
+					
+				}else{
+					
+					$(this).addClass('smpg_home_slider_settings');
+					
+				}	
+			});
+	});
 });
