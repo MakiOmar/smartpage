fc9aa2302ccc4549745a172d3a2d68b8b0e8d504
makiomar <prosentra@gmail.com>
2019-05-11 11:18:31 +0300

    implement standard post view

--- templates/category-posts-standard.php
+++ templates/category-posts-standard.php
@@ -1,22 +1,40 @@
 <?php
-		$tcp= new Smpg__Generate_Posts_View(
-				array(),
-				'blog-post',
-				true
-			);
-		if(is_front_page() or is_home()){
-			$tcp->sectionWrapperOpen = 
-			'<div class="section">
-				<div>
-					<h4 class="section_title clearfix">'.esc_html__('Recent Posts',TEXTDOM).'
-					</h4>
-			</div>';
-		}else{
-			$tcp->sectionWrapperOpen ='<div>';
+	$smpgOptions = Smpg__Options_Model::get_instance();
+	
+	$args = array('post_type' => 'post', 'posts_per_page' => 5);
+
+	if(isset($smpgOptions->smpg_slider_settings ) ){
+		
+		if($smpgOptions->smpg_slider_settings != 'rev-slider'){
+			
+			if($smpgOptions->smpg_slider_settings == 'featured-cat' && $smpgOptions->smpg_featured_cat_settings != '0'){
+				$FreaturedCat = get_term_by( 
+								'id', 
+								$smpgOptions->smpg_featured_cat_settings,
+								$smpgOptions->smpg_featured_tax_settings
+							);
+
+				$args['category__not_in'] = $FreaturedCat->term_id;
+
+			}elseif($smpgOptions->smpg_slider_settings == 'featured-post'){
+
+				$args['posts__not_in'] =  get_posts_ids_by_meta('smpg_set_featured', 'on');
+
+			}
+			
 		}
 		
-		$tcp->sectionWrapperClose ='</div>';
+		
+	}
 
-		$tcp->postsView();
-						
+$cps= new Smpg__Generate_Posts_View(
+					$args,
+					'standard',
+					true
+				);
+
+$cps->postsView();
 ?>
+
+
+
