47704bc8a606ebfaca4e78b440303f46a0abb990
makiomar <prosentra@gmail.com>
2019-05-12 11:04:06 +0300

    apply socials options

--- templates/top-header.php
+++ templates/top-header.php
@@ -1,24 +1,33 @@
-<?php 
+<?php
+$smpgOptions = Smpg__Options_Model::get_instance();
+
 $socials_follow = array(
-				'facebook' => '#',
-				'twitter' => '#',
-				'google-plus' => '#',
-				'youtube' => '#',
-				'pinterest' => '#',
-				'instagram' => '#',
-				'tumblr' => '#',
-				'rss' => '#',
+				'facebook' => $smpgOptions->facebook,
+				'twitter' => $smpgOptions->twitter,
+				'youtube' => $smpgOptions->youtube,
+				'pinterest' => $smpgOptions->pinterest,
+				'linkedin' => $smpgOptions->linkedin,
+				'instagram' => $smpgOptions->instagram,
+				'tumblr' => $smpgOptions->tumblr,
+				'rss' => $smpgOptions->rss,
 				);
 ?>
 <div id="top-header-wrapper">
 	<nav id="follow-contact">
 	  <ul id="follow">
-	  	<?php foreach($socials_follow as $social_follow => $url){
-	$link_title = esc_html__(sprintf('Follow with %s' , ucfirst($social_follow)),TEXTDOM);
+		<?php 
+			foreach($socials_follow as $social_follow => $url){
+				
+				$link_title = esc_html__(sprintf('Follow on %s' , ucfirst($social_follow)),TEXTDOM);
+				
 				echo '<li><a class="icon" href="'.$url.'" title="'.$link_title.'" target="_blank"><i class="fa fa-'.$social_follow.'"></i></a></li>';
-		} ?>
-		<li><a href="tel:00966536539253" class="phone-call"><i class="fa fa-phone"></i></a></li>
-		<li><a href="mailto:mo7amed.maki@gmail.com" class="email-me"><i class="fa fa-envelope"></i></a></li>
+				
+			} 
+		?>
+		<li><a href="tel:<?php echo $smpgOptions->phone ?>" class="phone-call"><i class="fa fa-phone"></i></a></li>
+		
+		<li><a href="mailto:<?php echo $smpgOptions->email ?>" class="email-me"><i class="fa fa-envelope"></i></a></li>
+		
 	  </ul>
 	  <?php echo smpg_main_navigation('languages-menu','') ?>
 	</nav>
