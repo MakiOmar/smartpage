ef163106f598ab779f1ba7af74f86dad7335b965
makiomar <prosentra@gmail.com>
2019-06-22 14:13:11 +0300

    Change theme's autoloading mechanism

--- functions/config.php
+++ functions/config.php
@@ -15,6 +15,12 @@ define( 'THEME_VERSION', '1.0' );
 
 define( 'SMPG_CLASSES', wp_normalize_path (LIBS_DIR . abstracted_generate_path(array('smpg'))));
 
+//Custom fileds classes
+define( 'SMPG_CF_CLASSES', wp_normalize_path (SMPG_CLASSES . abstracted_generate_path(array('cf'))));
+
+//views classes
+define( 'SMPG_VIEWS_CLASSES', wp_normalize_path (SMPG_CLASSES . abstracted_generate_path(array('views'))));
+
 define( 'LANG_DIR', wp_normalize_path(THEME_DIR. abstracted_generate_path(array('languages'))));
 
 define( 'STAR_RATE', $GLOBALS['wpdb']->prefix . 'star_rating' );
@@ -29,31 +35,29 @@ define( 'SMPG_OPTIONS_URI', wp_normalize_path (THEME_URI . abstracted_generate_p
 
 define('SuppTypes',serialize(array('pdf','doc','docx','7z','arj','deb','zip','iso','pkg','rar','rpm','z','gz','bin','dmg','toast','vcd','csv','dat','log','mdb','sav','tar','ods','xlr','xls','xlsx','odt','txt','rtf','tex','wks','wps','wpd')));
 
+
+define('SMPG_THEME_AUTOLOADS' ,serialize(array(SMPG_CLASSES, SMPG_CF_CLASSES, SMPG_VIEWS_CLASSES)));
 /*
 *Classes Auto loader
 */
-spl_autoload_register( 'smpg_autoloader' );
+spl_autoload_register( 'thm_autoloader' );
 
 /*
 *@param  string $class_name 
 */
-function smpg_autoloader( $class_name ) {
+function thm_autoloader( $class_name ) {
+	if ( false !== strpos( $class_name, '__' )) {
+		$class_name = preg_replace('/\w+__/', '', strtolower($class_name));
 
-	if ( false !== strpos( $class_name, 'Smpg__' )) {
+		$class_name  = str_replace('_', '-', $class_name);
 
-		$class_sub =  preg_replace('/__/', DIRS, strtolower($class_name));
-
-		$classes_dir = THEME_DIR .abstracted_generate_path(array('functions'));
-
-		$class_path =  wp_normalize_path($classes_dir) . str_replace('_','-',$class_sub) . '.php';
-
-		if(file_exists($class_path)){
-
-			require_once $class_path;
-
+		foreach(unserialize( SMPG_THEME_AUTOLOADS ) as $path){
+			$class_file = wp_normalize_path($path) .$class_name . '.php';
+			if(file_exists($class_file)){
+				require_once($class_file);
+			}
 		}
-
+		
 
 	}
-
 }
