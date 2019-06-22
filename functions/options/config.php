9eded15ef7aa61edd002c4b15d81b5c5c25619ac
makiomar <prosentra@gmail.com>
2019-06-22 14:13:55 +0300

    Change opts's autoloading mechanism

--- functions/options/config.php
+++ functions/options/config.php
@@ -1,9 +1,10 @@
 <?php
 $opts_consts = array( 
-	'SMPG_OPTIONS_DIR'      => THEME_DIR . "/functions/options/",
-	'SMPG_OPTIONS_URI'      => THEME_URI . "/functions/options/",
-	'SMPG_OPTIONS_FIELDS'   => THEME_DIR . "/functions/options/fields/",
-	'SMPG_OPTIONS_WIDGETS'  => THEME_DIR . "/functions/options/widgets/",
+	'DIRS'                 => DIRECTORY_SEPARATOR ,
+	'SMPG_OPTIONS_DIR'     => THEME_DIR . "/functions/options/",
+	'SMPG_OPTIONS_URI'     => THEME_URI . "/functions/options/",
+	'SMPG_OPTIONS_FIELDS'  => THEME_DIR . "/functions/options/fields/",
+	'SMPG_OPTIONS_WIDGETS' => THEME_DIR . "/functions/options/widgets/",
 	
 );
 
@@ -17,41 +18,37 @@ define('SMPG_OPTIONS_AUTOLOADS' ,serialize(array(SMPG_OPTIONS_DIR , SMPG_OPTIONS
 /*
 *Classes Auto loader
 */
-spl_autoload_register( 'opts_anony_autoloader' );
+spl_autoload_register( 'opts_autoloader' );
 
 /*
 *@param  string $class_name 
 */
-function opts_anony_autoloader( $class_name ) {
-	$class_name = strtolower($class_name);
-	$subFolder = '';
+function opts_autoloader( $class_name ) {
 	
-	$classDir = SMPG_OPTIONS_DIR;
-	
-	$class_file = $classDir . $class_name . '.php';
-	
-	
-	if(strpos( $class_name, 'field__' ) !== false){
-
-		$class_name = str_replace('field__','', $class_name);
-		$subFolder = $class_name . DIRECTORY_SEPARATOR;
-		$classDir = SMPG_OPTIONS_FIELDS; 
+	if ( false !== strpos( $class_name, '__' )) {
 		
-	}
-	
-	$class_file = wp_normalize_path($classDir . $subFolder . $class_name . '.php');
-
-	if(file_exists($class_file)){
-		require_once($class_file);
-	}else{
-
+		$class_name = preg_replace('/\w+__/', '', strtolower($class_name));
+		
 		foreach(unserialize( SMPG_OPTIONS_AUTOLOADS ) as $path){
-			$class_file = wp_normalize_path($path . DIRECTORY_SEPARATOR . $class_name . '.php');
+			$class_file = wp_normalize_path($path) .$class_name . '.php';
+			
 			if(file_exists($class_file)){
+				
 				require_once($class_file);
+				
+			}else{
+				
+				$class_file = wp_normalize_path($path. $class_name . DIRS) .$class_name . '.php';
+				
+				if(file_exists($class_file)){
+					
+					require_once($class_file);
+					
+				}
 			}
 		}
 		
+
 	}
 	
 }
