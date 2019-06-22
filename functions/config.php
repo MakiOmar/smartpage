4698c31d2df58fe7b9dff7d78960f21592213ed0
makiomar <prosentra@gmail.com>
2019-06-22 11:38:26 +0300

    apply get path function

--- functions/config.php
+++ functions/config.php
@@ -1,4 +1,6 @@
 <?php
+define( 'DIRS', DIRECTORY_SEPARATOR );
+
 define( 'THEME_DIR', wp_normalize_path( get_template_directory() ) );
 
 define( 'THEME_URI', get_template_directory_uri() );
@@ -9,9 +11,11 @@ define( 'TEXTDOM', strtolower(THEME_NAME) );
 
 define( 'THEME_VERSION', '1.0' );
 
-define( 'LIBS_DIR', THEME_DIR . '/functions/' );
+//define( 'LIBS_DIR', wp_normalize_path (THEME_DIR . abstracted_generate_path(array('functions'))));
 
-define( 'LANG_DIR', THEME_DIR. '/languages/' );
+define( 'SMPG_CLASSES', wp_normalize_path (LIBS_DIR . abstracted_generate_path(array('smpg'))));
+
+define( 'LANG_DIR', wp_normalize_path(THEME_DIR. abstracted_generate_path(array('languages'))));
 
 define( 'STAR_RATE', $GLOBALS['wpdb']->prefix . 'star_rating' );
 
@@ -21,7 +25,7 @@ define( 'BLOG_URL', get_bloginfo('url') );
 
 define( 'SMPG_OPTIONS', 'Smpg_Options' );
 
-define( 'SMPG_OPTIONS_URI', THEME_URI ."/functions/options/");
+define( 'SMPG_OPTIONS_URI', wp_normalize_path (THEME_URI . abstracted_generate_path(array('functions', 'options'))));
 
 define('SuppTypes',serialize(array('pdf','doc','docx','7z','arj','deb','zip','iso','pkg','rar','rpm','z','gz','bin','dmg','toast','vcd','csv','dat','log','mdb','sav','tar','ods','xlr','xls','xlsx','odt','txt','rtf','tex','wks','wps','wpd')));
 
@@ -34,18 +38,22 @@ spl_autoload_register( 'smpg_autoloader' );
 *@param  string $class_name 
 */
 function smpg_autoloader( $class_name ) {
-	
-  if ( false !== strpos( $class_name, 'Smpg__' ) /*|| false !== strpos( $class_name, 'Options__' )*/) {
-	  
-	$class_sub =  preg_replace('/__/', DIRECTORY_SEPARATOR, strtolower($class_name));
-    $classes_dir = THEME_DIR .DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR;
-	  
-    $class_path =  $classes_dir . str_replace('_','-',$class_sub) . '.php';
-	if(file_exists(wp_normalize_path($class_path))){
-		require_once wp_normalize_path($class_path);
+
+	if ( false !== strpos( $class_name, 'Smpg__' )) {
+
+		$class_sub =  preg_replace('/__/', DIRS, strtolower($class_name));
+
+		$classes_dir = THEME_DIR .DIRS.'functions'.DIRS;
+
+		$class_path =  wp_normalize_path($classes_dir) . str_replace('_','-',$class_sub) . '.php';
+
+		if(file_exists($class_path)){
+
+			require_once $class_path;
+
+		}
+
+
 	}
-    
-	  
-  }
-	
+
 }
