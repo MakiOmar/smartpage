a52ae2b2d2a243449b1e6c8b300598185d13bc1d
makiomar <prosentra@gmail.com>
2019-07-16 12:02:43 +0300

    secure translation using wp_kses

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,17 @@
- <div class="grid-col-sm-2-5 asidebar">
+<div class="grid-col-sm-2-5 asidebar">
 	<?php  
 		if (is_active_sidebar('right-sidebar')){
 		   dynamic_sidebar('right-sidebar');
 		}else{
-			$url = get_bloginfo('url').'/wp-admin/widgets.php';
-			printf(__( '<strong>Please add some widgets.</strong><a href="%s"> Add Here</a>', TEXTDOM ),esc_url($url));
+			$url = get_home_url().'/wp-admin/widgets.php';
+			$allowed_tags = array('strong'=> array(), 'a' => array('href'=> array()));
+			printf(
+				wp_kses(
+						__( '<strong>Please add some widgets.</strong><a href="%s"> Add Here</a>', TEXTDOM ), 
+						$allowed_tags
+					),
+				esc_url($url)
+			);
 		}
 		//get_template_part('templates/newsletters') ;
 	?>
