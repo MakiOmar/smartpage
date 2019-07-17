4a8d8f59fcf6dc38e5011393805fdfa7ee61d718
makiomar <prosentra@gmail.com>
2019-07-17 08:31:56 +0300

    Arrange/refactor admin functions code

--- functions/admin.php
+++ functions/admin.php
@@ -1,15 +1,39 @@
 <?php
-	add_action( 'admin_enqueue_scripts', 'smpg_load_admin_styles' );
-	function smpg_load_admin_styles() {
+/*
+*ALL administraion side functions
+*/
+
+/*--------------------------------------------------------------------
+*Hook to enqueue admin styles and scripts
+*-------------------------------------------------------------------*/
+add_action( 'admin_enqueue_scripts', function() {
+	
 	$styles = array('smpg-admin');
+	
 		foreach($styles as $style){
-			wp_enqueue_style( $style , get_theme_file_uri('/assets/css/'.$style.'.css') , false, filemtime(get_theme_file_path('/assets/css/'.$style.'.css')) );
+			
+			wp_enqueue_style( 
+				$style , 
+				THEME_URI . '/assets/css/'.$style.'.css' , 
+				false, 
+				filemtime( wp_normalize_path( THEME_DIR . '/assets/css/'.$style.'.css')  )
+			);
+			
 		}
 
 		$scripts = array('smpg-admin');
+	
 			foreach($scripts as $script){
-			wp_register_script( $script , get_theme_file_uri('/assets/js/'.$script.'.js') ,array('jquery'),filemtime(get_theme_file_path('/assets/js/'.$script.'.js')),true);
-				wp_enqueue_script($script);
+				
+			wp_register_script( 
+				$script , 
+				THEME_URI . '/assets/js/'.$script.'.js' ,
+				array('jquery'),
+				filemtime(wp_normalize_path( THEME_DIR . '/assets/js/'.$script.'.js' ) ),true
+			);
+			wp_enqueue_script($script);
 			}
-	} 
+	}  
+);
+
 ?>
