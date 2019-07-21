6ff545ba7d4615250029cb40bcf80bff6994cc06
makiomar <prosentra@gmail.com>
2019-07-21 12:38:52 +0300

    use new option name

--- templates/featured.php
+++ templates/featured.php
@@ -2,39 +2,40 @@
 
 $anonyOptions = opt_init_();
 
-if(isset($anonyOptions->slider )){
-		$args = array(
-					'posts_per_page' => 5,
-					'orderby'        => 'rand',
+$messege = '';
+
+$args = array(
+			'posts_per_page' => 5,
+			'orderby'        => 'rand',
+		);
+
+		
+if($anonyOptions->slider_content == 'featured-cat' && $anonyOptions->featured_cat != '0'){
+	$FreaturedCat = get_term_by( 
+					'id', 
+					$anonyOptions->featured_cat,
+					$anonyOptions->featured_tax
 				);
-		
-		if($anonyOptions->slider == 'featured-cat' && $anonyOptions->featured_cat != '0'){
-			$FreaturedCat = get_term_by( 
-							'id', 
-							$anonyOptions->featured_cat,
-							$anonyOptions->featured_tax
-						);
-			
-			if($FreaturedCat){
-				$args['cat'] = $FreaturedCat->term_id;
-			}else{
-				$fc->msg = esc_html__('Please make sure you select a category and its corresponding taxonomy from theme options->slider', TEXTDOM);
-			}
-		
-		}elseif($anonyOptions->slider == 'featured-post'){
-			$args['meta_key'] = 'anony_set_featured';
-		}
-		
-		$fc= new Class__Generate_Posts_View(
+
+	if($FreaturedCat){
+		$args['cat'] = $FreaturedCat->term_id;
+	}else{
+		$messege = esc_html__('Please make sure you select a category and its corresponding taxonomy from theme options->slider', TEXTDOM);
+	}
+
+}elseif($anonyOptions->slider_content == 'featured-post'){
+	$args['meta_key'] = 'anony_set_featured';
+}	
+
+
+$fc= new Class__Generate_Posts_View(
 					$args,
 					'featured',
 					true
 				);
-	
-		$fc->postsView();
+$fc->msg = $messege;
 
-}
-
+$fc->postsView();
 
 
 
