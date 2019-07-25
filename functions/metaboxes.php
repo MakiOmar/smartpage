6e5a9bea0d1474b400e7bec95bc7335b3a2c4443
makiomar <prosentra@gmail.com>
2019-07-25 23:39:21 +0300

    Make metabox id pattern

--- functions/metaboxes.php
+++ functions/metaboxes.php
@@ -8,11 +8,12 @@
  */
 
 //Array of metaboxes to register
+//id should be equal to title without the anony__ and underscores
 $metaBoxes = array(
 	'post' => array(
 				array(
-					'id'       => 'anony_set_featured',
-					'title'    => esc_html__( 'Set as featured post', TEXTDOM ),
+					'id'       => 'anony__set_as_featured',
+					'title'    => esc_html__( 'Set as featured', TEXTDOM ),
 					'context'  => 'side',
 					'type'     => 'checkbox',
 					'validate' => 'no_html',
@@ -20,14 +21,22 @@ $metaBoxes = array(
 				
 			),
 	'anony_download' => array(
-							array(
-								'id'        => 'anony_download_attachment',
-								'title'     => esc_html__( 'Upload your attachment', TEXTDOM ),
-								'context'   => 'normal',
-								'type'      => 'upload',
-								'validate'  => 'url|file_type:pdf,doc,docx,7z,arj,deb,zip,iso,pkg,rar,rpm,z,gz,bin,dmg,toast,vcd,csv,dat,log,mdb,sav,tar,ods,xlr,xls,xlsx,odt,txt,rtf,tex,wks,wps,wpd',
+				array(
+					'id'        => 'anony__upload_attachment',
+					'title'     => esc_html__( 'Upload attachment', TEXTDOM ),
+					'context'   => 'normal',
+					'type'      => 'upload',
+					'validate'  => 'url|file_type:pdf,doc,docx,7z,arj,deb,zip,iso,pkg,rar,rpm,z,gz,bin,dmg,toast,vcd,csv,dat,log,mdb,sav,tar,ods,xlr,xls,xlsx,odt,txt,rtf,tex,wks,wps,wpd',
 
-							),
+				),
+				array(
+					'id'        => 'anony__some_text',
+					'title'     => esc_html__( 'Some text', TEXTDOM ),
+					'context'   => 'normal',
+					'type'      => 'text',
+					'validate'  => 'number',
+
+				),
 			),
 
 );
