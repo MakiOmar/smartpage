ea59568b48de457c754b5fe18a8fe1337bfcc892
makiomar <prosentra@gmail.com>
2019-07-27 01:54:10 +0300

    refactor upload field

--- functions/class/cf/upload.php
+++ functions/class/cf/upload.php
@@ -5,7 +5,7 @@
  */
 
 class Cf__Upload extends Class__Custom_Field{
-		/**
+	/**
 	 * @var int Current post ID
 	 */
 	public $post_id;
@@ -38,46 +38,67 @@ class Cf__Upload extends Class__Custom_Field{
 		$this->field_id    = $field['id'];
 		
 		$this->field_label = $field['label'];
-		
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 
 	}
 	
 	public function render(){
 		
-		wp_nonce_field( $this->field_id.'_action', $this->field_id.'_nonce' );?>
+		wp_nonce_field( $this->field_id.'_action', $this->field_id.'_nonce' );
 		
-		<div class="anony-file-upload-override-button">
-			<a href="#" class="insert-media" data-editor="anony-my-editor"><?php esc_html_e('Select your file',TEXTDOM) ;?></a>
-		</div>
-		
-		<?php
-			$file_url = get_post_meta( $this->post_id, $this->field_id, true );
-		
-			if(is_array($file_url)){
-				delete_post_meta( $this->post_id, $this->field_id );
-			}
-		
-			if(!empty($file_url)){
-				   $html = '<div id="anony-download-file"><p>'.esc_html__('Current file:',TEXTDOM).'<span>'.basename($file_url).'</span></p><a href="'.esc_url($file_url).'">'.esc_html__('Download',TEXTDOM).'</a></div>';
-			}else{
-				$html = '<div id="anony-download-file"><p>'.'<span>'.esc_html__('No selected file ',TEXTDOM).'</span></p></div>';
-			}
-		
-			echo $html;
-		?>
-		<!-- Caller -->
-		<span id="anony-media-caller">
-			<div class="attachment">
-				<img width="277" height="300" alt="{{ alt }}">
-				<input type="hidden" name ="<?php echo $this->field_id ?>" value="{{ url }}">
-			</div>
-		</span>
+		$file_url = get_post_meta( $this->post_id, $this->field_id, true );
 
-		<!-- Results placeholder -->
-		<div id="anony-upload-result"></div>
+		if(is_array($file_url)){
+			delete_post_meta( $this->post_id, $this->field_id );
+		}
 		
-		<?php 
+		$html = sprintf('<fieldset class="anony-row" id="fieldset_%1$s">', esc_attr($this->field_id));
+		
+		$html .= sprintf( 
+						'<div class="anony-file-upload-override-button">
+							<a href="#" class="insert-media" data-editor="anony-my-editor">%1$s</a>
+						</div>',
+						esc_html__('Select your file',TEXTDOM)
+					);
+		
+		if(!empty($file_url)){
+			   $html .= sprintf(
+				   		'<div id="anony-download-file">
+			   				<p>%1$s<span>%2$s</span>
+							</p>
+							<a href="%3$s">%4$s</a>
+						</div>', 
+						esc_html__('Current file:',TEXTDOM),
+						basename($file_url),
+						esc_url($file_url),
+						esc_html__('Download',TEXTDOM)
+					);
+		}else{
+			$html .= sprintf(
+						'<div id="anony-download-file">
+							<p>'.'<span>%1$s</span></p>
+					 	</div>', 
+						esc_html__('No selected file ',TEXTDOM)
+					);
+		}
+		
+		$html .= sprintf( 
+						'<!-- Caller -->
+						<span id="anony-media-caller">
+							<div class="attachment">
+								<img width="277" height="300" alt="{{ alt }}">
+								<input type="hidden" name ="%1$s" value="{{ url }}">
+							</div>
+						</span>
+
+						<!-- Results placeholder -->
+						<div id="anony-upload-result"></div>', 
+						esc_attr($this->field_id)
+					);
+			
+		$html .= '</fieldset>';
+		
+		echo $html;
+
 	}
 	
 	public function enqueue_scripts(){
@@ -87,7 +108,7 @@ class Cf__Upload extends Class__Custom_Field{
 
 		foreach($scripts as $script){
 
-			wp_register_script( 
+			wp_enqueue_script( 
 				$script ,
 				THEME_URI.'/assets/js/wordpress-media-uploader/dist/jquery.'.$script.'.js'
 				,array('jquery', 'jquery-ui-core'),
@@ -95,7 +116,6 @@ class Cf__Upload extends Class__Custom_Field{
 				true
 			);
 
-			wp_enqueue_script($script);
 		}
 	}
 } 
