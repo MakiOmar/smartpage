9a849dbbddf24d0ff51383a1a413ed5ca50234ef
makiomar <prosentra@gmail.com>
2019-07-26 15:58:19 +0300

    rewrite checkbox class

--- functions/class/cf/checkbox.php
+++ functions/class/cf/checkbox.php
@@ -1,29 +1,91 @@
 <?php 
+/**
+ * Checkbox render class. 
+ *
+ */
 
 class Cf__Checkbox extends Class__Custom_Field{
-	public $post, $metaBox, $id, $idAttr;
-	public function __construct($post, $metaBox){
+	/**
+	 * @var int Current post ID
+	 */
+	public $post_id;
+	
+	/**
+	 * @var int Current field ID
+	 */
+	public $field_id;
+	
+	/**
+	 * @var int Current field type
+	 */
+	public $field_type;
+	
+	/**
+	 * @var int Current field label
+	 */
+	public $field_label;
+	
+	//Consructor
+	public function __construct($post_id, $field){
+		
 		parent::__construct();
-		$this->post = $post;
-		$this->metaBox= $metaBox;
-		$this->id= $this->metaBox['id'];
-		$this->idAttr= str_replace('_', '-', $this->id);
+		
+		//Set field object properties
+		$this->post_id     = $post_id;
+		
+		$this->field_type  = $field['type'];
+		
+		$this->field_id    = $field['id'];
+		
+		$this->field_label = $field['label'];
+
 	}
 	
+	/**
+	 * Render input field
+	 */
 	public function render(){
 		
-		wp_nonce_field( $this->id.'_action', $this->id.'_nonce' );
+		wp_nonce_field( $this->field_id.'_action', $this->field_id.'_nonce' );
 		
-		if(get_post_meta( $this->post->ID, $this->id, true )){
+		if(get_post_meta( $this->post_id, $this->field_id, true )){
 			
-			$checked = checked(get_post_meta( $this->post->ID, $this->id, true ), 'on', false);
+			$checked = checked(get_post_meta( $this->post_id, $this->field_id, true ), 'on', false);
 			
 		}else{
 			
 			$checked = '';
 			
 		}
+		
+		$class  = isset( $field['class'] ) && ! is_null( $field['class'] ) ? $field['class'] : 'anony-meta-field';
+		
+		$disabled  = isset( $field['disabled'] ) && ( $field['disabled'] == true ) ? " disabled" : "";
+		
+		
+		
+		
+		$html	= sprintf( 
+						'<fieldset class="anony-row" id="anony_fieldset_%1$s">', 
+						$this->field_id 
+					);
+		
+		$html	.= sprintf( 
+						'<label class="anony-label" for="anony_%1$s">%2$s</label>', 
+						$this->field_id, 
+						$this->field_label
+					);
 
-		echo '<input class="widefat" type="checkbox" id="'.$this->idAttr.'" name="'.$this->id.'"'.$checked.'/>';
+		$html  .= sprintf( 
+						'<input type="checkbox" class="widefat %1$s" id="anony_%2$s" name="anony_%2$s" %3$s %4$s/>', 
+						$class, 
+						$this->field_id,  
+						$checked,
+						$disabled
+					);
+		
+		$html	.= '</fieldset>';
+		
+		echo $html;
 	}
 } 
