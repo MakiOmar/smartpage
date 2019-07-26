f454e950bb9ecf8ce1d44baf528423ab597d1fb8
makiomar <prosentra@gmail.com>
2019-07-26 15:58:43 +0300

    rewrite upload class

--- functions/class/cf/upload.php
+++ functions/class/cf/upload.php
@@ -1,35 +1,61 @@
 <?php 
 /**
- * Upload custom field class
+ * Upload input render class. 
  *
- * @package Anonymous theme
- * @author Makiomar
- * @link http://makiomar.com
  */
 
 class Cf__Upload extends Class__Custom_Field{
-	public $post, $metaBox, $id;
-	public function __construct($post, $metaBox){
+		/**
+	 * @var int Current post ID
+	 */
+	public $post_id;
+	
+	/**
+	 * @var int Current field ID
+	 */
+	public $field_id;
+	
+	/**
+	 * @var int Current field type
+	 */
+	public $field_type;
+	
+	/**
+	 * @var int Current field label
+	 */
+	public $field_label;
+	
+	//Consructor
+	public function __construct($post_id, $field){
+		
 		parent::__construct();
-		$this->post = $post;
-		$this->metaBox= $metaBox;
-		$this->id= $this->metaBox['id'];
-		$this->idAttr= str_replace('_', '-', $this->id);
+		
+		//Set field object properties
+		$this->post_id     = $post_id;
+		
+		$this->field_type  = $field['type'];
+		
+		$this->field_id    = $field['id'];
+		
+		$this->field_label = $field['label'];
+		
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
 	}
 	
 	public function render(){
 		
-		wp_nonce_field( $this->id.'_action', $this->id.'_nonce' );?>
+		wp_nonce_field( $this->field_id.'_action', $this->field_id.'_nonce' );?>
 		
 		<div class="anony-file-upload-override-button">
 			<a href="#" class="insert-media" data-editor="anony-my-editor"><?php esc_html_e('Select your file',TEXTDOM) ;?></a>
 		</div>
 		
 		<?php
-			$file_url = get_post_meta( $this->post->ID, $this->id, true );
+			$file_url = get_post_meta( $this->post_id, $this->field_id, true );
 		
 			if(is_array($file_url)){
-				delete_post_meta( $this->post->ID, $this->id );
+				delete_post_meta( $this->post_id, $this->field_id );
 			}
 		
 			if(!empty($file_url)){
@@ -44,7 +70,7 @@ class Cf__Upload extends Class__Custom_Field{
 		<span id="anony-media-caller">
 			<div class="attachment">
 				<img width="277" height="300" alt="{{ alt }}">
-				<input type="hidden" name ="<?php echo $this->id ?>" value="{{ url }}">
+				<input type="hidden" name ="<?php echo $this->field_id ?>" value="{{ url }}">
 			</div>
 		</span>
 
@@ -52,6 +78,25 @@ class Cf__Upload extends Class__Custom_Field{
 		<div id="anony-upload-result"></div>
 		
 		<?php 
+	}
+	
+	public function enqueue_scripts(){
+		wp_enqueue_media();
+	
+		$scripts = array('wp-media-uploader.min','wp-media-uploader-custom');
+
+		foreach($scripts as $script){
+
+			wp_register_script( 
+				$script ,
+				THEME_URI.'/assets/js/wordpress-media-uploader/dist/jquery.'.$script.'.js'
+				,array('jquery', 'jquery-ui-core'),
+				filemtime(wp_normalize_path(THEME_DIR.'/assets/js/wordpress-media-uploader/dist/jquery.'.$script.'.js')),
+				true
+			);
+
+			wp_enqueue_script($script);
+		}
 	}
 } 
 
