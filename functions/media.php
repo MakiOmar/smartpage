d4e8bedd3a4b4138f66b8830c9fadf75f0bb8b56
makiomar <prosentra@gmail.com>
2019-07-26 15:57:53 +0300

    remove enqueue scripts

--- functions/media.php
+++ functions/media.php
@@ -13,21 +13,6 @@
 
 //Enqueue media scripts for uploader in anony_download post type
 add_action("admin_enqueue_scripts", function(){
-    wp_enqueue_media();
-	
-	$scripts = array('wp-media-uploader.min','wp-media-uploader-custom');
-	
-	foreach($scripts as $script){
-		
-		wp_register_script( 
-			$script ,
-			THEME_URI.'/assets/js/wordpress-media-uploader/dist/jquery.'.$script.'.js'
-			,array('jquery', 'jquery-ui-core'),
-			filemtime(wp_normalize_path(THEME_DIR.'/assets/js/wordpress-media-uploader/dist/jquery.'.$script.'.js')),
-			true
-		);
-		
-		wp_enqueue_script($script);
-	}
+  
 });
 ?>
