c9a7ca5f7719356374a1a0455113897f51c27626
makiomar <prosentra@gmail.com>
2019-07-28 14:27:00 +0300

    Add select input field class

---
+++ functions/class/cf/select.php
@@ -0,0 +1,127 @@
+<?php 
+/**
+ * Select input render class. 
+ *
+ */
+
+class Cf__Select extends Class__Custom_Field{
+	/**
+	 * @var int Current post ID
+	 */
+	public $post_id;
+	
+	/**
+	 * @var array Current field data
+	 */
+	public $field;
+	
+	//Consructor
+	public function __construct($post_id, $field){
+		
+		parent::__construct();
+		
+		//Set field object properties
+		$this->post_id = $post_id;
+		
+		$this->field   = $field;
+
+
+	}
+	
+	/**
+	 * Render input field
+	 */
+	public function render(){
+		
+		wp_nonce_field( $this->field['id'].'_action', $this->field['id'].'_nonce' );
+		
+		$value = '';
+		
+		if(isset($this->field['default'])){
+			
+			$value = $this->field['default'];
+			
+		}
+		
+		$single = true;
+		
+		if (isset( $this->field['multiple'] ) && ( $this->field['multiple'] == true )) $single = false;
+		
+		if(get_post_meta( $this->post_id, $this->field['id'], $single ) ){
+			
+			if($single){
+
+				$value = get_post_meta( $this->post_id, $this->field['id'], $single );
+				
+			}else{
+				
+				$value = get_post_meta( $this->post_id, $this->field['id'], $single )[0];
+				
+			}
+			
+			
+		}
+				
+		$class  = isset( $this->field['class'] ) && ! is_null( $this->field['class'] ) ? $this->field['class'] : 'anony-meta-field';
+		
+		$multiple  = isset( $this->field['multiple'] ) && ( $this->field['multiple'] == true ) ? " multiple " : "";
+
+		$disabled  = isset( $this->field['disabled'] ) && ( $this->field['disabled'] == true ) ? " disabled" : "";
+		
+		$autocomplete  = (isset( $this->field['auto-complete']) && $this->field['auto-complete'] == 'on') ? 'autocomplete="on"' : 'autocomplete="off"';
+				
+		$name 	   = isset( $this->field['multiple'] ) && ( $this->field['multiple'] == true ) ? $this->field['id'] . '[]' : $this->field['id'];
+
+		$html	= sprintf( 
+					'<fieldset class="anony-row" id="anony_fieldset_%1$s">', 
+					$this->field['id'] 
+				);
+		
+        $html	.= sprintf( 
+					'<label class="anony-label" for="%1$s">%2$s</label>', 
+					$this->field['id'], 
+					$this->field['label']
+				);
+		
+        $html   .= sprintf( 
+					'<select class="%1$s" name="%2$s" id="'.$this->field['id'].'" %3$s %4$s %5$s>', 
+					$class, 
+					$name, 
+					$disabled, 
+					$multiple,
+					$autocomplete
+				);
+		
+        if( $multiple == '' ) :
+
+        foreach ( $this->field['options'] as $key => $label ) {
+            $html .= sprintf( 
+						'<option value="%1$s"%2$s>%3$s</option>',
+						$key, 
+						selected( $value, $key, false ), 
+						$label 
+					);
+        }
+
+        else:
+        foreach ( $this->field['options'] as $key => $label ) {
+			
+        	$selected = is_array($value) && in_array( $key, $value ) && $key != '' ? ' selected' : '';
+			
+            $html .= sprintf( 
+						'<option value="%1$s"%2$s>%3$s</option>', 
+						$key, 
+						$selected, 
+						$label 
+					);
+        }
+
+        endif;
+
+        $html   .= '</select>';
+        $html	.= '</fieldset>';
+		
+		echo $html;
+
+	}
+} 
