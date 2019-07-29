ad63cb8cc35d187e1d4d5e63ecc1ae403f6b860b
makiomar <prosentra@gmail.com>
2019-07-29 14:09:47 +0300

    fix options navigation

--- functions/options/js/options.js
+++ functions/options/js/options.js
@@ -1,52 +1,51 @@
-var SettingsView = Backbone.View.extend({
-	el: "#anony-options-nav",
-	events: {
-		'click .nav-toggle': function(e){
-			"use strict";
-				e.preventDefault();
-
-				var targetID = e.target.id.split('-')[0];
-				
-				$('.anony-dropdown').hide();
-				if($('#' + targetID + '-dropdown')){
-					$('#' + targetID + '-dropdown').toggle();
-				}
-					
-
-		}
-	},
-});
-
- var settingsView = new SettingsView();
-
-SettingsRouter = Backbone.Router.extend({
-        routes: {
-            'anony-section/:section': function(section) {
-				"use strict";
-				var target = $('a[href="#anony-section/' + section + '"]'),
-					content = $('#anony-' + section + '-section-group');
-				$('.anony-nav-link').parent().removeClass('active');
-				$('.anony-section-group').removeClass('anony-show-section');
-				target.parent().addClass('active');
-				content.addClass('anony-show-section');
-			}
-        },
-        
-    });
-
-var settingsRouter = new SettingsRouter();
-
-Backbone.history.start();
-
 jQuery(document).ready(function($){
-	"use strict";
+	var SettingsView = Backbone.View.extend({
+		el: "#anony-options-nav",
+		events: {
+			'click .nav-toggle': function(e){
+				"use strict";
+					e.preventDefault();
 
+					var targetID = e.target.id.split('-')[0];
+
+					$('.anony-dropdown').hide();
+					if($('#' + targetID + '-dropdown')){
+						$('#' + targetID + '-dropdown').toggle();
+					}
+
+
+			}
+		},
+	});
+
+	var settingsView = new SettingsView();
+
+	var SettingsRouter = Backbone.Router.extend({
+			routes: {
+				'anony-section/:section': function(section) {
+					"use strict";
+					var target = $('a[href="#anony-section/' + section + '"]'),
+						content = $('#anony-' + section + '-section-group');
+					$('.anony-nav-link').parent().removeClass('active');
+					$('.anony-section-group').removeClass('anony-show-section');
+					target.parent().addClass('active');
+					content.addClass('anony-show-section');
+				}
+			},
+
+		});
+
+	var settingsRouter = new SettingsRouter();
+
+	Backbone.history.start();
+	
 	$( "input[type=checkbox]" ).change(function(){
 		$('.' + this.id + '_').toggle();
 	});
+	
+	
 	$( "input[type=radio]" ).change(function(){
 		$('.'+ this.className +'_').hide();
 		$('.' + this.value ).toggle();
 	});
-	
 });
