dd54e9ce7e2685c85f86883fc832a6ab1cdeab33
makiomar <prosentra@gmail.com>
2019-08-02 08:25:40 +0300

    Refactor page select

--- options/fields/pages_select/pages_select.php
+++ options/fields/pages_select/pages_select.php
@@ -17,24 +17,40 @@ class ANONY_optf__Pages_select extends ANONY__Theme_Settings{
 	}
 	
 	/**
-	 * Color field render Function.
-	 * **Description: ** Echoes out the field markup.
+	 * Pages select field render Function.
 	 *
 	 * @return void
 	 */
 	function render( $meta = false ){
 		
 		$class = ( isset( $this->field['class']) ) ? 'class="'.$this->field['class'].'" ' : '';
+
 		$name = ( ! $meta ) ? ( $this->args['opt_name'].'['.$this->field['id'].']' ) : $this->field['id'];
 		
-		$pages = get_pages('sort_column=post_title&hierarchical=0'); 
-		echo '<select name="'. $name .'" '.$class.'rows="6" >';	
-			echo '<option value="">'. esc_html__('-- select --','anony-opts') .'</option>';
+		$pages = get_pages('sort_column=post_title&hierarchical=0');
+
+		$html = sprintf(
+					'<select name="%1$s" %2$srows="6" >', 
+					$name, 
+					$class
+				);
+
+			$html .= sprintf('<option value="">%1$s</option>', esc_html__('-- select --','anony-opts'));
+
 			foreach ( $pages as $page ) {
-				echo '<option value="'.$page->ID.'"'.selected($this->value, $page->ID, false).'>'.$page->post_title.'</option>';
+
+				$html .= sprintf(
+							'<option value="%1$s"%2$s>%3$s</option>', 
+							$page->ID, 
+							selected($this->value, $page->ID, false), 
+							$page->post_title
+						);
 			}
-		echo '</select>';
-		echo (isset($this->field['desc']) && !empty($this->field['desc']))?' <div class="description">'.$this->field['desc'].'</div>':'';
+		$html .= '</select>';
+
+		$html .= (isset($this->field['desc']) && !empty($this->field['desc'])) ? ' <div class="description">'.$this->field['desc'].'</div>' : '';
+
+		echo $html;
 		
 	}
 	
