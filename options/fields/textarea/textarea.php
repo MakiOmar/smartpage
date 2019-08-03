b042553a6ecc59b2d9b5b0737b8969f2535b98ff
makiomar <prosentra@gmail.com>
2019-08-03 14:45:34 +0300

    Refactor options textarea field

--- options/fields/textarea/textarea.php
+++ options/fields/textarea/textarea.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Textarea field class
+ *
+ * @package Anonymous theme
+ * @author Makiomar
+ * @link http://makiomar.com
+ */
+
 class ANONY_optf__Textarea extends ANONY__Theme_Settings{	
 	
 	/**
@@ -17,18 +25,24 @@ class ANONY_optf__Textarea extends ANONY__Theme_Settings{
 	}
 	
 	/**
-	 * Color field render Function.
-	 * **Description: ** Echoes out the field markup.
+	 * Textarea field render Function.
 	 *
 	 * @return void
 	 */
 	function render( $meta = false ){
 		
 		$class = ( isset($this->field['class']) ) ? $this->field['class'] : 'large-text';
+
 		$name = ( ! $meta ) ? ( $this->args['opt_name'].'['.$this->field['id'].']' ) : $this->field['id'];
 		
-		echo '<textarea name="'. $name .'" class="'.$class.'" rows="6" >'.esc_textarea($this->value).'</textarea>';
-		echo (isset($this->field['desc']) && !empty($this->field['desc']))?'<br/><div class="description">'.$this->field['desc'].'</div>':'';
+		$html = sprintf(
+					'<textarea name="%1$s" class="%2$s" rows="6" >%3$s</textarea>', 
+					$name, 
+					$class, 
+					esc_textarea($this->value)
+				);
+
+		$html .= (isset($this->field['desc']) && !empty($this->field['desc']))?'<br/><div class="description">'.$this->field['desc'].'</div>':'';
 		
 	}
 	
