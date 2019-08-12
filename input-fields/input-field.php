9e92bdbd21da698fd7eba930451cfd09c11e27b9
makiomar <prosentra@gmail.com>
2019-08-13 01:51:32 +0300

    rewite input field class

--- input-fields/input-field.php
+++ input-fields/input-field.php
@@ -47,6 +47,11 @@ if( ! class_exists( 'ANONY__Input_Field' )){
 		public $value;
 
 		/**
+		 * @var mixed Default field value
+		 */
+		public $default;
+
+		/**
 		 * Inpud field constructor That decides field context
 		 * @param array    $field    An array of field's data
 		 * @param string   $context  The context of where the field is used
@@ -62,33 +67,54 @@ if( ! class_exists( 'ANONY__Input_Field' )){
 
 			$this->context = $context;
 
+			$this->default = isset($this->field['default']) ? $this->field['default'] : '';
+
 			$this->set_field_data();
 
 			$this->select_field();
-
-			$this->field_init();
 		}
 
 		public function set_field_data(){
 			switch ($this->context) {
 				case 'option':
-					$this->input_name = ANONY_OPTIONS.'['.$this->field['id'].']';
-
-					$fieldID      = $this->field['id'];
-					
-					$fieldDefault = isset($this->field['default']) ? $this->field['default'] : '';
-
-					$this->value = (isset($this->options->$fieldID))? $this->options->$fieldID : $fieldDefault;
-
+						$this->opt_field_data();
 					break;
 
 				case 'meta':
-					$this->input_name = $this->field['id'];
+						$this->meta_field_data();
 					break;
 				
 				default:
 					$this->input_name = $this->field['id'];
 					break;
+			}
+		}
+
+		/**
+		 * Set options field data
+		 */
+		public function opt_field_data(){
+			$this->input_name = ANONY_OPTIONS.'['.$this->field['id'].']';
+
+			$fieldID      = $this->field['id'];
+
+			$this->value = (isset($this->options->$fieldID))? $this->options->$fieldID : $this->default;
+		}
+
+		/**
+		 * Set metabox field data
+		 */
+		public function meta_field_data(){
+			$this->input_name = $this->field['id'];
+
+			if(isset($this->field['options']) && is_array($this->field['options']))
+			{
+				$meta = get_post_meta( $this->post_id, $this->field['id'], false );
+				$this->value = ($meta != '') ? $meta[0] : $this->default;
+			}else
+			{
+				$meta = get_post_meta( $this->post_id, $this->field['id'], true);
+				$this->value = ($meta  != '') ? $meta : $this->default;	
 			}
 		}
 
@@ -121,14 +147,18 @@ if( ! class_exists( 'ANONY__Input_Field' )){
 
 			if(!is_null($this->field_class) && class_exists($this->field_class))
 			{
-				$field_class = $this->field_class;
-				if(class_exists($field_class)){
-					$field = new $field_class($this->field, $this);
+				
+				if(class_exists($this->field_class)){
+
+					$field_class = $this->field_class;
+
+					$field = new $field_class($this);
+
+					if($this->context == 'meta'){
+						wp_nonce_field( $this->field['id'].'_action', $this->field['id'].'_nonce' );
+					}
 					$field->render();
 				}
-				
-				
-				//
 
 			}
 		}
