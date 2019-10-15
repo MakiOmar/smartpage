f068c192b57ed63c9ac9eaa65743cf8a955421ae
makiomar <mo7amed.maki@gmail.com>
2019-10-15 09:19:47 +0300

    Call user control menu
    If user control plugin is active, it's user menu will be displayed otherwise the themes menu

--- templates/top-header.php
+++ templates/top-header.php
@@ -14,6 +14,19 @@ $socials_follow = array(
 ?>
 <div id="anony-top-header-wrapper">
 	<nav id="anony-follow-contact">
+
+		 <?php
+			/** 
+			 * The ANONY_MENU constant is defined in User control plugin.
+			 * It contains user menu slug, defined by the plugin
+			 */
+		 	if(defined('ANONY_MENU') && wp_get_nav_menu_object( ANONY_MENU )){
+		 		echo wp_nav_menu(['menu' => ANONY_MENU , 'fallback_cb' => false]);
+		 	}else{
+		 		echo anony_navigation('anony-user-menu');
+		 	}
+		 ?>
+
 	  <ul id="anony-follow">
 		<?php 
 			foreach($socials_follow as $social_follow => $url){
@@ -27,8 +40,6 @@ $socials_follow = array(
 		<li><a href="tel:<?php echo $anonyOptions->phone ?>" class="phone-call"><i class="fa fa-phone"></i></a></li>
 		
 		<li><a href="mailto:<?php echo $anonyOptions->email ?>" class="email-me"><i class="fa fa-envelope"></i></a></li>
-		
-		<a href="<?php echo wp_logout_url(); ?>">Logout</a>
 		
 	  </ul>
 
