5f0f131061446f0288a55fdca750970774f36723
makiomar <mo7amed.maki@gmail.com>
2020-02-03 12:01:13 +0300

    Load singles conditionally

--- single.php
+++ single.php
@@ -1,7 +1,14 @@
 <?php get_header();?>
   <div class="anony-grid">
   	<div class="anony-grid-row anony-grid-col">
-       <?php get_template_part( 'template-parts/single-post/'.get_post_type() );?>
+       <?php
+       $sites = ['dam', 'reservoir'];
+       if(in_array(get_post_type(), $sites)){
+       		get_template_part( 'template-parts/single-post/site' );
+       }else{
+       		get_template_part( 'template-parts/single-post/'.get_post_type() );
+       }
+       ?>
 	</div>
   </div>
  <?php get_footer();?>
