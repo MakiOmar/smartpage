6aa5c0c4d4d5b023d989b025d0158f03b59e7d83
makiomar <mo7amed.maki@gmail.com>
2020-03-01 21:03:13 +0300

    insert multi input on click

--- input-fields/multi-value/field_multi_value.js
+++ input-fields/multi-value/field_multi_value.js
@@ -2,6 +2,30 @@ jQuery(document).ready(function($){
 	'use strict';
 
 	$('.multi-value-btn').on('click', function(){
+		var thisBtn = $(this);
+
+		var targetId = thisBtn.attr('rel-id');
+
+		//Target element to be duplicated
+		var targetClass = thisBtn.attr('rel-class');
+
+		//set new count
+		$('#' + targetId + '-counter').val( parseInt($('#' + targetId + '-counter').val()) + 1 );
+
+		//Counter for duplication
+		var targetCounter = $('#' + targetId + '-counter').val();
+		
+		//Template id
+		var defaultId = targetId + '-default';
+
+
+		var dafaultHtml = $('#' + defaultId).html();
+
+		var x = dafaultHtml.replace(/(\w+)\[(\d+)\]/g, function(a, b, c){
+		    return b + '[' + targetCounter + ']';
+		});
+
+		$('.'+targetClass).append(x);
 		
 	});
 });
