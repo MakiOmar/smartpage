114683859a0a25ba0439f966d480f154627e4a1e
makiomar <mo7amed.maki@gmail.com>
2020-03-05 18:19:29 +0300

    add contract single view
    omdb site

---
+++ template-parts/single-post/contract.php
@@ -0,0 +1,32 @@
+<?php 
+	global $anonyOptions;
+	anony_get_correct_sidebar();
+
+	if(has_action('post_ad')){
+		do_action('post_ad');
+	}
+?>
+<div class="anony-grid-col <?php echo ($anonyOptions->single_sidebar == '1') ? 'anony-grid-col-sm-7' : 'anony-grid-col-sm-9-5' ?>">
+
+	<?php anony_breadcrumbs()?>
+	<?php 
+		if ( have_posts() ) {
+			while (have_posts() ) { 
+				the_post();
+				$post_id = get_the_ID();
+				?>
+				<div class="anony-grid-col anony-post-contents anony-single_post">
+					<div class="anony-post-info">
+						<div class="anony-single-text">
+							
+							<?php the_content();?>
+						</div>
+					</div>
+
+				 </div>
+		  <?php } 
+		} 
+	?>
+</div>
+
+<?php anony_get_correct_sidebar();?>
