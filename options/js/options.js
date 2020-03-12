227aa05671884e951f99d2460865afa5f8c7d5b8
makiomar <mo7amed.maki@gmail.com>
2020-03-12 14:17:37 +0300

    refactor backbone

--- options/js/options.js
+++ options/js/options.js
@@ -1,6 +1,44 @@
+/**
+ * check cookie and get value
+ */
+function anonyGetCookie( name ) {
+    var dc,
+        prefix,
+        begin,
+        end;
+
+    dc = document.cookie;
+    prefix = name + "=";
+    begin = dc.indexOf("; " + prefix);
+    end = dc.length; // default to end of the string
+
+    // found, and not in first position
+    if (begin !== -1) {
+        // exclude the "; "
+        begin += 2;
+    } else {
+        //see if cookie is in first position
+        begin = dc.indexOf(prefix);
+        // not found at all or found as a portion of another cookie name
+        if (begin === -1 || begin !== 0 ) return null;
+    } 
+
+    // if we find a ";" somewhere after the prefix position then "end" is that position,
+    // otherwise it defaults to the end of the string
+    if (dc.indexOf(";", begin) !== -1) {
+        end = dc.indexOf(";", begin);
+    }
+
+    return decodeURI(dc.substring(begin + prefix.length, end) ).replace(/\"/g, ''); 
+}
+
+
 jQuery(document).ready(function($){
 	var SettingsView = Backbone.View.extend({
-		el: "#anony-options-nav",
+		el: "#options-wrap",
+		initialize: function(){
+			this.showFirstSection();
+		},
 		events: {
 			'click .nav-toggle': function(e){
 				"use strict";
@@ -14,6 +52,28 @@ jQuery(document).ready(function($){
 					}
 
 
+			},
+
+			'click #submit': function(e){
+				var currentFragment = Backbone.history.getFragment();
+				if(currentFragment !== "undefined" || currentFragment !== ''){
+					document.cookie = "currentFragment=" + currentFragment;
+				}
+			},
+
+			'change input[type="checkbox"]': function(e){
+				$('.' + e.target.id + '_').toggle();
+			},
+
+			'change input[type="radio"]': function(e){
+				$('.'+ e.target.className +'_').hide();
+				$('.' + e.target.value ).toggle();
+			}
+		},
+
+		showFirstSection: function(){
+			if($('.anony-show-section').length == 0){
+				$('.anony-section-group:first').addClass('anony-show-section');
 			}
 		},
 	});
@@ -22,7 +82,7 @@ jQuery(document).ready(function($){
 
 	var SettingsRouter = Backbone.Router.extend({
 			routes: {
-				'anony-section/:section': function(section) {
+				'anony-section/:section': function(section){
 					"use strict";
 					var target = $('a[href="#anony-section/' + section + '"]'),
 						content = $('#anony_' + section + '_section');
@@ -38,18 +98,17 @@ jQuery(document).ready(function($){
 	var settingsRouter = new SettingsRouter();
 
 	Backbone.history.start();
+
 	
-	$( "input[type=checkbox]" ).change(function(){
-		$('.' + this.id + '_').toggle();
-	});
-	
-	
-	$( "input[type=radio]" ).change(function(){
-		$('.'+ this.className +'_').hide();
-		$('.' + this.value ).toggle();
-	});
-	if($('.anony-show-section').length == 0){
-		$('.anony-section-group:first').addClass('anony-show-section');
+	/*--------activate last tab was before form submit------------*/
+
+	var lastActiveTab  = anonyGetCookie('currentFragment');
+
+	if(lastActiveTab !== null){
+		var targetID = lastActiveTab.split('/')[1];
+		$('#' + targetID).closest('.anony-dropdown').show();
+		settingsRouter.navigate(lastActiveTab, {trigger: true});
+		document.cookie =  'currentFragment=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 	}
 	
 });
