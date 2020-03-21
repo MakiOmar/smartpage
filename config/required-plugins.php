611049a6ab7c3c3e9d887a7e8eb46f17a8b5ba8a
makiomar <mo7amed.maki@gmail.com>
2020-03-21 18:45:09 +0300

    Stop loading theme if plugin AnonyEngine is not active

--- config/required-plugins.php
+++ config/required-plugins.php
@@ -1,9 +1,16 @@
 <?php
+add_action( 'init', function(){
+	if (!defined('ANOENGINE') && !is_admin()) {
+		wp_die( 'Please activate/install AnonyEngine plugin, for User AnonyEngine theme can work properly... <a href="'.esc_url(admin_url()).'">Go to admin</a>' );
+		/*wp_redirect( admin_url(  ) );
+		exit();*/
+	}
+} );
 /**
  * Display a notification if one of required plugins is not activated/installed
  */
 add_action( 'admin_notices', function() {
-	if (!class_exists('ANONY_HELP') ) {
+	if (!defined('ANOENGINE')) {
 	    ?>
 	    <div class="notice notice-error is-dismissible">
 	        <p><?php esc_html_e( 'Please activate/install AnonyEngine plugin, for User AnonyEngine theme can work properly' ); ?></p>
