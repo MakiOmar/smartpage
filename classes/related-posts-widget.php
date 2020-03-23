998eb537cbd0ff949e009a044489ba50593e9df2
makiomar <mo7amed.maki@gmail.com>
2020-03-23 15:18:28 +0300

    fix undefined variable

--- classes/related-posts-widget.php
+++ classes/related-posts-widget.php
@@ -22,63 +22,63 @@ if ( ! class_exists( 'ANONY_Related_Posts_Widget' ) ) {
 		public function widget($parms, $instance){
 			if ( is_single() ) {
 				
-			extract($parms);
+				extract($parms);
+				
+				extract($instance);
+				
+				if(empty($title)) $title = esc_html__('Related posts', ANONY_TEXTDOM);
+		
+				echo $before_widget;
+				
+				echo $before_title.$title.$after_title;
+				
+				$p_ID = get_The_ID();
+
+				$cats = wp_get_post_categories($p_ID);
+
+				if (!is_wp_error($cats) && !empty($cats)) {
+
+					$first_cat = $cats[0];
+
+					$catpostcount = anony_cat_posts_count($first_cat);?>
+
+					<ul class="anony-grid-col">
+					<?php
+					if ($catpostcount >= 1){
+							$args=array(
+							  'cat' => $first_cat,
+							  'post__not_in' => array($p_ID),
+							  'order' => 'ASC',
+							  'posts_per_page'=>-1,
+							);
+
+							$same_cat_posts = new WP_Query($args);
+
+							if( $same_cat_posts->have_posts() ) {?>
+
+
+
+									<?php 
+									/*echo '<h3 class="widgeted_title"><a href="'.get_category_link( $first_cat ).'">'.get_cat_name( $first_cat ).'</a></h3>';*/
+									while ($same_cat_posts->have_posts()) : $same_cat_posts->the_post(); ?>
+
+										<li class="anony-same_cat_post"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title();?></a></li>
+
+								   <?php 
+										endwhile;
+										wp_reset_postdata();  // Restore global post data stomped by the_post().
+									?>
+
+
+							<?php } //if ($same_cat_posts)
+					}else{
+						echo '<p>'. esc_html__('No more posts in this category') . '</p>';
+					}
+				} //if ($cats)?>
+				</ul>
+		<?php echo $after_widget; }
 			
-			extract($instance);
 			
-			if(empty($title)) $title = esc_html__('Related posts', ANONY_TEXTDOM);
-	
-			echo $before_widget;
-			
-			echo $before_title.$title.$after_title;
-			
-			$p_ID = get_The_ID();
-
-			$cats = wp_get_post_categories($p_ID);
-
-			if (!is_wp_error($cats) && !empty($cats)) {
-
-				$first_cat = $cats[0];
-
-				$catpostcount = anony_cat_posts_count($first_cat);?>
-
-				<ul class="anony-grid-col">
-				<?php
-				if ($catpostcount >= 1){
-						$args=array(
-						  'cat' => $first_cat,
-						  'post__not_in' => array($p_ID),
-						  'order' => 'ASC',
-						  'posts_per_page'=>-1,
-						);
-
-						$same_cat_posts = new WP_Query($args);
-
-						if( $same_cat_posts->have_posts() ) {?>
-
-
-
-								<?php 
-								/*echo '<h3 class="widgeted_title"><a href="'.get_category_link( $first_cat ).'">'.get_cat_name( $first_cat ).'</a></h3>';*/
-								while ($same_cat_posts->have_posts()) : $same_cat_posts->the_post(); ?>
-
-									<li class="anony-same_cat_post"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title();?></a></li>
-
-							   <?php 
-									endwhile;
-									wp_reset_postdata();  // Restore global post data stomped by the_post().
-								?>
-
-
-						<?php } //if ($same_cat_posts)
-				}else{
-					echo '<p>'. esc_html__('No more posts in this category') . '</p>';
-				}
-			} //if ($cats)?>
-			</ul>
-		<?php }
-			
-			echo $after_widget;
 		}
 	}
 }
