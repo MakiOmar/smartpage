9852aca39a536a67ee6797a0e680eb2e7aec179a
makiomar <mo7amed.maki@gmail.com>
2020-04-05 21:27:24 +0300

    refactor comments

--- comments.php
+++ comments.php
@@ -1,15 +1,39 @@
 <?php
 if ( post_password_required() )
 	return;
+$data = [
+	'have_comments'   => have_comments(),
+	'comments_number' => sprintf( 
+							_nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', ANONY_TEXTDOM ), 
+							number_format_i18n( get_comments_number() ) 
+						),
+
+	'comments' => wp_list_comments( 
+		[ 
+			'echo'=> false ,
+			'avatar_size'=>'64',
+			'format'=>'xhtml',
+			'style' => 'div'
+		] 
+	),
+
+	'comments_open' => comments_open(),
+	'comments_off_text' => esc_html__( 'Comments off' , ANONY_TEXTDOM ),
+
+];
+
+extract($data);
 ?>
 
 <div id="anony-comments" class="anony-comments-area anony-grid-col">
-	<?php if ( have_comments() ) : ?>
+	<?php if ( $have_comments ) : ?>
+
 		<h2 class="anony-comments-title">
-			<?php printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', ANONY_TEXTDOM ), number_format_i18n( get_comments_number() ) );?>
+			<?= $comments_number ;?>
 		</h2>
+
 		<div class="anony-commentlist">
-			<?php wp_list_comments( array( 'avatar_size'=>'64','format'=>'xhtml','style' => 'div' ) ); ?>
+			<?= $comments ?>
 		</div><!-- .anony-commentlist -->
 
 
@@ -17,9 +41,9 @@ if ( post_password_required() )
 		/* If there are no comments and comments are closed, let's leave a note.
 		 * But we only want the note on posts and pages that had comments in the first place.
 		 */
-		if ( ! comments_open() && get_comments_number() ) : ?>
-		<p class="nocomments"><?php esc_html_e( 'Comments off' , ANONY_TEXTDOM ); ?></p>
-		<?php endif; ?>
+		if ( $comments_open  ) : ?>
+		<p class="nocomments"><?= $comments_off_text ?></p>
+		<?php endif ?>
 
 	<?php endif; 
 	comment_form(array( 
