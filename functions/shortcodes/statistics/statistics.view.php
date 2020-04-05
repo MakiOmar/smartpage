7a19ccfbc010b2285f18b7c43aa8aa08b892f46d
makiomar <mo7amed.maki@gmail.com>
2020-04-05 21:19:50 +0300

    add shortcodes folder

---
+++ functions/shortcodes/statistics/statistics.view.php
@@ -0,0 +1,9 @@
+<div id="<?= $id ?>" class="anony-counter-wrapper">
+	<div class="anony-counter-icon"><i class="fa fa-<?= $icon ?> fa-4x"></i></div>
+	
+	<div class= "anony-counter">
+		<div class= "anony-counter-inner counter"><?= $count ?></div>
+	</div>
+	
+	<h3 class="anony-counter-title"><?= $title ?></h3>
+</div>
