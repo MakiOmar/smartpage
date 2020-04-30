8b1a19a164aea54e04b9e6b6d8d6c34589be7049
makiomar <mo7amed.maki@gmail.com>
2020-05-01 01:00:27 +0300

    separate single view

--- single.php
+++ single.php
@@ -1,14 +1,7 @@
-<?php get_header();?>
-  <div class="anony-grid">
-  	<div class="anony-grid-row anony-grid-col">
-       <?php
-       $sites = ['dam', 'reservoir'];
-       if(in_array(get_post_type(), $sites)){
-       		get_template_part( 'template-parts/single-post/site' );
-       }else{
-       		get_template_part( 'template-parts/single-post/'.get_post_type() );
-       }
-       ?>
-	</div>
-  </div>
- <?php get_footer();?>
+<?php 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+anony_set_post_views(get_the_ID());
+get_template_part('templates/single.view');
+?>
---
+++ templates/single.view.php
@@ -0,0 +1,16 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+get_header(); ?>
+
+<div class="anony-grid">
+	<div class="anony-grid-row anony-grid-col">
+        <?php
+            get_template_part( 'template-parts/single-post/'.get_post_type() );
+            anony_get_correct_sidebar();
+        ?>
+    </div>
+</div>
+<?php get_footer();?>
