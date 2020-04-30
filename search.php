1f3fc035dd5eae0141cd42936aa9bcec384f8e58
makiomar <mo7amed.maki@gmail.com>
2020-05-01 00:19:15 +0300

    separate search view

--- search.php
+++ search.php
@@ -1,36 +1,29 @@
 <?php 
-get_header();
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+$data = [];
+
+if ( have_posts() ) {
+	while (have_posts() ) { the_post();
+		
+		$data[] = anony_common_post_data();
+
+	}
+	
+
+	$prev_text = is_rtl() ? 'right' : 'left';
+	
+	$next_text = is_rtl() ? 'left'  : 'right';
+	
+	$pagination = get_the_posts_pagination( array(
+			'type' => 'list',
+			'prev_text' => '<i class="fa fa-arrow-'.$prev_text.'"></i>',
+			'next_text' => '<i class="fa fa-arrow-'.$next_text.'"></i>',
+			'screen_reader_text'=>' ',
+
+		) );
+}
+include(locate_template( 'templates/index.view.php', false, false ));
 ?>
-  <div class="anony-grid">
-  	<div class="anony-grid-row">
-      <div class="anony-grid-col">
-        <div class="anony-grid-col-sm-9-5">
-        <?php 
-
-			if ( have_posts() ) {
-				while (have_posts() ) { the_post();
-				get_template_part('templates/blog-post') ;
-
-				} 
-			}
-			if(is_rtl()){
-				$prev_text = '<i class="fa fa-arrow-right"></i>';
-				$next_text = '<i class="fa fa-arrow-left"></i>';
-			}else{
-				$prev_text = '<i class="fa fa-arrow-left"></i>';
-				$next_text = '<i class="fa fa-arrow-right"></i>';
-			}
-        	the_posts_pagination( array(
-					'type' => 'list',
-					'prev_text' => $prev_text,
-					'next_text' => $next_text,
-					'screen_reader_text'=>' ',
-
-				) );
-		?>	
-        </div>
-       <?php get_sidebar();?>
-	  </div>
-	</div>
-  </div>
- <?php get_footer();?>
