a1ee2d96bae88991548f5cda15ae9f793efce5e6
makiomar <mo7amed.maki@gmail.com>
2020-05-01 19:46:15 +0300

    Fix slider conditions

--- front-page.php
+++ front-page.php
@@ -12,19 +12,15 @@ $homeSlider = anonyGetOpt($anonyOptions, 'home_slider');
 
 $chooseSlider = esc_html__("You didn't choose a slider, Please select one from theme options page");
 
-if(anonyGetOpt($anonyOptions, 'slider') ){
 	
-	if(function_exists('putRevSlider') && $homeSlider == '1'){
+if(function_exists('putRevSlider') && $homeSlider == '1'){
+	
+	if($revSlider != ''){
 		
-		if($revSlider && $revSlider != '0'){
-			
-			$slider = ANONY_HELP::obGet('putRevSlider', [$revSlider]);
-			
-		}
+		$slider = ANONY_HELP::obGet('putRevSlider', [$revSlider]);
+		
 	}
-	
 }
-
 	
 include(locate_template( 'templates/front-page.view.php', false, false ));
 ?>
