4410ed51a13324f1bb55fd7db762e1e5ebc84957
makiomar <mo7amed.maki@gmail.com>
2020-05-01 19:45:40 +0300

    Rewrite popular posts code

--- controllers/popular.php
+++ controllers/popular.php
@@ -2,18 +2,36 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
-	$tcp= new ANONY_Generate_Posts_View(
-						array(
-							//NO. of posts you want to show 
-							'posts_per_page' => 4,
-							'meta_key' => 'post_views_count',
-							// Order according to numbers not name 
-							'orderby'=> 'meta_value_num',
-							'order' => 'DESC', 
-						),
-						'popular',
-						true
-					);
-	$tcp->postsView();
 
+$query = new WP_Query(
+		[
+			//NO. of posts you want to show 
+			'posts_per_page' => 4,
+			'meta_key' => 'post_views_count',
+			// Order according to numbers not name 
+			'orderby'=> 'meta_value_num',
+			'order' => 'DESC', 
+		]
+	);
+
+$data = [];
+
+if ($query->have_posts()) {
+	
+	while($query->have_posts()) {
+		$query->the_post();
+		
+		$temp = anony_common_post_data();
+		
+		$temp['thumb_img'] = get_the_post_thumbnail( get_the_ID(),array('150','150'),array( 'class' => 'post-thumb'));
+		
+		$data[] = $temp;
+		
+	}
+	
+	wp_reset_postdata();
+}
+if (empty($data)) return;
+
+include(locate_template( 'templates/popular.view.php', false, false ));
 ?>
