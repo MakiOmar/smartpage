466e88a0bec8b4d3591d86ab7d1d00f5526578db
makiomar <mo7amed.maki@gmail.com>
2020-05-01 19:44:32 +0300

    Add popular posts view

---
+++ templates/popular.view.php
@@ -0,0 +1,37 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+?>
+<div id="anony-popular" class ="anony-tab_content">
+			
+	<?php foreach ($data as $p) : extract($p)?>
+		
+		<div id="anony-popular-<?= $id ?>" class="anony-posts-list-wrapper">
+		
+			<?php if($thumb){?>
+			
+				<div class="anony-posts-list-thumb">
+					<?= $thumb_img  ?>	
+				</div>
+				
+			<?php } ?>
+			
+				<div class="anony-posts-list">
+				
+					<div>
+						  <h3><a href="<?= $permalink ?>"><?= $title ?></a></h3> 
+					</div>
+					
+					<div class="anony-metadata"><i class="fa fa-calendar"></i>&nbsp;<?= $date ?></div>
+					<div class="anony-metadata"><i class="fa fa-eye"></i>&nbsp;<?= $views ?></div>
+					
+					<?php get_template_part('controllers/rate') ?>
+					
+				</div>
+				
+		</div>	
+
+	<?php endforeach ?>
+		
+</div>
