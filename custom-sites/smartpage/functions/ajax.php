6eb6f4dc6c2da930763248fbb1a76a0d9a318c66
makiomar <mo7amed.maki@gmail.com>
2020-05-02 21:48:33 +0300

    Fix downlooad times ajax

--- custom-sites/smartpage/functions/ajax.php
+++ custom-sites/smartpage/functions/ajax.php
@@ -9,23 +9,32 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @author Makiomar
  * @link http://makiomar.com
  */
-function smpg_download_ajax() {
+function anony_download_times() {
 	//Add and update downloads counter
 	if(isset($_POST['download_id']) && !empty($_POST['download_id'])){
-		$download_meta = get_post_meta($_POST['download_id'], 'smpg_download',true);
-
+		
+		$post_id = intval($_POST['download_id']);
+		
+		$download_meta = get_post_meta($post_id, 'anony_download',true);
+		
+		//If empty this means something is wrong
 		if(empty($download_meta)) return;
 
-		if(!isset($download_meta['download_times']) || empty($download_meta['download_times'])){
-			$download_meta['download_times'] = 1;
-			update_post_meta($_POST['download_id'], 'smpg_download', $download_meta);
+		if(!isset($download_meta['anony_download']['download_times']) || empty($download_meta['anony_download']['download_times'])){
+			
+			$download_meta['anony_download']['download_times'] = 1;
+			
+			$update = update_post_meta($post_id, 'anony_download', $download_meta);
+			
 		}else{
-			$download_meta['download_times'] = intval($download_meta['download_times']) + 1;
-			update_post_meta($_POST['download_id'], 'smpg_download', $download_meta);
+			$download_meta['anony_download']['download_times'] = intval($download_meta['anony_download']['download_times']) + 1;
+			$update = update_post_meta($post_id, 'anony_download', $download_meta);
 		}
-
-		die();
+		
+		
 	}
+	die();
+		
 }
-add_action('wp_ajax_download', 'smpg_download_ajax');
-add_action('wp_ajax_nopriv_download', 'smpg_download_ajax');//for users that are not logged in.
+add_action('wp_ajax_anony_download_times', 'anony_download_times');
+add_action('wp_ajax_nopriv_anony_download_times', 'anony_download_times');//for users that are not logged in.
