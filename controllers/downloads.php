25cb16cd35ad09de1bd11d8d9d08d6c287c8e140
makiomar <mo7amed.maki@gmail.com>
2020-05-02 09:25:30 +0300

    separate download view

--- controllers/downloads.php
+++ controllers/downloads.php
@@ -3,12 +3,54 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-	$tcp= new ANONY_Generate_Posts_View(
-						array('post_type' => 'anony_download','posts_per_page'=>5),
-						'downloads',
-						true
-					);
+$query= new WP_Query(['post_type' => 'anony_download','posts_per_page'=>5]);
 
-	$tcp->postsView();
+$data = [];
 
+if ($query->have_posts()) {
+	
+	while($query->have_posts()) {
+		$query->the_post();
+		
+		$temp = anony_common_post_data();
+		
+		$temp['thumb_img'] = get_the_post_thumbnail(get_the_ID(), [160, 180]);
+		
+		$curr_download_meta = get_post_meta( get_the_ID(), 'anony_download', true );
+		
+		if($curr_download_meta && $curr_download_meta != ''){
+			$curr_download = $curr_download_meta['anony_download']['anony_download_attachment'];
+			
+			$file_url = wp_get_attachment_url( intval($curr_download) ) ?  esc_url( wp_get_attachment_url( intval($curr_download) ) )  : flase;
+			if($file){
+				$temp['file_url'] = $file_url;
+			}
+			 
+		}
+		
+		$download_times = get_post_meta($id,'download_times',true);
+
+		if(!$download_times || empty($download_times)){
+
+			$download_times = 0;
+
+		}
+		
+		$temp['download_times'] = $download_times;
+		
+		$data[] = $temp;
+		
+	}
+	
+	wp_reset_postdata();
+}
+
+if (empty($data)) return;
+
+$sec_class = (is_front_page() || ishome()) ? ' section-front-page' : '';
+$sec_title = esc_html__('Suggested downloads',ANONY_TEXTDOM);
+$downloads_text = esc_html__('Downloads',ANONY_TEXTDOM);
+$default_thumb = get_theme_file_uri() . '/images/temporary-book-bg.jpg';
+
+include(locate_template( 'templates/download.view.php', false, false ));
 ?>
---
+++ templates/download.view.php
@@ -0,0 +1,58 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+?>
+<div class="anony-section<?= $sec_class  ?>">
+	<div>
+		<h4 class="anony-section_title clearfix"><?= $sec_title ?></h4>
+	</div>
+	
+	<div class="anony-posts-wrapper">
+
+		<div id="download">
+
+			<?php foreach ($data as $p) : extract($p);?>
+
+				<div id="download-<?= $id ?>" class="download-wrapper anony-grid-col-max-480-6 anony-grid-col-av-4 grid-10-col-md-2">
+
+					<div class="anony-grid-col anony-post-contents">
+
+						<div class="anony-download-meta">
+
+							<div class="anony-hover-toggle anony-post-image-wrapper" style="background-color: transparent">
+
+								<span id="download-<?= $id?>-count" class="anony-download-counter"><?= $download_times ?><br><?= $downloads_text ?></span>
+
+								<div class="anony-box-shadow anony-download-thumb">
+									<?php if( $thumb && $thumb_exists ) : ?>
+								
+										<?= $thumb_img ?>
+
+									<?php else :?>
+
+									<a href="<?= $permalink ?>" title="<?= $title ?>"><img src="<?= $default_thumb ?>" alt="<?= $title ?>"/></a>
+
+									<?php endif ?>
+
+								</div>
+							</div>
+
+						</div>
+
+						<h4 class="download-title"><?= $title ?></h4>
+
+						<div class="download">
+
+							<a title="download-<?= $id?>" target="_blank" class="anony-download" href="<?= $file_url ?>"><i class="fa fa-download"></i></a>
+						</div>
+
+					</div>
+
+				</div>
+
+			<?php endforeach ?>
+
+		</div>
+	</div>
+</div>
