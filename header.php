5911050082d7cc3939c89600e89207083f57c015
makiomar <mo7amed.maki@gmail.com>
2020-05-03 21:23:42 +0300

    Add menus data

--- header.php
+++ header.php
@@ -2,9 +2,6 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
-// start output buffering at the top of our script with this simple command
-// we've added "ob_gzhandler" as a parameter of ob_start
-ob_start('ob_gzhandler');
 
 $langAtts    = get_language_attributes();
 $contentType = get_bloginfo( 'html_type' );
@@ -12,6 +9,73 @@ $charSet     = get_bloginfo( 'charset' );
 $bodyClass   = 'class="' . join( ' ', get_body_class() ) . '"';
 $logo        = anony_get_custom_logo('orange');
 $nav         = anony_navigation('anony-main-menu');
+
+/** 
+ * The ANONY_MENU constant is defined in User control plugin.
+ * It contains user menu slug, defined by the plugin
+ */
+if(defined('ANONY_MENU') && wp_get_nav_menu_object( ANONY_MENU )){
+	$user_nav = wp_nav_menu(['menu' => ANONY_MENU , 'fallback_cb' => false]);
+}else{
+	$user_nav = anony_navigation('anony-user-menu');
+}
+
+$languages_menu = anony_navigation('anony-languages-menu','');
+
+$anonyOptions = anonyOpt();
+
+$socials_follow = 
+	[
+		[
+			'icon'   => 'facebook',
+			'url'    => $anonyOptions->facebook,
+			'title'  => esc_html__( 'Follow us on Facebook', ANONY_TEXTDOM )
+		],
+		
+		[
+			'icon'   => 'twitter',
+			'url'    => $anonyOptions->twitter,
+			'title'  => esc_html__( 'Follow us on Twitter', ANONY_TEXTDOM )
+		],
+		
+		[
+			'icon'   => 'youtube',
+			'url'    => $anonyOptions->youtube,
+			'title'  => esc_html__( 'Follow us on Youtube', ANONY_TEXTDOM )
+		],
+		
+		[
+			'icon'   => 'pinterest',
+			'url'    => $anonyOptions->pinterest,
+			'title'  => esc_html__( 'Follow us on Pinterest', ANONY_TEXTDOM )
+		],
+		
+		[
+			'icon'   => 'linkedin',
+			'url'    => $anonyOptions->linkedin,
+			'title'  => esc_html__( 'Follow us on Linkedin', ANONY_TEXTDOM )
+		],
+		
+		[
+			'icon'   => 'instagram',
+			'url'    => $anonyOptions->instagram,
+			'title'  => esc_html__( 'Follow us on Instagram', ANONY_TEXTDOM ),
+		],
+
+		[
+			'icon'   => 'tumblr',
+			'url'    => $anonyOptions->tumblr,
+			'title'  => esc_html__( 'Follow us on Tumblr', ANONY_TEXTDOM ),
+		],
+		
+		[
+			'icon'   => 'rss',
+			'url'    => $anonyOptions->rss,
+			'title'  => esc_html__( 'Follow us with RSS feed', ANONY_TEXTDOM ),
+		],
+	];
+$phone = $anonyOptions->phone;
+$email = $anonyOptions->email;
 
 include(locate_template( 'templates/header.view.php', false, false ));
 ?>
