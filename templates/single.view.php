76d316adaac3e875d491caeffc489b4a4bca6825
makiomar <mo7amed.maki@gmail.com>
2020-05-28 02:51:22 +0300

    change breadcrumbs location

--- templates/single.view.php
+++ templates/single.view.php
@@ -6,7 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header(); ?>
 
 <div class="anony-grid">
+	
 	<div class="anony-grid-row anony-grid-col">
+		<?php anony_breadcrumbs()?>
         <?php
             get_template_part( 'template-parts/single-post/'.get_post_type() );
             anony_get_correct_sidebar();
