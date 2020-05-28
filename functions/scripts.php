f72d7a6e88d32ecbcc2f99937e1aa24ee46573b0
makiomar <mo7amed.maki@gmail.com>
2020-05-28 16:52:34 +0300

    Load jquery mousewheel in archives

--- functions/scripts.php
+++ functions/scripts.php
@@ -101,11 +101,7 @@ function anony_scripts(){
 	$scripts = array();
 
 	if(is_archive()){
-		$scripts = array_merge($scripts, array('jquery.contentcarousel', 'jquery.easing.1.3'));
-	}
-
-	if(is_home() || is_front_page()){
-		$scripts = array_merge($scripts, array('jquery.mousewheel'));
+		$scripts = array_merge($scripts, array('jquery.mousewheel', 'jquery.contentcarousel', 'jquery.easing.1.3'));
 	}
 
 	
