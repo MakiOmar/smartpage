72d6e92ca99f292ed632e32e4bad3831200dddfb
makiomar <mo7amed.maki@gmail.com>
2020-05-28 16:54:13 +0300

    Apply performance options

--- functions/theme.php
+++ functions/theme.php
@@ -9,25 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @author Makiomar
  * @link http://makiomar.com
  */
-//controls add query strings to scripts/styles
-function anony_control_query_strings($src, $handle){
-	if(is_admin()) return $src;
-
-	$anonyOptions = ANONY_Options_Model::get_instance();
-	
-	//Keep query string for these items
-	$neglected = array();
-	
-	if(!empty($anonyOptions->keep_query_string)){
-		$neglected = explode(',',$anonyOptions->keep_query_string);
-	}
-	
-	if($anonyOptions->query_string != '0' && !in_array( $handle, $neglected )){
-		$src = remove_query_arg('ver', $src);
-	}
-	return $src;
-	
-}
 
 if (!function_exists('anony_hide_admin_bar')) {
 	/**
@@ -160,23 +141,6 @@ add_filter( 'post_thumbnail_html', function ( $html, $post_id, $post_thumbnail_i
     return $html;
 }, 10, 5 );
 
-//Remove type attribute from styles.
-add_filter('style_loader_tag', 'anony_remove_type_attr', 10, 2);
-
-//Remove type attribute from scripts.
-add_filter('script_loader_tag', 'anony_remove_type_attr', 10, 2);
-
-// custom login logo tooltip
-add_filter('login_headertext', function(){
-	
-	$anonyOptions = ANONY_Options_Model::get_instance();
-	
-	if($anonyOptions->change_login_title != '0'){
-		
-		return get_bloginfo();
-	}
-});
-
 add_action('init', function(){
 	
 	anony_restrict_admin_access();
@@ -184,6 +148,41 @@ add_action('init', function(){
 	anony_display_ads();
 }, 200);
 
+//favicon
+add_action('wp_head', function(){ 
+	$site_icon = get_option('site_icon');
+	
+	if($site_icon && $site_icon != '0') return;
+	?>
+    <!-- Custom Favicons -->
+    <link rel="shortcut icon" href="<?= ANONY_THEME_URI ?>/images/favicon.ico"/>
+    <link rel="icon" href="<?= ANONY_THEME_URI ?>/images/favicon.ico" />
+    <link rel="apple-touch-icon" href="<?= ANONY_THEME_URI ?>/images/favicon.ico">
+<?php });
+
+/*-------------------------------------------------------------------------
+ *  Performance
+ *-----------------------------------------------------------------------*/
+
+//controls add query strings to scripts/styles
+function anony_control_query_strings($src, $handle){
+	if(is_admin()) return $src;
+
+	$anonyOptions = ANONY_Options_Model::get_instance();
+	
+	//Keep query string for these items
+	$neglected = array();
+	
+	if(!empty($anonyOptions->keep_query_string)){
+		$neglected = explode(',',$anonyOptions->keep_query_string);
+	}
+	
+	if($anonyOptions->query_string != '0' && !in_array( $handle, $neglected )){
+		$src = remove_query_arg('ver', $src);
+	}
+	return $src;
+	
+}
 
 //controls add query strings to scripts
 add_filter( 'script_loader_src', 'anony_control_query_strings', 15, 2 );
@@ -191,4 +190,117 @@ add_filter( 'script_loader_src', 'anony_control_query_strings', 15, 2 );
 //controls add query strings to styles
 add_filter( 'style_loader_src', 'anony_control_query_strings', 15, 2);
 
-?>
+//Use custom avatar instead of Gravatar.com
+add_filter( 'get_avatar', function($avatar){
+	$anonyOptions = ANONY_Options_Model::get_instance();
+	
+	if(!$anonyOptions->gravatar || $anonyOptions->gravatar != '1') return $avatar;
+	
+	$avatar = '<img src="'.ANONY_THEME_URI.'/images/user.png"/>';
+    return $avatar;
+}, 200 );
+
+
+function anony_disable_wp_embeds(){
+	$anonyOptions = ANONY_Options_Model::get_instance();
+	
+	$keep = true;
+	
+	if($anonyOptions->disable_embeds == '1' ) $keep = false;
+	
+	if($anonyOptions->enable_singular_embeds == '1' && is_single()) $keep = true;
+
+	if($keep) return;
+	
+	// Remove the REST API endpoint.
+	remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+	// Turn off oEmbed auto discovery.
+	add_filter( 'embed_oembed_discover', '__return_false' );
+
+	// Don't filter oEmbed results.
+	remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+	// Remove oEmbed discovery links.
+	remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+	// Remove oEmbed-specific JavaScript from the front-end and back-end.
+	remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+	
+	add_filter( 'tiny_mce_plugins', function ($plugins) {
+		return array_diff($plugins, array('wpembed'));
+	});
+
+	// Remove all embeds rewrite rules.
+	add_filter( 'rewrite_rules_array', function($rules) {
+		foreach($rules as $rule => $rewrite) {
+		    if(false !== strpos($rewrite, 'embed=true')) {
+		        unset($rules[$rule]);
+		    }
+		}
+		return $rules;
+	} );
+
+	// Remove filter of the oEmbed result before any HTTP requests are made.
+	remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+}
+
+function anony_disable_wp_emojis(){
+	$anonyOptions = ANONY_Options_Model::get_instance();
+	
+	$keep = true;
+	
+	if($anonyOptions->disable_emojis == '1' ) $keep = false;
+	
+	if($anonyOptions->enable_singular_emojis == '1' && is_single()) $keep = true;
+
+	if($keep) return;
+	
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	
+	/**
+	 * Filter function used to remove the tinymce emoji plugin.
+	 * 
+	 * @param  array $plugins 
+	 * @return array Difference betwen the two arrays
+	 */
+	add_filter( 'tiny_mce_plugins', function( $plugins ) {
+
+		return ( is_array( $plugins ) ) ? array_diff( $plugins, array( 'wpemoji' ) ) : [];
+	} );
+	
+	
+	/**
+	 * Remove emoji CDN hostname from DNS prefetching hints.
+	 *
+	 * @param  array $urls URLs to print for resource hints.
+	 * @param  string $relation_type The relation type the URLs are printed for.
+	 * @return array Difference betwen the two arrays.
+	 */
+	add_filter( 'wp_resource_hints', function( $urls, $relation_type ) {
+		if ( 'dns-prefetch' == $relation_type ) {
+			/** This filter is documented in wp-includes/formatting.php */
+			$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+			$urls = array_diff( $urls, array( $emoji_svg_url ) );
+		}
+
+		return $urls;
+	}, 10, 2 );
+}
+//template_redirect runs before page/post is rendered
+add_action( 'template_redirect', function () {
+	
+	//Remove WP embeds
+	anony_disable_wp_embeds();
+	
+	//Remove WP emojies
+	anony_disable_wp_emojis();
+	
+}, 9999 );
