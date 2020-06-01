0aec6f7ad268216123328753bdd47d91f1365bde
makiomar <mo7amed.maki@gmail.com>
2020-06-01 20:19:09 +0300

    Leave only search form

--- searchform.php
+++ searchform.php
@@ -1,15 +1,8 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
-?>
-<div id="anony-hidden-search-form">
-	
-	<a class="anony-search-form-toggle" href="#" title="Scroll page">
-		<i class="fa fa-search"></i>
-	</a>
-	<form method="get" id="anony-searchform" class="anony-search-form anony-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="search" class="anony-search-input" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search', ANONY_TEXTDOM ); ?>" />
-		<button type="submit" class="anony-form_submit" name="submit" form="anony-searchform" value="<?php esc_attr_e( 'Search', ANONY_TEXTDOM ); ?>"><i class="fa fa-search"></i></button>
-	</form>
-</div>
+if ( ! defined( 'ABSPATH' ) ) { exit; /*Exit if accessed directly*/ }?>
+
+<form id="anony-searchform" class="anony-search-form anony-search" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+	<input type="search" class="anony-search-input" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search', ANONY_TEXTDOM ); ?>" />
+	<button type="submit" class="anony-form_submit" name="submit" form="anony-searchform" value="<?php esc_attr_e( 'Search', ANONY_TEXTDOM ); ?>"><i class="fa fa-search"></i></button>
+</form>
+
