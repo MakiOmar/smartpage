c312a8d108598ab737150e48df118ed70c9e922a
makiomar <mo7amed.maki@gmail.com>
2020-06-04 12:10:48 +0300

    Arrange

--- sidebar-single.php
+++ sidebar-single.php
@@ -2,28 +2,28 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
-/*
-*Sidebar only shows on a Single post
-*Can be controlled for theme options > layout > sidebar -> single post sidebar
-*/
 
+/**
+ * Sidebar only shows on a Single post
+ * Can be controlled for theme options > layout > sidebar -> single post sidebar
+ */
+  
+$widgets_url    = esc_url(get_home_url().'/wp-admin/widgets.php');
+$go_widget      = esc_html__( 'Please add some widgets. ', ANONY_TEXTDOM );
+$link_text      = esc_html__( 'Add Here', ANONY_TEXTDOM );
+$is_active_left = is_active_sidebar('left-sidebar');
 ?>
 
 <span class="anony-toggle-sidebar"><i class="fa fa-arrow-down"></i></span>
+
 <div class="anony-grid-col-sm-2-5 anony-asidebar anony-single-sidebar">
-	<?php  
-		if (is_active_sidebar('left-sidebar')){
-		   dynamic_sidebar('left-sidebar');
-		}else{
-			$url = get_home_url().'/wp-admin/widgets.php';
-			$allowed_tags = array('strong'=> array(), 'a' => array('href'=> array()));
-			printf(
-				wp_kses(
-						__( '<strong>Please add some widgets.</strong><a href="%s"> Add Here</a>', ANONY_TEXTDOM ), 
-						$allowed_tags
-					),
-				esc_url($url)
-			);
-		}
-	?>
+	<?php  if ($is_active_left) :
+			
+		dynamic_sidebar('left-sidebar');
+		   
+	else : ?>
+			
+		<strong><?= $go_widget ?></strong><a href="<?= $widgets_url ?>"><?= $link_text ?></a>
+			
+	<?php endif ?>
 </div>
