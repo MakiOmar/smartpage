8be82b70c68f636dc06df6cde4a874b519ab2409
makiomar <mo7amed.maki@gmail.com>
2020-06-04 11:58:15 +0300

    Arrange

--- sidebar-secondary.php
+++ sidebar-secondary.php
@@ -2,20 +2,28 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
-/*
-*This is the sidebar in the featured section
-*/
+/**
+ * This is the sidebar in the featured section
+ */
 
+$popular_title = esc_html_e('Popular',ANONY_TEXTDOM);
+$recent_comments_title = esc_html__('Recent comments',ANONY_TEXTDOM);
 ?>
 <div id="anony-secondary-sidebar" class="anony-grid-col-md-4 anony-grid-col">
-	  <div class="anony-grid-col-12 white-bg">
+	
+	<div class="anony-grid-col-12 white-bg">
+	  	
 		<ul class="tabs">
-		  <li class="anony-active-tab anony-grid-col-6 anony-popular" rel-id="anony-popular"><?php esc_html_e('Popular',ANONY_TEXTDOM)?></li>
-		  <li class="anony-grid-col-6 comments" rel-id="anony-comments"><?php esc_html_e('Recent comments',ANONY_TEXTDOM)?></li>
+			
+		  <li class="anony-active-tab anony-grid-col-6 anony-popular" rel-id="anony-popular"><?= $popular_title ?></li>
+		  <li class="anony-grid-col-6 comments" rel-id="anony-comments"><?= $recent_comments_title ?></li>
+		  
 		</ul>
+		
 		<?php 
 		   get_template_part('models/popular') ;
 		   get_template_part('templates/latest-comments') ;
-		 ?>	
-	  </div>
+		?>
+		 	
+	</div>
 </div>
