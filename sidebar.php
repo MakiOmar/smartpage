d7e0d7e2de72825a6068f28279958f8dce6fe8eb
makiomar <mo7amed.maki@gmail.com>
2020-06-04 12:14:11 +0300

    Arrange

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,26 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
-/*
-*Main sidebar
-*/
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+/**
+ * Main sidebar
+ */
+  
+$widgets_url    = esc_url(get_home_url().'/wp-admin/widgets.php');
+$go_widget      = esc_html__( 'Please add some widgets. ', ANONY_TEXTDOM );
+$link_text      = esc_html__( 'Add Here', ANONY_TEXTDOM );
+$is_active_right = is_active_sidebar('right-sidebar');
 
 ?>
+
 <div class="anony-grid-col anony-grid-col-sm-2-5 anony-asidebar">
-	<?php  
-		if (is_active_sidebar('right-sidebar')){
-		   dynamic_sidebar('right-sidebar');
-		}else{
-			$url = get_home_url().'/wp-admin/widgets.php';
-			$allowed_tags = array('strong'=> array(), 'a' => array('href'=> array()));
-			printf(
-				wp_kses(
-						__( '<strong>Please add some widgets.</strong><a href="%s"> Add Here</a>', ANONY_TEXTDOM ), 
-						$allowed_tags
-					),
-				esc_url($url)
-			);
-		}
-	?>
+	<?php  if ($is_active_right) :
+			
+		dynamic_sidebar('right-sidebar');
+		   
+	else : ?>
+			
+		<strong><?= $go_widget ?></strong><a href="<?= $widgets_url ?>"><?= $link_text ?></a>
+			
+	<?php endif ?>
 </div>
