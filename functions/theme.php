b462b4ed34e925b507eb9fcc698e20e500e63b25
makiomar <mo7amed.maki@gmail.com>
2020-06-06 11:42:19 +0300

    fix missing

--- functions/theme.php
+++ functions/theme.php
@@ -134,7 +134,7 @@ add_action('widgets_init',function(){
 		[
 			'right-sidebar'     => is_rtl() ? esc_html__( 'Right Sidebar', ANONY_TEXTDOM ) : esc_html__( 'Left Sidebar', ANONY_TEXTDOM ),
 			'left-sidebar'      => is_rtl() ? esc_html__( 'Left Sidebar', ANONY_TEXTDOM )  : esc_html__( 'Right Sidebar', ANONY_TEXTDOM ),
-			'secondary-sidebar' => esc_html__( 'Secondary Sidebar',
+			'secondary-sidebar' => esc_html__( 'Secondary Sidebar', ANONY_TEXTDOM ),
 			'footer-widget-1'   => esc_html__( 'Footer Widget 1', ANONY_TEXTDOM ),
 			'footer-widget-2'   => esc_html__( 'Footer Widget 2', ANONY_TEXTDOM ),
 			'footer-widget-3'   => esc_html__( 'Footer Widget 3', ANONY_TEXTDOM ),
