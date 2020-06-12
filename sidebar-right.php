589d9fa13545b0c412a0bc721b5499203ee01456
makiomar <mo7amed.maki@gmail.com>
2020-06-12 13:33:11 +0300

    Add sidebar right

---
+++ sidebar-right.php
@@ -0,0 +1,31 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Sidebar only shows on a Single post
+ * Can be controlled for theme options > layout > sidebar -> single post sidebar
+ */
+  
+$widgets_url    = esc_url(get_home_url().'/wp-admin/widgets.php');
+$go_widget      = esc_html__( 'Please add some widgets. ', ANONY_TEXTDOM );
+$link_text      = esc_html__( 'Add Here', ANONY_TEXTDOM );
+$is_active_right = is_active_sidebar('right-sidebar');
+?>
+
+<span class="anony-toggle-sidebar"><i class="fa fa-arrow-down"></i></span>
+
+<div class="anony-grid-col-sm-2-5 anony-asidebar anony-single-sidebar">
+	<?php  if ($is_active_right) :
+			
+		dynamic_sidebar('right-sidebar');
+		   
+	else : ?>
+			
+		<strong><?= $go_widget ?></strong><a href="<?= $widgets_url ?>"><?= $link_text ?></a>
+			
+	<?php endif ?>
+	
+	<?php do_action('sidebar_ad')?>
+</div>
