88757a947a17cec3db44913be0c35593129e41fc
makiomar <mo7amed.maki@gmail.com>
2020-06-12 13:08:42 +0300

    Arrange index view

--- templates/index.view.php
+++ templates/index.view.php
@@ -4,27 +4,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header();?>
-  <div class="anony-grid">
-  	<div class="anony-grid-row">
-      <div class="anony-grid-col">
-        <div class="anony-grid-col-sm-9-5 anony-grid-col">
-        	<div id="anony-<?= $grid ?>">
 
-				    <div id="anony-blog-post">
-			        <?php foreach ($data as $p) : extract($p);
-                
-								include(locate_template( 'templates/blog-post.view.php', false, false ));
-                
-					     endforeach;?>
-               
-  			    </div>
-        	</div>
+<div class="anony-grid">
+  
+	<div class="anony-grid-row">
+    
+    <div class="anony-grid-col">
+      
+      <div class="anony-grid-col-sm-9-5 anony-grid-col">
+        
+      	<div id="anony-<?= $grid ?>">
+
+			    <div id="anony-blog-post">
+            
+		        <?php 
+            foreach ($data as $p) : extract($p);
+              
+							include(locate_template( 'templates/blog-post.view.php', false, false ));
+              
+            endforeach;
+            ?>
+             
+			    </div>
           
-          <?= $pagination;?>
-          
-        </div>
-       <?php get_sidebar();?>
-	  </div>
-	</div>
+      	</div>
+        
+        <?= $pagination;?>
+        
+      </div>
+      
+      <?php get_sidebar();?>
+     
+    </div>
+    
   </div>
- <?php get_footer();?>
+  
+</div>
+
+<?php get_footer();?>
