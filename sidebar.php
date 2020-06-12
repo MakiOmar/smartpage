9b030288878387979128e5740ec2cea35d96db55
makiomar <mo7amed.maki@gmail.com>
2020-06-12 13:35:40 +0300

    Implement main sidebar widget

--- sidebar.php
+++ sidebar.php
@@ -9,14 +9,14 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 $widgets_url     = esc_url(get_home_url().'/wp-admin/widgets.php');
 $go_widget       = esc_html__( 'Please add some widgets. ', ANONY_TEXTDOM );
 $link_text       = esc_html__( 'Add Here', ANONY_TEXTDOM );
-$is_active_right = is_active_sidebar('right-sidebar');
+$is_active_main = is_active_sidebar('main-sidebar');
 
 ?>
 
 <div class="anony-grid-col anony-grid-col-sm-2-5 anony-asidebar">
-	<?php  if ($is_active_right) :
+	<?php  if ($is_active_main) :
 			
-		dynamic_sidebar('right-sidebar');
+		dynamic_sidebar('main-sidebar');
 		   
 	else : ?>
 			
