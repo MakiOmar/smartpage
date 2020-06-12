b49834d5f2ef28f70ebdc8cb1b343678da289855
makiomar <mo7amed.maki@gmail.com>
2020-06-12 13:33:39 +0300

    transfere sidebar registration to widgets file

--- functions/theme.php
+++ functions/theme.php
@@ -127,34 +127,6 @@ add_action( 'after_setup_theme', function() {
 	
 }, 20 );
 
-//Register Sidebars
-add_action('widgets_init',function(){
-	
-	
-	$sidebars = 
-		[
-			'right-sidebar'     => is_rtl() ? esc_html__( 'Right Sidebar', ANONY_TEXTDOM ) : esc_html__( 'Left Sidebar', ANONY_TEXTDOM ),
-			'left-sidebar'      => is_rtl() ? esc_html__( 'Left Sidebar', ANONY_TEXTDOM )  : esc_html__( 'Right Sidebar', ANONY_TEXTDOM ),
-			'secondary-sidebar' => esc_html__( 'Secondary Sidebar', ANONY_TEXTDOM ),
-			'footer-widget-1'   => esc_html__( 'Footer Widget 1', ANONY_TEXTDOM ),
-			'footer-widget-2'   => esc_html__( 'Footer Widget 2', ANONY_TEXTDOM ),
-			'footer-widget-3'   => esc_html__( 'Footer Widget 3', ANONY_TEXTDOM ),
-			'footer-widget-4'   => esc_html__( 'Footer Widget 4', ANONY_TEXTDOM )
-		];
-	foreach($sidebars as $sidebar_id => $sidebar){
-	    $args = array(
-			'name'          => $sidebar,
-			'id'            => $sidebar_id,
-			'class'         => $sidebar_id,
-			'before_widget' => '<div class="white-bg widgeted anony-grid-col-md-6 anony-grid-col-av-6 anony-grid-col-sm-12 anony-grid-col">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h3 class="widgeted_title">',
-			'after_title'   => '</h3>' 
-	    );
-	   register_sidebar( $args );
-	}
-});
-
 //Remove width|height from img
 add_filter( 'post_thumbnail_html', function ( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
     $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
