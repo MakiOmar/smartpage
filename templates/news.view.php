4783f2fa37d892175350e8b1e57e3a66c8be84cc
makiomar <mo7amed.maki@gmail.com>
2020-11-21 02:29:46 +0200

    Use marque for news bar

--- templates/news.view.php
+++ templates/news.view.php
@@ -12,13 +12,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div id="anony-dun-text">
 	<div id="dun_text_wrapper"<?= $dun_wrapper_class ?>>
-	
+	<marquee direction="right" scrollamount="3" behavior="scroll" onmouseover="this.stop()" onmouseout="this.start()">
 		<?php
 		 foreach ($data as $p) { extract($p) ?>
 
 			<p id="anony-dun-text-<?= $id ?>" class="dun_text"><?= $content ?></p>
 
 		<?php } ?>
+	</marquee>
 		
 	</div>
 </div>
