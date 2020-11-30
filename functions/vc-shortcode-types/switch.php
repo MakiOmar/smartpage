fc3864400d0bb6f89d202f42e62b0c69588b24c3
makiomar <mo7amed.maki@gmail.com>
2020-11-30 11:25:09 +0200

    Prevent direct access

--- functions/vc-shortcode-types/switch.php
+++ functions/vc-shortcode-types/switch.php
@@ -1,21 +1,26 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
 
-vc_add_shortcode_param( 'anony-switch', function ( $settings, $value ) {
+if (function_exists('vc_add_shortcode_param')){
 	
-	ob_start(); ?>
+	vc_add_shortcode_param( 'anony-switch', function ( $settings, $value ) {
 	
-	<div class="anony-vc-type-block">
-	
-		<input name="<?= esc_attr( $settings['param_name'] ) ?>" class="anony-vc-switch-set-value wpb_vc_param_value wpb-textinput <?= 
-	 esc_attr( $settings['param_name'] ) ?> <?= esc_attr( $settings['type'] ) ?>_field" type="hidden" value="<?= esc_attr( $value ) ?>" />
-	 
-	 <ul class="anony-vc-switch-set-list">
-	 	<li class="vc-switch-set-item" data-value="on"><span>ON</span></li>
-	 	<li class="vc-switch-set-item" data-value="off"><span>OFF</span></li>
-	 </ul>
-	 
-	</div>
-	
-	<?php return ob_get_clean(); 
-	
-}, ANONY_THEME_URI .'/functions/vc-shortcode-types/vc-scripts/switch.js');
+		ob_start(); ?>
+		
+		<div class="anony-vc-type-block">
+		
+			<input name="<?= esc_attr( $settings['param_name'] ) ?>" class="anony-vc-switch-set-value wpb_vc_param_value wpb-textinput <?= 
+		 esc_attr( $settings['param_name'] ) ?> <?= esc_attr( $settings['type'] ) ?>_field" type="hidden" value="<?= esc_attr( $value ) ?>" />
+		 
+		 <ul class="anony-vc-switch-set-list">
+		 	<li class="vc-switch-set-item" data-value="on"><span>ON</span></li>
+		 	<li class="vc-switch-set-item" data-value="off"><span>OFF</span></li>
+		 </ul>
+		 
+		</div>
+		
+		<?php return ob_get_clean(); 
+		
+	}, ANONY_THEME_URI .'/functions/vc-shortcode-types/vc-scripts/switch.js');
+
+}
