ada215211d5c90decaa070df46b17082c7e061ec
unknown <mo7amed.maki@gmail.com>
2022-02-16 19:00:57 +0200

    Comply to PHPCS standards

--- comments.php
+++ comments.php
@@ -1,32 +1,49 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
+/**
+ * Comments template
+ *
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); // Exit if accessed direct
+if (post_password_required()) { 
+    return;
 }
-if ( post_password_required() ) return;
 
 $data = [
-	'have_comments'   => have_comments(),
-	'comments_number' => sprintf( 
-							_nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', ANONY_TEXTDOM ), 
-							number_format_i18n( get_comments_number() ) 
-						),
+    'have_comments'   => have_comments(),
+    'comments_number' => sprintf( 
+        _nx(
+            'One Comment',
+            '%1$s Comments',
+            get_comments_number(),
+            'comments title',
+            ANONY_TEXTDOM
+        ), 
+        number_format_i18n(get_comments_number()) 
+    ),
 
-	'comments' => wp_list_comments( 
-		[ 
-			'echo'=> false ,
-			'avatar_size'=>'64',
-			'format'=>'xhtml',
-			'style' => 'div'
-		] 
-	),
+    'comments' => wp_list_comments( 
+        [ 
+            'echo'=> false ,
+            'avatar_size'=>'64',
+            'format'=>'xhtml',
+            'style' => 'div'
+        ] 
+    ),
 
-	'comments_open' => comments_open(),
-	'comments_off_text' => esc_html__( 'Comments off' , ANONY_TEXTDOM ),
+    'comments_open' => comments_open(),
+    'comments_off_text' => esc_html__('Comments off', ANONY_TEXTDOM),
 
 ];
 
 extract($data);
-wp_enqueue_script( 'jquery.validate.min' );
-wp_enqueue_script( 'anony-ajax_comment' );
-include(locate_template( 'templates/comments-single.view.php', false, false ));
+wp_enqueue_script('jquery.validate.min');
+wp_enqueue_script('anony-ajax_comment');
+require locate_template('templates/comments-single.view.php', false, false);
 ?>
