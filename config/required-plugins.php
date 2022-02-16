ecdbcfed99b295dea82e468dcb7168766e286f8f
unknown <mo7amed.maki@gmail.com>
2022-02-16 20:28:34 +0200

    Comply to PHPCS standards

--- config/required-plugins.php
+++ config/required-plugins.php
@@ -1,29 +1,43 @@
 <?php
-if (! defined('ABSPATH') ) {
-    exit; // Exit if accessed directly
-}
+/**
+ * Required plugins
+ *
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); // Exit if accessed direct
+
 add_action(
-    'init', function () {
-        if ($GLOBALS['pagenow'] === 'wp-login.php' ) { return;
+    'init',
+    function () {
+        if ($GLOBALS['pagenow'] === 'wp-login.php') {
+            return;
         }
-    
+
         if (!defined('ANOENGINE') && !is_admin()) {
             wp_die('Please activate/install AnonyEngine plugin, for AnonyEngine theme can work properly... <a href="'.esc_url(admin_url()).'">Go to admin</a>');
             /*wp_redirect( admin_url(  ) );
             exit();*/
         }
-    } 
+    }
 );
 /**
  * Display a notification if one of required plugins is not activated/installed
  */
 add_action(
-    'admin_notices', function () {
+    'admin_notices',
+    function () {
         if (!defined('ANOENGINE')) {
             ?>
         <div class="notice notice-error is-dismissible">
             <p><?php esc_html_e('Please activate/install AnonyEngine plugin, for User AnonyEngine theme can work properly'); ?></p>
         </div>
-        <?php }
+            <?php
+        }
     }
 );
