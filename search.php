84ea17654ed95c630834ae3415a78802963d807d
unknown <mo7amed.maki@gmail.com>
2022-02-16 18:25:53 +0200

    Comply to PHPCS standards

--- search.php
+++ search.php
@@ -1,29 +1,41 @@
 <?php 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
+/**
+ * Search form
+ *
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); // Exit if accessed direct
 
 $data = [];
 
-if ( have_posts() ) {
-	while (have_posts() ) { the_post();
-		
-		$data[] = anony_common_post_data();
+if (have_posts() ) {
+    while (have_posts() ) {
+        the_post();
+        
+        $data[] = anony_common_post_data();
 
-	}
-	
+    }
+    
 
-	$prev_text = is_rtl() ? 'right' : 'left';
-	
-	$next_text = is_rtl() ? 'left'  : 'right';
-	
-	$pagination = get_the_posts_pagination( array(
-			'type' => 'list',
-			'prev_text' => '<i class="fa fa-arrow-'.$prev_text.'"></i>',
-			'next_text' => '<i class="fa fa-arrow-'.$next_text.'"></i>',
-			'screen_reader_text'=>' ',
+    $prev_text = is_rtl() ? 'right' : 'left';
+    
+    $next_text = is_rtl() ? 'left'  : 'right';
+    
+    $pagination = get_the_posts_pagination(
+        array(
+        'type' => 'list',
+        'prev_text' => '<i class="fa fa-arrow-'.$prev_text.'"></i>',
+        'next_text' => '<i class="fa fa-arrow-'.$next_text.'"></i>',
+        'screen_reader_text'=>' ',
 
-		) );
+        ) 
+    );
 }
-include(locate_template( 'templates/index.view.php', false, false ));
+require locate_template('templates/index.view.php', false, false);
 ?>
