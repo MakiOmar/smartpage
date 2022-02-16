6bc6013c57852b073b9dd921c5667858bf6e02e5
unknown <mo7amed.maki@gmail.com>
2022-02-16 18:46:42 +0200

    Comply to PHPCS standards

--- front-page.php
+++ front-page.php
@@ -1,4 +1,16 @@
-<?php 
+<?php
+/**
+ * Frontpage template
+ *
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); // Exit if accessed direct
 
 $anonyOptions = ANONY_Options_Model::get_instance();
 
@@ -10,17 +22,20 @@ $revSlider = $anonyOptions->rev_slider;
 
 $homeSlider = $anonyOptions->home_slider;
 
-$chooseSlider = esc_html__("You didn't choose a slider, Please select one from theme options page");
+$chooseSlider 
+    = esc_html__(
+        "You didn't choose a slider, Please select one from theme options page"
+    );
 
-	
-if(function_exists('putRevSlider') && $homeSlider == '1'){
-	
-	if($revSlider != ''){
-		
-		$slider = ANONY_HELP::obGet('putRevSlider', [$revSlider]);
-		
-	}
+    
+if (function_exists('putRevSlider') && $homeSlider == '1') {
+    
+    if ($revSlider != '') {
+        
+        $slider = ANONY_HELP::obGet('putRevSlider', [$revSlider]);
+        
+    }
 }
-	
-include(locate_template( 'templates/front-page.view.php', false, false ));
+    
+require locate_template('templates/front-page.view.php', false, false);
 ?>
