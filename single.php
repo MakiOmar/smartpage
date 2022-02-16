a9cc876177ec327f4ee360f9557a541159f430da
unknown <mo7amed.maki@gmail.com>
2022-02-16 18:06:19 +0200

    Comply to PHPCS standards

--- single.php
+++ single.php
@@ -1,33 +1,43 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
+<?php
+/**
+ * Theme single template
+ * 
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+!defined('ABSPATH') or die(); // Exit if accessed directly
 
 $anonyOptions = ANONY_Options_Model::get_instance();
 
 $wrapper_class = ($anonyOptions->single_sidebar == '1') ? 'anony-grid-col-sm-7' : 'anony-grid-col-sm-9-5' ;
 
-$post_type = get_post_type( );
+$post_type = get_post_type();
 
 $data = [];
 
-if ( have_posts() ) {
-	
-	while (have_posts() ) { 
-		the_post();
-		$data = anony_common_post_data(get_post_type());
-	}
+if (have_posts() ) {
+    
+    while (have_posts() ) { 
+        the_post();
+        $data = anony_common_post_data(get_post_type());
+    }
 }
 
 $right_sidebar = is_rtl() ? 'right' : 'left' ;
 
 $left_sidebar  = is_rtl() ? 'left'  : 'right' ;
 
-if (empty($data)) return;
+if (empty($data)) { return;
+}
 
 extract($data);
 
-do_action( 'anony_before_inculde_single',$data,  $post_type);
+do_action('anony_before_inculde_single', $data,  $post_type);
 
 
-include(locate_template( 'template-parts/single-post/'.get_post_type().'.php', false, false ));
+require locate_template('template-parts/single-post/'.get_post_type().'.php', false, false);
