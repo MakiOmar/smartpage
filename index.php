cd54304eca379e025b519040a2d5ce6b25b1dd29
unknown <mo7amed.maki@gmail.com>
2022-02-16 16:59:48 +0200

    Comply to PHPCS standards

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
+<?php
+/**
+ * SmartPage Theme index
+ * 
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); 
+
 $anonyOptions = ANONY_Options_Model::get_instance();
 
 $grid = $anonyOptions->posts_grid;
 
 $data = [];
 
-if ( have_posts() ) {
-	while (have_posts() ) { the_post();
-		
-		$data[] = anony_common_post_data();
+if (have_posts()) {
+    while (have_posts() ) {
+        the_post();
+        
+        $data[] = anony_common_post_data();
 
-	}
-	
+    }
+    
 
-	$pagination = anony_pagination();
+    $pagination = anony_pagination();
 }
-if (empty($data)) return;
+if (!empty($data)) {
+    include locate_template('templates/index.view.php', false, false);
+}
 
-include(locate_template( 'templates/index.view.php', false, false ));
+
 ?>
