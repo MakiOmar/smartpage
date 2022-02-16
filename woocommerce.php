bc9b062a7f7fa06de2917507a00bd6504cd6aaf7
unknown <mo7amed.maki@gmail.com>
2022-02-16 17:12:25 +0200

    Comply to PHPCS standards

--- woocommerce.php
+++ woocommerce.php
@@ -1,32 +1,45 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
+<?php
+/**
+ * Woocommerce store file
+ * 
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+!defined('ABSPATH') or die(); // Exit if accessed directly
+
 get_header();?>
 <div class="anony-grid">
-  	
-  	<div class="anony-grid-row anony-grid-col">
-  		
+    
+    <div class="anony-grid-row anony-grid-col">
+        
         <div class="anony-grid-col-sm-9-5">
-        	
-        	<div class="anony-grid-col anony-posts-wrapper">
-        		
-        	<?php if(has_action('page_ad')) do_action('page_ad');?>
-        	
-				<div class="anony-grid-container">
-					
-					<?php woocommerce_content() ?>
-				
-				</div>
-				
-			</div>
-			
+            
+            <div class="anony-grid-col anony-posts-wrapper">
+                
+            <?php 
+            if (has_action('page_ad')) {
+                do_action('page_ad')
+            }?>
+            
+                <div class="anony-grid-container">
+                    
+                    <?php woocommerce_content() ?>
+                
+                </div>
+                
+            </div>
+            
         </div>
         
        <?php get_sidebar();?>
        
-	</div>
-	
+    </div>
+    
 </div>
 
 <?php get_footer();?>
