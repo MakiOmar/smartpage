0aef1e89e71a167c128df0d115e880ffc5d7f7d3
unknown <mo7amed.maki@gmail.com>
2022-02-16 18:22:50 +0200

    Comply to PHPCS standards

--- searchform.php
+++ searchform.php
@@ -1,12 +1,42 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) { exit; /*Exit if accessed directly*/ }
+/**
+ * Search form
+ *
+ * PHP version 7.3 Or Later
+ * 
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); // Exit if accessed direct
 
 $search_query = get_search_query();
-$search_text  = esc_attr__( 'Search', ANONY_TEXTDOM );
+$search_text  = esc_attr__('Search', ANONY_TEXTDOM);
 ?>
 
-<form id="anony-searchform" class="anony-search-form anony-search" action="<?= ANONY_BLOG_URL ?>" method="get">
-	<input type="search" class="anony-search-input" name="s" id="s" value="<?= $search_query ?>" placeholder="<?= $search_text ?>" />
-	<button type="submit" class="anony-form_submit" name="submit" form="anony-searchform" value="<?= $search_text ?>"><i class="fa fa-search"></i></button>
+<form 
+id="anony-searchform" 
+class="anony-search-form anony-search" 
+action="<?php echo ANONY_BLOG_URL ?>" 
+method="get">
+
+    <input 
+    type="search" 
+    class="anony-search-input" 
+    name="s" 
+    id="s" 
+    value="<?php echo $search_query ?>" 
+    placeholder="<?php echo $search_text ?>" />
+
+    <button 
+    type="submit" 
+    class="anony-form_submit" 
+    name="submit" 
+    form="anony-searchform" 
+    value="<?php echo $search_text ?>">
+    <i class="fa fa-search"></i>
+    </button>
 </form>
 
