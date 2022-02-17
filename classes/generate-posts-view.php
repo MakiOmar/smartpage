16c454efbff4fb351f6ff3ef5a7a7c0f3f7f799d
unknown <mo7amed.maki@gmail.com>
2022-02-17 17:14:13 +0200

    Comply to PHPCS standards

--- classes/generate-posts-view.php
+++ classes/generate-posts-view.php
@@ -1,7 +1,29 @@
 <?php
-if (! defined('ABSPATH') ) {
-    exit; // Exit if accessed directly
-}
+/**
+ * Post view generator
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+defined('ABSPATH') or die(); // Exit if accessed direct
+
+!class_exists('ANONY_Generate_Posts_View')) or return;
+
+/**
+ * Post view generator class
+ *
+ * @category   Widgets
+ * @package    Widgets
+ * @subpackage Category
+ * @author     Makiomar <info@makior.com>
+ * @license    https://makiomar.com SmartPage Licence
+ * @link       https://makiomar.com
+ */
 class ANONY_Generate_Posts_View
 {
 
@@ -56,13 +78,13 @@ class ANONY_Generate_Posts_View
     */
     public $msg = null;
     
-    /*
-    *generatePostView constructor
-    *@param  array   $args      array to create post object
-    *@param  string  $template  the name of posts template
-    *@param  bool    $reset     if the loop need to be reset
-    */
-
+    /**
+     * Class constructor
+     * 
+     * @param array  $args     array to create post object
+     * @param string $template the name of posts template
+     * @param bool   $reset    if the loop need to be reset
+     */
     public function __construct($args, $template = 'blog-post.view', $reset = false)
     {
 
@@ -78,38 +100,40 @@ class ANONY_Generate_Posts_View
 
     }
     
-    /*
-    *Display posts list view
-    */
+    /**
+     * Display posts list view
+     * 
+     * @return void
+     */
     public function postsView()
     {
         $className= 'ANONY_views__'.ucfirst($this->postsTemplate);
         
-        if(class_exists($className)) {
+        if (class_exists($className)) {
                 
             $view = new $className($this);
 
-            if(method_exists($view, 'IfNot')) {
+            if (method_exists($view, 'IfNot')) {
                 $this->IfNot = $view->IfNot();
             }
                 
         }
         
-        if($this->post->have_posts()) {
-            if(isset($view)) {
-                if(!is_null($this->msg) && !empty($this->msg)) {
+        if ($this->post->have_posts()) {
+            if (isset($view)) {
+                if (!is_null($this->msg) && !empty($this->msg)) {
                     $view->msg = $this->msg;
                 }
                 $view->render();
             }
                 
-            if($this->resetLoop === true) {
+            if ($this->resetLoop === true) {
 
                 wp_reset_postdata();
 
             }
             
-        }else{
+        } else {
             echo $this->IfNot;
         }
     }
