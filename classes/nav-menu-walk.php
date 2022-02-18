5fda29c49c7a46133dcf9941fa23840064a13fd9
unknown <mo7amed.maki@gmail.com>
2022-02-19 00:17:12 +0200

    Comply to WordPress standards

--- classes/nav-menu-walk.php
+++ classes/nav-menu-walk.php
@@ -1,19 +1,45 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed direct.ly
-}
 /**
- * Custom Frontend Main Menu Walker
+ * Frontend Main Menu Walker
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @category WordPress
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+
+/**
+ * Frontend Main Menu Walker class
+ *
+ * @category   Walker
+ * @package    Navigation
+ * @subpackage Category
+ * @author     Makiomar <info@makior.com>
+ * @license    https://makiomar.com SmartPage Licence
+ * @link       https://makiomar.com
  */
 class ANONY_Nav_Menu_Walk extends Walker_Nav_Menu {
 
-
+	/**
+	 * Starts the list before the elements are added.
+	 *
+	 * @see Walker::start_lvl()
+	 *
+	 * @param string   $output Used to append additional content (passed by reference).
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
 		$t      = "\t";
 		$n      = "\n";
 		$indent = str_repeat( $t, $depth );
 
-		// Default class.
+		// Default html classes.
 		$classes = array( 'anony-dropdown', 'anony-sub-menu' );
 
 		$class_names = join( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
@@ -21,6 +47,15 @@ class ANONY_Nav_Menu_Walk extends Walker_Nav_Menu {
 		$output     .= "{$n}{$indent}<ul $class_names>{$n}";
 	}
 
+	/**
+	 * Ends the list of after the elements are added.
+	 *
+	 * @see Walker::end_lvl()
+	 *
+	 * @param string   $output Used to append additional content (passed by reference).
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
 	public function end_lvl( &$output, $depth = 0, $args = array() ) {
 		$t       = "\t";
 		$n       = "\n";
@@ -28,8 +63,19 @@ class ANONY_Nav_Menu_Walk extends Walker_Nav_Menu {
 		$output .= "$indent</ul>{$n}";
 	}
 
+	/**
+	 * Starts the element output.
+	 *
+	 * @see Walker::start_el()
+	 *
+	 * @param string   $output            Used to append additional content (passed by reference).
+	 * @param WP_Post  $item       Menu item data object.
+	 * @param int      $depth             Depth of menu item. Used for padding.
+	 * @param stdClass $args              An object of wp_nav_menu() arguments.
+	 * @param int      $id                Optional. ID of the current menu item. Default 0.
+	 */
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		 $t     = "\t";
+		$t      = "\t";
 		$n      = "\n";
 		$indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
 
@@ -76,6 +122,16 @@ class ANONY_Nav_Menu_Walk extends Walker_Nav_Menu {
 		$output      .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
 
+	/**
+	 * Ends the element output, if needed.
+	 *
+	 * @see Walker::end_el()
+	 *
+	 * @param string   $output      Used to append additional content (passed by reference).
+	 * @param WP_Post  $item        Menu item data object. Not used.
+	 * @param int      $depth       Depth of page. Not Used.
+	 * @param stdClass $args        An object of wp_nav_menu() arguments.
+	 */
 	public function end_el( &$output, $item, $depth = 0, $args = array() ) {
 		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
 			$t = '';
