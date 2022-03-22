b709cce6504afd15052c7ba05c7456a85379c2fa
unknown <mo7amed.maki@gmail.com>
2022-03-22 21:19:30 +0200

    Add missing css class

--- page.php
+++ page.php
@@ -9,61 +9,58 @@
  * @license  https://makiomar.com SmartPage Licence
  * @link     https://makiomar.com
  */
+
 defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 
-get_header();?>
-<div class="anony-grid">
-	  
-	  <div class="anony-grid-row anony-grid-col">
-		  
-		<div class="anony-grid-col-sm-9-5">
-			
-			<div class="anony-grid-col anony-posts-wrapper">
-				
-			<?php
-			if ( has_action( 'page_ad' ) ) {
-				do_action( 'page_ad' );
-			}
-			?>
-			
-				<div class="anony-grid-container">
-					
-				<?php
-				if ( have_posts() ) {
+get_header();
 
-					while ( have_posts() ) {
+?>
+
+<div class="anony-grid">
+
+	<div class="anony-grid-row anony-grid-col">
+
+		<div class="anony-grid-col anony-grid-col-sm-9-5">
+
+			<div class="anony-grid-col anony-posts-wrapper">
+
+				<div class="anony-grid-container">
+
+				<?php
+				if ( have_posts() ) :
+
+					while ( have_posts() ) :
 
 						the_post();
 						?>
-					
-						<div 
-						class="anony-grid-col anony-post-contents anony-single_post">
-							
+
+						<div class="anony-grid-col anony-post-contents anony-single_post">
+
 							<div class="anony-post-info">
-								
+
 								<div class="anony-single-text">
-						<?php the_content(); ?>
+									<?php the_content(); ?>
 								</div>
-								
+
 							</div>
-							
+
 						</div>
-					
+
 						<?php
-					}
-				};
+					endwhile;
+				endif;
 				?>
-				
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
-		
-	   <?php get_sidebar(); ?>
-	   
+
+		<?php get_sidebar(); ?>
+
 	</div>
-	
+
 </div>
 
 <?php get_footer(); ?>
