e3c8f7a87261978e5b5d5ef49a9a52ca471d4026
unknown <mo7amed.maki@gmail.com>
2022-09-17 18:22:41 +0200

    Add more skin elements

--- functions/skin.php
+++ functions/skin.php
@@ -6,10 +6,11 @@ function anony_theme_skin(){
 	$secondary_color = $anony_options->secondary_skin_color; ?>
 
 	<style type="text/css">
-		.anony-skew-bg::after, .page-numbers li, .active, .button, .anony-active-tab,
+
+		.anony-skew-bg::after, .page-numbers li, .anony-active, .button, .anony-active-tab,
 .anony-page-numbers a, .widgeted_title, #anony-dun-title, #submit, .anony-form_submit,
 .anony-featured-posts-title, .anony-section_title, #anony-page-scroll, .anony-button,
-.anony-post-image-wrapper h4, .f-post-title, .reply, .dun_text:after {
+.anony-post-image-wrapper h4, .f-post-title, .reply, .dun_text:after, .single-download, .anony-toggle-sidebar, #anony-main_nav_con, .anony-popular-tabs span:not(.anony-active-tab):nth-child(2), li .current::after {
 		  background-color: <?php echo esc_html( $primary_color ) ;?>;
 		}
 		.anony-post-info .anony-nothumb-post .anony-thumb-post-title, #anony-top-header-wrapper, #anony-page-loading-bg {
@@ -18,10 +19,7 @@ function anony_theme_skin(){
 		.anony-section, #anony-slider-wrapper {
 		  border-top-color: <?php echo esc_html( $primary_color ) ;?>;
 		}
-		.single-download, .anony-toggle-sidebar, #anony-main_nav_con, .anony-popular-tabs span:not(.anony-active-tab):nth-child(2) {
-		  background-color: <?php echo esc_html( $primary_color ) ;?>;
-		}
-		.fa-calendar, .fa-comments-o, .fa-folder-open, .fa-eye, .fa-download, a, h1, h2, h3, h4, h5, h6, .anony-breadcrumbs li, .anony-breadcrumbs a {
+		.fa-calendar, .fa-comments-o, .fa-folder-open, .fa-eye, .fa-download, a, .anony-breadcrumbs li, .anony-breadcrumbs a {
 		  color: <?php echo esc_html( $primary_color ) ;?>;
 		}
 		#anony-main-menu-con .anony-sub-menu li a {
@@ -46,16 +44,29 @@ function anony_theme_skin(){
 		  border-bottom: 1px solid <?php echo esc_html( $primary_color ) ;?>;
 		  background-color: <?php echo esc_html( $primary_color ) ;?>;
 		}
-		li .current::after {
-		  background-color: <?php echo esc_html( $primary_color ) ;?>;
+		
+		.anony-custom-bullets ul li::before {
+		  color: <?php echo esc_html( $secondary_color ) ;?>; /* Change the color */
 		}
+		
+		<?php if ( class_exists( 'woocommerce' ) ) : ?>
+		.woocommerce ul.products li.product a.add_to_cart_button{
+			background-color: <?php echo esc_html( $secondary_color ) ;?>;
+		}
+		
+		.woocommerce ul.products li.product .price{
+			color: <?php echo esc_html( $secondary_color ) ?>;
+		}
+		
+		<?php endif; ?>
 		@media screen and (min-width: 768px) {
 		  #anony-main-menu-con li a:hover {
 			background-color: <?php echo esc_html( $primary_color ) ;?>;
 			font-size: 16px;
 		  }
 		}
-
+		
+		
 	</style>
 <?php }
 add_action( 'wp_head', 'anony_theme_skin' );
