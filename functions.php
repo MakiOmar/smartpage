7c4fc046fd3c573b0c93e24aa7d9c6755836f229
unknown <mo7amed.maki@gmail.com>
2022-10-16 15:47:56 +0200

    Move update checker to functions.php

--- functions.php
+++ functions.php
@@ -136,3 +136,12 @@ require_once wp_normalize_path( ANONY_LIBS_DIR . 'vc-shortcode-types/switch.php'
  * Elementor includes
  */
 require_once wp_normalize_path( ANONY_ELEMENTOR_EXTENSION . 'elementor-incl.php' );
+
+
+require ANONY_THEME_DIR . '/plugin-update-checker/plugin-update-checker.php';
+
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/MakiOmar/smartpage/blob/8d04c1006f386b056103e28ad29a573677c9d972/plugin-update-checker/examples/theme.json',
+    __FILE__, //Full path to the main plugin file or functions.php.
+    'anony-smartpage'
+);
--- functions/updates.php
+++ functions/updates.php
@@ -7,12 +7,3 @@
  *
  * @package    SmartPage
  */
-
-
-require ANONY_THEME_DIR . '/plugin-update-checker/plugin-update-checker.php';
-
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/MakiOmar/smartpage/blob/8d04c1006f386b056103e28ad29a573677c9d972/plugin-update-checker/examples/theme.json',
-    __FILE__, //Full path to the main plugin file or functions.php.
-    'anony-smartpage'
-);
