c594b1dd1bc0d58b87ecf7edff3637b3d9dd808c
makiomar <mo7amed.maki@gmail.com>
2023-11-26 11:21:20 +0200

    Rename file

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Frontpage template
+ * Frontpage model
  *
  * PHP version 7.3 Or Later
  *
@@ -9,6 +9,7 @@
  * @license  https://makiomar.com SmartPage Licence
  * @link     https://makiomar.com
  */
+
 defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 
 $anony_options = ANONY_Options_Model::get_instance();
@@ -17,24 +18,23 @@ $grid = $anony_options->posts_grid;
 
 $slider = false;
 
-$revSlider = $anony_options->rev_slider;
+$rev_slider = $anony_options->rev_slider;
 
-$homeSlider = $anony_options->home_slider;
+$home_slider = $anony_options->home_slider;
 
-$chooseSlider
-	= esc_html__(
+$choose_slider
+	= __(
 		"You didn't choose a slider, Please select one from theme options page"
 	);
 
 
-if ( function_exists( 'putRevSlider' ) && $homeSlider == '1' ) {
+if ( function_exists( 'putRevSlider' ) && '1' === $home_slider ) {
 
-	if ( $revSlider != '' ) {
+	if ( '' !== $rev_slider ) {
 
-		$slider = ANONY_HELP::obGet( 'putRevSlider', array( $revSlider ) );
+		$slider = ANONY_HELP::ob_get( 'putRevSlider', array( $rev_slider ) );
 
 	}
 }
 
-require locate_template( 'templates/front-page.view.php', false, false );
-
+require locate_template( 'templates/front-page-view.php', false, false );
---
+++ templates/front-page-view.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Frontpage view
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
+
+get_header(); ?>
+
+<div class="anony-grid">
+
+	<?php if ( $slider ) : ?> 
+	 
+		<?php
+			// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo $slider;
+			// phpcs:enable.
+		?>
+	 
+	<?php endif ?>
+	 
+	<?php if ( ! $slider && '1' === $home_slider ) : ?>
+
+		<p class="anony-warning"><?php echo esc_html( $choose_slider ); ?></p>        
+
+	<?php endif ?>
+
+		<div class="anony-grid-row anony-grid-col">
+		<div class="anony-grid-col-sm-9-5">
+			<div class="anony-content-wrapper">
+				 
+				<?php get_sidebar( 'secondary' ); ?>
+			   
+				<div id="anony-section-top" class="anony-grid-col-md-8 anony-grid-col">
+
+					<?php get_template_part( 'models/news' ); ?>
+					 
+					<?php get_template_part( 'models/featured' ); ?>
+					 
+				</div>   
+			</div>
+			<?php
+
+				get_template_part( 'models/downloads' );
+
+				require locate_template( 'models/category-posts-section.php', false, false );
+				$video_template = false;
+			if ( $video_template ) {
+				get_template_part( 'models/video' );
+			}
+
+			?>
+			<div class="anony-ad">
+				 
+				<?php do_action( 'before_footer_ad' ); ?>
+				 
+			</div>
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
+	</div>
+<?php get_footer(); ?>
