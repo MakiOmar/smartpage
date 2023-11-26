56c73b26573e099296a06db13587dbd90100be64
makiomar <mo7amed.maki@gmail.com>
2023-11-26 22:37:48 +0200

    WPCS

--- models/rate.php
+++ models/rate.php
@@ -1,26 +1,33 @@
-<?php 
-if (! defined('ABSPATH') ) {
-    exit; // Exit if accessed direct.ly
-}
+<?php
+/**
+ * Rate model
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com/smartpage
+ */
 
-$post_ratings = get_post_meta($pID, 'anony_post_rating', true);
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 
-if(!$post_ratings || empty($post_ratings) || !is_array($post_ratings)) {
-    $rateTimes = 0;
-    $rateValue = 0;
-    $rateBg = 0;
-}else{
-    $rateTimes = count($post_ratings);
-    $sumRates = array_sum(array_values($post_ratings));
-    $rateValue = $sumRates/$rateTimes;
-    $rateBg = (($rateValue)/5)*100;
+$post_ratings = get_post_meta( $id, 'anony_post_rating', true );
+
+if ( ! $post_ratings || empty( $post_ratings ) || ! is_array( $post_ratings ) ) {
+	$rate_times = 0;
+	$rate_value = 0;
+	$rate_bg    = 0;
+} else {
+	$rate_times = count( $post_ratings );
+	$sum_rates  = array_sum( array_values( $post_ratings ) );
+	$rate_value = $sum_rates / $rate_times;
+	$rate_bg    = ( ( $rate_value ) / 5 ) * 100;
 }
-$ratedCount  = floor($rateValue);
-$ratedText   = esc_html__('Rated', 'smartpage');
-$outOf       = esc_html__('out of', 'smartpage');
-$reviewsText = esc_html__('Review(s)', 'smartpage');
+$rated_count  = floor( $rate_value );
+$rated_text   = __( 'Rated', 'smartpage' );
+$out_of       = __( 'out of', 'smartpage' );
+$reviews_text = __( 'Review(s)', 'smartpage' );
 
 $rate_class = is_user_logged_in() ? ' rate-btn' : '';
-//$rateStatus  = !in_array($_SERVER["REMOTE_ADDR"],$userIps) ? 'new_rate'  : 'current_rate';
-require locate_template('templates/rate-view.php', false, false);
-?>
+require locate_template( 'templates/rate-view.php', false, false );
--- templates/rate-view.php
+++ templates/rate-view.php
@@ -1,31 +1,40 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed direct.ly
-}
+/**
+ * Rate view
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com/smartpage
+ */
+
+defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 ?>
-<div id="rating-<?php echo $id; ?>" class="anony-metadata">
+<div id="rating-<?php echo esc_attr( $id ); ?>" class="anony-metadata">
 
 	<?php
 	for ( $r = 1; $r <= 5; $r++ ) {
 
-		$rate_star = ( $r <= $ratedCount ) ? 'fa-star' : 'fa-star-o';
+		$rate_star = ( $r <= $rated_count ) ? 'fa-star' : 'fa-star-o';
 		?>
 
-		<i id="<?php echo $r; ?>-<?php echo $id; ?>" class="fa <?php echo $rate_star; ?> btn-<?php echo $id; ?>-<?php echo $r; ?> <?php echo $rate_class; ?>"></i><?php echo ( ( $r != 5 ) ? '&nbsp' : '' ); ?>
+		<i id="<?php echo esc_attr( $r ); ?>-<?php echo esc_attr( $id ); ?>" class="fa <?php echo esc_attr( $rate_star ); ?> btn-<?php echo esc_attr( $id ); ?>-<?php echo esc_attr( $r ); ?> <?php echo esc_attr( $rate_class ); ?>"></i><?php echo ( ( 5 !== $r ) ? '&nbsp' : '' ); ?>
 
 	<?php } ?>
 	 
-	<span class="hidden" id="rated-<?php echo $id; ?>"><?php echo $ratedCount; ?></span>
+	<span class="hidden" id="rated-<?php echo esc_attr( $id ); ?>"><?php echo esc_html( $rated_count ); ?></span>
 	 
-	<span class="hidden" id="clicked-<?php echo $id; ?>"></span>
+	<span class="hidden" id="clicked-<?php echo esc_attr( $id ); ?>"></span>
 	 
 	<?php if ( is_single() ) : ?>
 
-		<p class ="rated-text"><?php echo $ratedText; ?>
+		<p class ="rated-text"><?php echo esc_html( $rated_text ); ?>
 
-			<span class="rated-<?php echo $id; ?>"><?php echo $ratedCount; ?></span><?php echo $outOf; ?>
+			<span class="rated-<?php echo esc_attr( $id ); ?>"><?php echo esc_html( $rated_count ); ?></span><?php echo esc_html( $out_of ); ?>
 
-			<span class="times-<?php echo $id; ?>">&nbsp;<?php echo $rateTimes; ?>&nbsp;</span><?php echo $reviewsText; ?>
+			<span class="times-<?php echo esc_attr( $id ); ?>">&nbsp;<?php echo esc_html( $rate_times ); ?>&nbsp;</span><?php echo esc_html( $reviews_text ); ?>
 
 		</p>
 
@@ -33,4 +42,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 </div>
 
-<input id="post-id-<?php echo $id; ?>" type="hidden" value="<?php echo $id; ?>"/>
+<input id="post-id-<?php echo esc_attr( $id ); ?>" type="hidden" value="<?php echo esc_attr( $id ); ?>"/>
