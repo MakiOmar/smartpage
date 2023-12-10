35cffdf33ff2b849258e4ca816232b12634876b9
makiomar <mo7amed.maki@gmail.com>
2023-12-10 10:45:54 +0200

    WPCS

--- page.php
+++ page.php
@@ -14,18 +14,17 @@ defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 
 get_header();
 
-// Check if the Elementor plugin is active
-if (is_plugin_active('elementor/elementor.php')) {
-    // Check if the current page is built with Elementor
-    if (get_post_meta(get_the_ID(), '_elementor_edit_mode', true) === 'builder') {
-        the_content();
-    } else {
-        get_template_part( 'templates/page', 'view' );
-    }
+// Check if the Elementor plugin is active.
+if ( is_plugin_active( 'elementor/elementor.php' ) ) {
+	// Check if the current page is built with Elementor.
+	if ( get_post_meta( get_the_ID(), '_elementor_edit_mode', true ) === 'builder' ) {
+		the_content();
+	} else {
+		get_template_part( 'templates/page', 'view' );
+	}
 } else {
-    // Elementor plugin is not active
-    get_template_part( 'templates/page', 'view' );
+	// Elementor plugin is not active.
+	get_template_part( 'templates/page', 'view' );
 }
 
 get_footer();
-
