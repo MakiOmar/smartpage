f27bc73610044c2e397057b46f6d69280dc5d9eb
makiomar <mo7amed.maki@gmail.com>
2023-12-12 15:53:00 +0200

    Change search icon

--- functions/menus.php
+++ functions/menus.php
@@ -184,8 +184,19 @@ add_filter(
 add_filter(
 	'wp_nav_menu_items',
 	function ( $item, $args ) {
-		if ( $args->theme_location == 'anony-main-menu' ) {
-			$item .= '<li class="anony-search-form-toggle active"><a href="#"><i class="fa fa-search"></i></a></li>';
+		if ( 'anony-main-menu' === $args->theme_location ) {
+			$item .= '<li class="anony-search-form-toggle active"><a href="#"><svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g clip-path="url(#clip0_15_152)">
+				<rect width="24" height="24" fill="none"/>
+				<circle cx="10.5" cy="10.5" r="6.5" stroke="#ffffff" stroke-linejoin="round"/>
+				<path d="M19.6464 20.3536C19.8417 20.5488 20.1583 20.5488 20.3536 20.3536C20.5488 20.1583 20.5488 19.8417 20.3536 19.6464L19.6464 20.3536ZM20.3536 19.6464L15.3536 14.6464L14.6464 15.3536L19.6464 20.3536L20.3536 19.6464Z" fill="#ffffff"/>
+				</g>
+				<defs>
+				<clipPath id="clip0_15_152">
+				<rect width="24" height="24" fill="white"/>
+				</clipPath>
+				</defs>
+				</svg></a></li>';
 			return $item;
 		} else {
 			return $item;
