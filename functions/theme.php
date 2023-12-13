6576dfb940d571610a3aa137bf01a0617cf26cb9
makiomar <mo7amed.maki@gmail.com>
2023-12-13 15:10:57 +0200

    Add new thumbnail sizes

--- functions/theme.php
+++ functions/theme.php
@@ -133,6 +133,8 @@ function anony_thumbs_sizes() {
 	add_image_size( 'category-post-thumb', 495 ); // 300 pixels wide (and unlimited height)
 	add_image_size( 'popular-post-thumb', 60, 60, true ); // 60*60 pixels and crop
 	add_image_size( 'download-thumb', 195, 250, true ); // 195*250 pixels and crop
+	add_image_size( 'thumb-80-80', 80, 80, true ); // 195*250 pixels and crop
+	add_image_size( 'thumb-50-50', 50, 50, true ); // 195*250 pixels and crop
 }
 
 add_action(
