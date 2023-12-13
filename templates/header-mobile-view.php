7ed734b0c02d49634c5d0bcea4e94dd68c9aad41
makiomar <mo7amed.maki@gmail.com>
2023-12-13 15:13:21 +0200

    Create a mobile view for head

---
+++ templates/header-mobile-view.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Header bobile template
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+require 'document-head.php';
