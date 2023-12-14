c5d8628f351718226cebc9e8717f4a1e90c992e1
makiomar <mo7amed.maki@gmail.com>
2023-12-14 10:55:18 +0200

    Allow zooming

--- templates/document-head.php
+++ templates/document-head.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 <meta http-equiv="Content-Type" content="<?php echo esc_html( $content_type ); ?>" charset="<?php echo esc_html( $content_type ); ?>"/>
 <meta http-equiv="x-ua-compatible" content="IE=edge" >
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
 <?php wp_head(); ?>
 </head>
 <?php //phpcs:disable ?>
