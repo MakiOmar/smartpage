4a8344ed3b985a83de73bde17d58ba12323c94a6
makiomar <mo7amed.maki@gmail.com>
2023-12-14 23:20:13 +0200

    Add logo to mobile header

--- templates/header-mobile-view.php
+++ templates/header-mobile-view.php
@@ -21,7 +21,13 @@ require 'document-head.php';
 	<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
 		<a class="anony-mobile-menu-button anony-inline-flex" href="#" title="Menu"><svg width="30px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Layer_1" x="0px" y="0px" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;" xml:space="preserve"><g> <rect y="312.5" width="455" height="30"></rect> <rect y="212.5" width="455" height="30"></rect> <rect y="112.5" width="455" height="30"></rect></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></a>
 	</div>
-	<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center"></div>
+	<div id="header-mobile-logo" class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
+		<?php
+		// phpcs:disable
+		echo anony_get_theme_logo();
+		// phpcs:enable.
+		?>
+	</div>
 	<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
 		<?php require 'partials/mini-cart.php'; ?>
 	</div>
