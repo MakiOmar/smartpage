0811b91a529552eeba96cb14d5e66bdf60df6392
makiomar <mo7amed.maki@gmail.com>
2023-12-14 10:55:50 +0200

    Add title attribute

--- templates/footer-view.php
+++ templates/footer-view.php
@@ -18,7 +18,7 @@
 
 	<div id="anony-page-scroll-bg"></div>
 	 
-	<a href="#" id="anony-page-scroll"><i class="fa fa-angle-down fa-3x"></i></a>
+	<a href="#" title="<?php esc_attr_e( 'Page scroll', 'smartpage' ); ?>" id="anony-page-scroll"><i class="fa fa-angle-down fa-3x"></i></a>
 	 
 </div>
 
