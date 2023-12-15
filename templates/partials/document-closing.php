fdf2cad8bc38b9c6ef40e710fc52b59b5c098a40
makiomar <mo7amed.maki@gmail.com>
2023-12-15 16:54:30 +0200

    Create document closing partial

---
+++ templates/partials/document-closing.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Document closing
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<?php wp_footer(); ?> 
+</body>
+</html>
