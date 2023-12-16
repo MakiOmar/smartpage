771a3945f0fadca9407c34030209ab9644e3cc3f
makiomar <mo7amed.maki@gmail.com>
2023-12-16 10:24:09 +0200

    Remove AD code

--- templates/footer-view.php
+++ templates/footer-view.php
@@ -16,15 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <footer id="anony-footer" class="anony-grid-col-md-12 anony-grid-col">
 	 
-	<?php if ( $footer_ad ) : ?>
-	 
-		<div class="anony-ad">
-
-		<?php do_action( 'footer_ad' ); ?>
-		 
-		</div>
-	 
-	<?php endif ?>
 	 
 	<p><?php echo wp_kses_post( $copyright ); ?></p>
 	<?php
