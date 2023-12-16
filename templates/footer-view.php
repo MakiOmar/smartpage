388306c2f51b281a9be436c0b9aec41e2c72ade1
makiomar <mo7amed.maki@gmail.com>
2023-12-16 16:41:39 +0200

    Remove dummy content

--- templates/footer-view.php
+++ templates/footer-view.php
@@ -14,10 +14,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<footer id="anony-footer" class="anony-grid-col-md-12 anony-grid-col">
-	 
-	 
-	<p><?php echo wp_kses_post( $copyright ); ?></p>
+<footer id="anony-footer" class="anony-grid-col">
+	<div class="anony-grid-row">
+		<div class="anony-grid-col anony-grid-col-sm-4">
+			
+			<h1>Important links</h1>
+			
+		</div>
+		<div class="anony-grid-col anony-grid-col-sm-4"></div>
+		<div class="anony-grid-col anony-grid-col-sm-4"></div>
+	</div>
+	<div class="anony-grid-row anony-footer-bottom">
+		<div class="anony-grid-col anony-grid-col-sm-4"><p><?php echo wp_kses_post( $copyright ); ?></p></div>
+	</div>
 	<?php
 	if ( wp_is_mobile() ) {
 		get_template_part( 'templates/partials/mobile-footer-menu', 'view' );
