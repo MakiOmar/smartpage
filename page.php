459602a47f6c6a7e283b88be8e9be1c47efb3140
makiomar <mo7amed.maki@gmail.com>
2023-12-18 17:37:09 +0200

    Consider gutenberg blocks

--- page.php
+++ page.php
@@ -14,16 +14,13 @@ defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 
 get_header();
 
-// Check if the Elementor plugin is active.
-if ( is_plugin_active( 'elementor/elementor.php' ) ) {
-	// Check if the current page is built with Elementor.
-	if ( get_post_meta( get_the_ID(), '_elementor_edit_mode', true ) === 'builder' ) {
+// Check if built with elementor or gutenburg blocks.
+if ( have_posts() && ( is_plugin_active( 'elementor/elementor.php' ) && get_post_meta( get_the_ID(), '_elementor_edit_mode', true ) === 'builder' || has_blocks() ) ) {
+	while ( have_posts() ) :
+		the_post();
 		the_content();
-	} else {
-		get_template_part( 'templates/page', 'view' );
-	}
+	endwhile;
 } else {
-	// Elementor plugin is not active.
 	get_template_part( 'templates/page', 'view' );
 }
 
