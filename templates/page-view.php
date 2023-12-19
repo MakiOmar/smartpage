5127788a8d0537dd76662a893d862761edf50c58
makiomar <mo7amed.maki@gmail.com>
2023-12-19 02:46:19 +0200

    Change page markup

--- templates/page-view.php
+++ templates/page-view.php
@@ -15,49 +15,26 @@ defined( 'ABSPATH' ) || die();
 $anony_options = ANONY_Options_Model::get_instance();
 ?>
 
-<div class="anony-grid">
+<div class="anony-grid-row flex-h-center">
 
-	<div class="anony-grid-row anony-grid-col flex-h-center">
-
-		<div class="anony-grid-col<?php ( 'no-sidebar' !== $anony_options->sidebar ) ? ' anony-grid-col-sm-9-5' : ''; ?>">
-
-			<div class="anony-grid-col flex-h-center">
-
-				<?php
-				if ( have_posts() ) :
-
-					while ( have_posts() ) :
-
-						the_post();
-						?>
-
-						<div class="anony-grid-col anony-post-contents">
-
-							<div class="anony-post-info">
-
-								<div class="anony-single-text">
-									<?php the_content(); ?>
-								</div>
-
-							</div>
-
-						</div>
-
-						<?php
-					endwhile;
-				endif;
-				?>
-
-			</div>
-
-		</div>
+	<div class="anony-content anony-grid-col<?php ( 'no-sidebar' !== $anony_options->sidebar ) ? ' anony-grid-col-sm-9-5' : ''; ?>">
 
 		<?php
-		if ( 'no-sidebar' !== $anony_options->sidebar ) {
-			get_sidebar();
-		}
-		?>
+		if ( have_posts() ) :
 
+			while ( have_posts() ) :
+
+				the_post();
+				the_content();
+			endwhile;
+		endif;
+		?>
 	</div>
+
+	<?php
+	if ( 'no-sidebar' !== $anony_options->sidebar ) {
+		get_sidebar();
+	}
+	?>
 
 </div>
