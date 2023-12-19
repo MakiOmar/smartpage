960717e34737e44fb005c2097f2837ecc22a19ed
makiomar <mo7amed.maki@gmail.com>
2023-12-19 18:54:38 +0200

    Add layout buttons to TinyMce

--- functions/mce/cmce-plugin.js
+++ functions/mce/cmce-plugin.js
@@ -12,21 +12,43 @@
 						icon : 'anony-shortcode',
 						classes: 'widget btn anony-shortcode',
 						menu : [
-						{
-							text : 'Content',
-							menu : [ {
-								text : 'Inline English',
-								onclick : function () {
-									editor.insertContent( '[ltrtext][/ltrtext]' );
-								}
-							}, {
-								text : 'Neat quote',
-								onclick : function () {
-									editor.insertContent( '[quoteit][/quoteit]' );
-								}
+							{
+								text : 'Content',
+								menu : [
+									{
+										text : 'Inline English',
+										onclick : function () {
+											editor.insertContent( '[ltrtext][/ltrtext]' );
+										}
+									},
+									{
+										text : 'Neat quote',
+										onclick : function () {
+											editor.insertContent( '[quoteit][/quoteit]' );
+										}
+									}
+								]
 							},
-							]
-						}]
+							{
+
+								text : 'Layout',
+								menu : [
+									{
+										text : 'Row',
+										onclick : function () {
+											editor.insertContent( '[anony_row][/anony_row]' );
+										}
+									},
+									
+									{
+										text : 'column',
+										onclick : function () {
+											editor.insertContent( '[anony_column classes=""][/anony_column]' );
+										}
+									},
+								]
+							}
+						]
 
 					}
 				);
