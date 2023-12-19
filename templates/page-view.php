ea94366baa706c0351d7fd767433c42b4ccd2b50
makiomar <mo7amed.maki@gmail.com>
2023-12-19 18:56:13 +0200

    Change css classes

--- templates/page-view.php
+++ templates/page-view.php
@@ -17,7 +17,7 @@ $anony_options = ANONY_Options_Model::get_instance();
 
 <div class="anony-grid-row flex-h-center">
 
-	<div class="anony-content anony-grid-col<?php ( 'no-sidebar' !== $anony_options->sidebar ) ? ' anony-grid-col-sm-9-5' : ''; ?>">
+	<div class="anony-content<?php ( 'no-sidebar' !== $anony_options->sidebar ) ? ' anony-grid-col anony-grid-col-sm-9-5' : ''; ?>">
 
 		<?php
 		if ( have_posts() ) :
