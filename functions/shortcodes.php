13aa19335f09ed831f5f36362420c737c5912180
makiomar <mo7amed.maki@gmail.com>
2023-12-21 14:34:45 +0200

    Convert featured slider to shortcode

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -20,10 +20,122 @@ $shcods = array(
 	'anony_row',
 	'anony_column',
 	'anony_shape_divider',
+	'anony_posts_slider',
 );
 
 foreach ( $shcods as $code ) {
 	add_shortcode( $code, $code . '_shcode' );
+}
+
+/**
+ * Renders posts slider
+ *
+ * @param  string $atts the shortcode attributes.
+ * @return string
+ */
+function anony_posts_slider_shcode( $atts ) {
+	$atts = shortcode_atts(
+		array(
+			'slider_content' => '', // Accepts (featured-cat or featured-post).
+			'featured_cat'   => '0',
+			'taxonomy'       => 'category',
+			'post_type'      => 'post',
+		),
+		$atts,
+		'anony_posts_slider'
+	);
+
+	$anony_options   = ANONY_Options_Model::get_instance();
+	$slider_settings = array(
+
+		'style'           => 'one',
+		'show_read_more'  => false,
+		'show_pagination' => true,
+		'pagination_type' => 'dots', // Accepts (thumbnails or dots).
+		'slider_data'     => array(
+			'transition' => 5000,
+			'animation'  => 1500,
+		),
+	);
+
+	$message = '';
+
+	$args = array(
+		'post_type'      => $atts['post_type'],
+		'posts_per_page' => 5,
+		'order'          => 'ASC',
+		// phpcs:disable WordPress.DB.SlowDBQuery.slow_db_query_meta_query
+		'meta_query'     => array(
+			array(
+				'key'     => '_thumbnail_id',
+				'compare' => 'EXISTS',
+			),
+		),
+		// phpcs:enable.
+	);
+
+	if ( 'featured-cat' === $atts['slider_content'] && '0' !== $atts['featured_cat'] ) {
+
+		$freatured_cat = get_term_by(
+			'id',
+			absint( $atts['featured_cat'] ),
+			$atts['taxonomy']
+		);
+
+		if ( $freatured_cat ) {
+			$args['cat'] = $freatured_cat->term_id;
+		} else {
+			$message = esc_html__( 'Please make sure you select a category and its corresponding taxonomy from theme options->slider', 'smartpage' );
+		}
+	} elseif ( 'featured-post' === $anony_options->slider_content ) {
+		// phpcs:disable
+		$args['meta_key'] = 'anony__set_as_featured';
+		// phpcs:enable.
+	}
+
+	$query = new WP_Query( $args );
+
+	$data = array();
+
+	if ( $query->have_posts() ) {
+
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			$data[] = anony_common_post_data();
+		}
+
+		wp_reset_postdata();
+	}
+
+	$count = count( $data );
+
+	if ( $slider_settings['show_pagination'] ) {
+
+		$slider_nav = array();
+
+		foreach ( $data as $index => $p ) :
+
+			$slider_nav_temp['permalink']     = $p['permalink'];
+			$slider_nav_temp['id']            = $p['id'];
+			$slider_nav_temp['title']         = $p['title'];
+			$slider_nav_temp['class']         = 0 === $index ? 'anony-active-slide ' : '';
+			$slider_nav_temp['thumbnail_img'] = get_the_post_thumbnail( $p['id'], 'full' );
+
+			$slider_nav[] = $slider_nav_temp;
+
+		endforeach;
+	}
+
+	$title_link = isset( $args['cat'] ) ? get_category_link( $args['cat'] ) : '#';
+
+	$title_text = isset( $args['cat'] ) ? get_cat_name( $args['cat'] ) : __( 'Featured Posts', 'smartpage' );
+
+	$output = '';
+	ob_start();
+	require locate_template( 'templates/featured-' . $slider_settings['style'] . '-view.php', false, false );
+	$output .= ob_get_clean();
+	wp_enqueue_script( 'anony-featured-slider' );
+	return $output;
 }
 
 /**
@@ -96,7 +208,7 @@ function anony_row_shcode( $atts, $content = null ) {
 	} else {
 		$class = '';
 	}
-	return '<div class="anony-grid-row'. $class .'">' . do_shortcode( $content ) . '</div>';
+	return '<div class="anony-grid-row' . $class . '">' . do_shortcode( $content ) . '</div>';
 }
 
 /**
