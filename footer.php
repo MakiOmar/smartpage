f22a41a83e29699d02c519882b9a48106abb6fd0
makiomar <mo7amed.maki@gmail.com>
2023-12-21 11:47:24 +0200

    Remove unneccessary lines

--- footer.php
+++ footer.php
@@ -15,11 +15,5 @@ defined( 'ABSPATH' ) || die(); // Exit if accessed direct.
 if ( ! defined( 'ANOENGINE' ) ) {
 	require_once 'footer-default.php';
 } else {
-	$anony_options = ANONY_Options_Model::get_instance();
-
-	$copyright = esc_html( $anony_options->copyright );
-
-	$footer_ad = has_action( 'footer_ad' );
-
 	require locate_template( 'templates/footer-view.php', false, false );
 }
