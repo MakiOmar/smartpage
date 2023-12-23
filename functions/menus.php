7f98cdd7d88daff5e09522e86ef6e3f6a3ef936e
makiomar <mo7amed.maki@gmail.com>
2023-12-23 06:04:13 +0200

    WPCS

--- functions/menus.php
+++ functions/menus.php
@@ -34,7 +34,7 @@ function anony_navigation( $location_slug, $container = 'nav' ) {
 	$menu_id = $location_slug . '-con';
 
 	if ( has_nav_menu( $location_slug ) ) {
-		$args           = array(
+		$args = array(
 			'theme_location' => $location_slug,
 			'depth'          => 0,
 			'menu_id'        => $menu_id,
@@ -46,7 +46,7 @@ function anony_navigation( $location_slug, $container = 'nav' ) {
 			$args['walker'] = $walker;
 		}
 			return wp_nav_menu( $args );
-	} elseif ( 'anony-main-menu' == $location_slug ) {
+	} elseif ( 'anony-main-menu' === $location_slug ) {
 			$page_for_posts = get_option( 'page_for_posts' );
 			$args           = array(
 				'title_li' => 0,
@@ -70,40 +70,42 @@ function anony_navigation( $location_slug, $container = 'nav' ) {
 function anony_breadcrumbs() {
 	global $post, $wp;
 	$page_link = home_url( $wp->request );
-	$home_link = home_url();
+	$time_y    = get_the_time( 'Y' );
+	$time_f    = get_the_time( 'F' );
 	echo '<ul class="anony-breadcrumbs">';
-	echo '<li class="home"><i class="fa fa-home"></i> <a href="' . $home_link . '">' . esc_html__( 'Home', 'smartpage' ) . '</a> <span>/</span></li>';
+	echo '<li class="home"><i class="fa fa-home"></i> <a href="' . esc_url( home_url() ) . '">' . esc_html__( 'Home', 'smartpage' ) . '</a> <span>/</span></li>';
 
 	// Blog Category.
 	if ( is_category() ) {
-		// echo '<li><a href="'. $page_link .'">'. single_cat_title('', false) . '</a></li>';.
+		//phpcs:disable WordPress.WP.DiscouragedFunctions.wp_reset_query_wp_reset_query
 		wp_reset_query();
+		//phpcs:enable.
 		$incurr_category    = get_category( get_query_var( 'cat' ) );
 		$incurr_category_id = $incurr_category->cat_ID;
 		echo '<li class="parent-categories">';
-		echo get_category_parents( $incurr_category_id, true, ' / ' );
+		echo wp_kses( get_category_parents( $incurr_category_id, true, ' / ' ), array( 'a' => array( 'href' => array() ) ) );
 		echo '</li>';
 
 		// Blog Day.
 	} elseif ( is_day() ) {
-		echo '<li><a href="' . get_year_link( get_the_time( 'Y' ) ) . '">' . get_the_time( 'Y' ) . '</a> <span>/</span></li>';
-		echo '<li><a href="' . get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) ) . '">' . get_the_time( 'F' ) . '</a> <span>/</span></li>';
-		echo '<li><a href="' . $page_link . '">' . get_the_time( 'd' ) . '</a></li>';
+		echo '<li><a href="' . esc_url( get_year_link( esc_html( $time_y ) ) ) . '">' . esc_html( $time_y ) . '</a> <span>/</span></li>';
+		echo '<li><a href="' . esc_url( get_month_link( esc_html( $time_y ), get_the_time( 'm' ) ) ) . '">' . esc_html( $time_f ) . '</a> <span>/</span></li>';
+		echo '<li><a href="' . esc_url( $page_link ) . '">' . esc_html( get_the_time( 'd' ) ) . '</a></li>';
 
 		// Blog Month.
 	} elseif ( is_month() ) {
-		echo '<li><a href="' . get_year_link( get_the_time( 'Y' ) ) . '">' . get_the_time( 'Y' ) . '</a> <span>/</span></li>';
-		echo '<li><a href="' . $page_link . '">' . get_the_time( 'F' ) . '</a></li>';
+		echo '<li><a href="' . esc_url( get_year_link( esc_html( $time_y ) ) ) . '">' . esc_html( $time_y ) . '</a> <span>/</span></li>';
+		echo '<li><a href="' . esc_url( $page_link ) . '">' . esc_html( $time_f ) . '</a></li>';
 
 		// Blog Year.
 	} elseif ( is_year() ) {
-		echo '<li><a href="' . $page_link . '">' . get_the_time( 'Y' ) . '</a></li>';
+		echo '<li><a href="' . esc_url( $page_link ) . '">' . esc_html( $time_y ) . '</a></li>';
 
 		// Single Post.
 	} elseif ( is_single() && ! is_attachment() ) {
 
 		// Custom post type.
-		if ( get_post_type() != 'post' ) {
+		if ( get_post_type() !== 'post' ) {
 			$post_type = get_post_type_object( get_post_type() );
 			$slug      = $post_type->rewrite;
 
@@ -112,44 +114,49 @@ function anony_breadcrumbs() {
 			$cat = get_the_category();
 			$cat = $cat[0];
 			echo '<li>';
-			echo get_category_parents( $cat, true, ' <span>/</span>' );
+			echo wp_kses(
+				get_category_parents( $cat, true, ' / ' ),
+				array(
+					'a'    => array( 'href' => array() ),
+					'span' => array(),
+				)
+			);
 			echo '</li>';
-			echo '<li><a href="' . $page_link . '">' . wp_title( '', false ) . '</a></li>';
+			echo '<li><a href="' . esc_url( $page_link ) . '">' . wp_title( '', false ) . '</a></li>';
 		}
 
 		// Taxonomy.
 	} elseif ( get_post_taxonomies() ) {
-
+		echo '';
 		// Page with parent.
 	} elseif ( is_page() && $post->post_parent ) {
 		$parent_id   = $post->post_parent;
 		$breadcrumbs = array();
 		while ( $parent_id ) {
-			$page          = get_page( $parent_id );
+			$page          = get_post( $parent_id );
 			$breadcrumbs[] = '<li><a href="' . get_permalink( $page->ID ) . '">' . get_the_title( $page->ID ) . '</a> <span>/</span></li>';
 			$parent_id     = $page->post_parent;
 		}
 		$breadcrumbs = array_reverse( $breadcrumbs );
 		foreach ( $breadcrumbs as $crumb ) {
-			echo $crumb;
+			echo wp_kses_post( $crumb );
 		}
 
-		echo '<li><a href="' . $page_link . '">' . get_the_title() . '</a></li>';
+		echo '<li><a href="' . esc_url( $page_link ) . '">' . esc_html( get_the_title() ) . '</a></li>';
 
-		// Default
-	} elseif ( get_the_title() != 'Home' ) {
-		echo '<li><a href="' . $page_link . '">' . get_the_title() . '</a></li>';
+		// Default.
+	} elseif ( get_the_title() !== 'Home' ) {
+		echo '<li><a href="' . esc_url( $page_link ) . '">' . esc_html( get_the_title() ) . '</a></li>';
 	}
 
 	echo '</ul>';
 }
 
-/*
--------------------------------------------------------------
+/**
  * Menus hooks
- *-----------------------------------------------------------*/
+*/
 
-// Register theme menus
+// Register theme menus.
 add_action(
 	'after_setup_theme',
 	function () {
@@ -169,30 +176,29 @@ add_action(
 // Adds active class to the currently active menu item.
 add_filter(
 	'nav_menu_css_class',
-	function ( $classes, $item ) {
+	function ( $classes ) {
 
-		if ( in_array( 'current-menu-item', $classes ) ) {
+		if ( in_array( 'current-menu-item', $classes, true ) ) {
 			$classes[] = 'active ';
 		}
 		return $classes;
 	},
-	10,
-	2
+	10
 );
 
 // Adds WPML language switcher to languages-menu location.
 add_filter(
 	'wp_nav_menu_items',
-	function ( $item, $args ) {
+	function ( $item ) {
 
-		if ( ! class_exists( 'ANONY_Wpml_Help' ) || ! ANONY_Wpml_Help::is_active() ) {
+		if ( ! class_exists( 'ANONY_Wpml_Help' ) || ! ANONY_Wpml_Help::is_active() || ! function_exists( 'icl_get_languages' ) ) {
 			return $item;
 		}
 
 		$languages = icl_get_languages( 'skip_missing=0' );
 		if ( ! empty( $languages ) ) {
 			foreach ( $languages as $l ) {
-				if ( $l['language_code'] == ICL_LANGUAGE_CODE ) {
+				if ( ICL_LANGUAGE_CODE === $l['language_code'] ) {
 						$curr_lang = $l;
 				}
 				$item .= '<li class="anony-lang">';
@@ -205,11 +211,14 @@ add_filter(
 		}
 		return $item;
 	},
-	10,
-	2
 );
 
-
+/**
+ * Get WPMLswitcher
+ *
+ * @param string $style switcher style.
+ * @return void
+ */
 function anony_get_wpml_switcher( $style = '1' ) {
 	if ( ! class_exists( 'ANONY_Wpml_Help' ) || ! ANONY_Wpml_Help::is_active() ) {
 		return;
@@ -221,7 +230,7 @@ function anony_get_wpml_switcher( $style = '1' ) {
 
 		foreach ( $languages as $l ) {
 
-			if ( $l['language_code'] == ICL_LANGUAGE_CODE ) {
+			if ( ICL_LANGUAGE_CODE === $l['language_code'] ) {
 				$curr_lang_flag = $l['country_flag_url'];
 			}
 
@@ -241,24 +250,20 @@ function anony_get_wpml_switcher( $style = '1' ) {
 // Adds the active class to menu item of current active page.
 add_filter(
 	'page_css_class',
-	function ( $css_class, $page, $depth, $args, $current_page ) {
-		if ( in_array( 'current_page_item', $css_class ) ) {
+	function ( $css_class ) {
+		if ( in_array( 'current_page_item', $css_class, true ) ) {
 			$css_class[] = 'active ';
 		}
 		return $css_class;
 	},
-	10,
-	5
 );
 
 // add a menu item for homepage to pages menu.
 add_filter(
 	'wp_list_pages',
-	function ( $output, $r, $pages ) {
+	function ( $output ) {
 		$home   = '<li><a href="' . get_home_url() . '">' . __( '<i class="fa fa-home"></i>', 'smartpage' ) . '</a></li>';
 		$output = $home . $output;
 		return $output;
 	},
-	10,
-	3
 );
