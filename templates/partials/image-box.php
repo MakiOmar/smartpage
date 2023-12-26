7b41bd720607c737bf3f610435f80b1ef6e4b2b1
makiomar <mo7amed.maki@gmail.com>
2023-12-26 14:16:25 +0200

    Add ability to lazyload image

--- templates/partials/image-box.php
+++ templates/partials/image-box.php
@@ -17,9 +17,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="anony-image-box-wrapper">
 	<div class="anony-grid-row anony-inline-flex">
 		<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-v-center">
-		<?php if ( $image_url && ! empty( $image_url ) ) { ?>
-			<img width="55" height="68" src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_box_title ); ?>"/>
-		<?php } ?>
+		<?php
+		if ( $image_url && ! empty( $image_url ) ) {
+			if ( class_exists( 'ANONY_Options_Model' ) ) {
+				$anofl_options = ANONY_Options_Model::get_instance( 'Anofl_Options' );
+				if ( '1' === $anofl_options->add_missing_image_dimensions && '1' === $anofl_options->lazyload_images ) {
+					?>
+					<img loading="lazy" data-src="<?php echo esc_url( $image_url ); ?>" src="data:image/svg+xml,%3Csvg%20xmlns=\'http://www.w3.org/2000/svg\'%20viewBox=\'0%200%20225%20225\'%3E%3C/svg%3E" alt="<?php echo esc_attr( $image_box_title ); ?>">
+					<?php
+				}
+			} else {
+				?>
+				<img width="55" height="68" src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_box_title ); ?>"/>
+				<?php
+			}
+		}
+		?>
 		</div>
 		<div class="anony-grid-col anony-grid-col-8 anony-inline-flex flex-v-center anony-flex-column">
 			<?php if ( $image_box_title && ! empty( $image_box_title ) ) { ?>
