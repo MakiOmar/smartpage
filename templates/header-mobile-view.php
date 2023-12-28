4d0b842876ca5fe9a340a39b29f2a811ca507e70
makiomar <mo7amed.maki@gmail.com>
2023-12-28 22:20:42 +0200

    Call popup shortcode

--- templates/header-mobile-view.php
+++ templates/header-mobile-view.php
@@ -19,7 +19,8 @@ require 'document-head.php';
 
 <header id="anony-mobile-header" class="anony-grid-row">
 	<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
-		<a class="anony-mobile-menu-button anony-inline-flex" href="#" title="Menu"><svg width="30px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Layer_1" x="0px" y="0px" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;" xml:space="preserve"><g> <rect y="312.5" width="455" height="30"></rect> <rect y="212.5" width="455" height="30"></rect> <rect y="112.5" width="455" height="30"></rect></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></a>
+		<a class="anony-mobile-menu-button anony-inline-flex anony-popup-toggle" href="#" data-target="anony-mobile-menu" title="Menu"><svg width="30px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Layer_1" x="0px" y="0px" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;" xml:space="preserve"><g> <rect y="312.5" width="455" height="30"></rect> <rect y="212.5" width="455" height="30"></rect> <rect y="112.5" width="455" height="30"></rect></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></a>
+		<?php echo do_shortcode( '[anony_popup id="anony-mobile-menu"]' ); ?>
 	</div>
 	<div id="header-mobile-logo" class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
 		<?php
