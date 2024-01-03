c58d99808ae45e8d6c08ca23b0908e8f80124192
makiomar <mo7amed.maki@gmail.com>
2024-01-03 16:56:27 +0200

    Add css shadow class

--- templates/header-mobile-view.php
+++ templates/header-mobile-view.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 require 'document-head.php';
 ?>
 
-<header id="anony-mobile-header" class="anony-grid-row">
+<header id="anony-mobile-header" class="anony-grid-row anony-box-shadow">
 	<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
 		<a class="anony-mobile-menu-button anony-inline-flex anony-popup-toggle" href="#" data-target="anony-mobile-menu" title="Menu"><svg width="30px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Layer_1" x="0px" y="0px" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;" xml:space="preserve"><g> <rect y="312.5" width="455" height="30"></rect> <rect y="212.5" width="455" height="30"></rect> <rect y="112.5" width="455" height="30"></rect></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></a>
 		<?php echo do_shortcode( '[anony_popup id="anony-mobile-menu"]' ); ?>
