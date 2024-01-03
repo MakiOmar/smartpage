b08a861e16ac8401461c0b76209a33b0f84329d0
makiomar <mo7amed.maki@gmail.com>
2024-01-03 19:23:57 +0200

    One query for all orders' statuses

--- functions/woocommerce-orders.php
+++ functions/woocommerce-orders.php
@@ -15,26 +15,74 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Render status orders
+ *
+ * @param array  $orders Orders array.
+ * @param string $status Order status.
+ * @return string
+ */
+function anony_render_status_orders( $orders, $status ) {
+	$money_icon      = '<svg xmlns="http://www.w3.org/2000/svg" width="18.067" height="18.067" viewBox="0 0 18.067 18.067"><path id="coins_2_" data-name="coins (2)" d="M12.421,0C9.255,0,6.775,1.488,6.775,3.387V5.335a9.647,9.647,0,0,0-1.129-.065C2.48,5.269,0,6.758,0,8.657v6.022c0,1.9,2.48,3.387,5.646,3.387,2.565,0,4.679-.976,5.39-2.357a9.63,9.63,0,0,0,1.385.1c3.166,0,5.646-1.488,5.646-3.387V3.387C18.067,1.488,15.587,0,12.421,0Zm4.14,9.41c0,.888-1.771,1.882-4.14,1.882a8.157,8.157,0,0,1-1.129-.078V9.721a9.784,9.784,0,0,0,1.129.065,7.787,7.787,0,0,0,4.14-1.062ZM1.506,10.982a7.787,7.787,0,0,0,4.14,1.062,7.787,7.787,0,0,0,4.14-1.062v.686c0,.888-1.771,1.882-4.14,1.882s-4.14-.994-4.14-1.882ZM16.561,6.4c0,.888-1.771,1.882-4.14,1.882A8.161,8.161,0,0,1,11.242,8.2,2.982,2.982,0,0,0,9.958,6.448a9.043,9.043,0,0,0,2.463.327,7.787,7.787,0,0,0,4.14-1.062Zm-4.14-4.893c2.37,0,4.14.994,4.14,1.882s-1.771,1.882-4.14,1.882-4.14-.994-4.14-1.882S10.051,1.506,12.421,1.506ZM5.646,6.775c2.37,0,4.14.994,4.14,1.882s-1.771,1.882-4.14,1.882-4.14-.994-4.14-1.882S3.276,6.775,5.646,6.775Zm0,9.786c-2.37,0-4.14-.994-4.14-1.882v-.686a7.787,7.787,0,0,0,4.14,1.062,7.787,7.787,0,0,0,4.14-1.062v.686C9.786,15.567,8.016,16.561,5.646,16.561ZM12.421,14.3a8.156,8.156,0,0,1-1.129-.078V12.732a9.784,9.784,0,0,0,1.129.065,7.787,7.787,0,0,0,4.14-1.062v.686C16.561,13.309,14.791,14.3,12.421,14.3Z" fill="#f7cc3f"/></svg>';
+	$wp_button_class = wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '';
+	ob_start();
+	foreach ( $orders as $order ) {
+		echo '<div class="anony-grid-col-sm-6">';
+		echo '<div class="order-container">';
+		echo '<div class="order-row">';
+		echo '<div class="order-number">' . esc_html__( 'Order number:', 'woocommerce' ) . ' #' . esc_html( $order->get_order_number() ) . '</div>';
+		echo '<div class="order-status"><span class="status-indicator ' . esc_html( $status ) . '"></span> ' . esc_html( wc_get_order_status_name( $order->get_status() ) ) . '</div>';
+		echo '</div>';
+		echo '<div class="order-row">';
+		echo '<div class="order-date">' . esc_html( wc_format_datetime( $order->get_date_created() ) ) . '</div>';
+		echo '</div>';
+		echo '<div class="order-row">';
+		echo '<div class="order-total-label">' . esc_html__( 'Total:', 'woocommerce' ) . '</div>';
+		// phpcs:disable
+		echo '<div class="order-total-value">' . $money_icon . ' ' . wp_kses_post( $order->get_formatted_order_total() ) . '</div>';
+		// phpcs:enable
+		echo '</div>';
+		$actions = wc_get_account_orders_actions( $order );
+
+		if ( ! empty( $actions ) ) {
+			echo '<div class="anony-flex anony-flex-end anony-order-actions">';
+			foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+				echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-button' . esc_attr( $wp_button_class ) . ' button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+			}
+			echo '</div>';
+		}
+
+		echo '</div>';
+		echo '</div>';
+
+	}
+
+	return ob_get_clean();
+}
+/**
  * Render orders function
  *
- * @param string $status Order status.
+ * @param array  $statuses An array of order status.
+ * @param string $has_orders If user has orders.
  * @param int    $current_page Current page.
  * @return void
  */
-function anony_app_render_orders( $status, $current_page = 1 ) {
+function anony_app_render_orders( $statuses, $has_orders, $current_page = 1 ) {
 	$current_page    = empty( $current_page ) ? 1 : absint( $current_page );
 	$wp_button_class = wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '';
-	$customer_orders = wc_get_orders(
+	$anony_options   = ANONY_Options_Model::get_instance();
+
+	$orders_query_args = apply_filters(
+		'woocommerce_my_account_my_orders_query',
 		array(
-			'status'         => array( $status ),
+			'status'         => array_keys( $statuses ),
 			'page'           => $current_page,
-			'posts_per_page' => -1,
+			'posts_per_page' => ! empty( $anony_options->orders_per_page ) ? $anony_options->orders_per_page : 20,
 			'paginate'       => true,
 			'customer_id'    => get_current_user_id(),
 		)
 	);
 
-	$money_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="18.067" height="18.067" viewBox="0 0 18.067 18.067"><path id="coins_2_" data-name="coins (2)" d="M12.421,0C9.255,0,6.775,1.488,6.775,3.387V5.335a9.647,9.647,0,0,0-1.129-.065C2.48,5.269,0,6.758,0,8.657v6.022c0,1.9,2.48,3.387,5.646,3.387,2.565,0,4.679-.976,5.39-2.357a9.63,9.63,0,0,0,1.385.1c3.166,0,5.646-1.488,5.646-3.387V3.387C18.067,1.488,15.587,0,12.421,0Zm4.14,9.41c0,.888-1.771,1.882-4.14,1.882a8.157,8.157,0,0,1-1.129-.078V9.721a9.784,9.784,0,0,0,1.129.065,7.787,7.787,0,0,0,4.14-1.062ZM1.506,10.982a7.787,7.787,0,0,0,4.14,1.062,7.787,7.787,0,0,0,4.14-1.062v.686c0,.888-1.771,1.882-4.14,1.882s-4.14-.994-4.14-1.882ZM16.561,6.4c0,.888-1.771,1.882-4.14,1.882A8.161,8.161,0,0,1,11.242,8.2,2.982,2.982,0,0,0,9.958,6.448a9.043,9.043,0,0,0,2.463.327,7.787,7.787,0,0,0,4.14-1.062Zm-4.14-4.893c2.37,0,4.14.994,4.14,1.882s-1.771,1.882-4.14,1.882-4.14-.994-4.14-1.882S10.051,1.506,12.421,1.506ZM5.646,6.775c2.37,0,4.14.994,4.14,1.882s-1.771,1.882-4.14,1.882-4.14-.994-4.14-1.882S3.276,6.775,5.646,6.775Zm0,9.786c-2.37,0-4.14-.994-4.14-1.882v-.686a7.787,7.787,0,0,0,4.14,1.062,7.787,7.787,0,0,0,4.14-1.062v.686C9.786,15.567,8.016,16.561,5.646,16.561ZM12.421,14.3a8.156,8.156,0,0,1-1.129-.078V12.732a9.784,9.784,0,0,0,1.129.065,7.787,7.787,0,0,0,4.14-1.062v.686C16.561,13.309,14.791,14.3,12.421,14.3Z" fill="#f7cc3f"/></svg>';
+	$customer_orders = wc_get_orders( $orders_query_args );
 	?>
 	<style>
 			.order-container {
@@ -113,36 +161,35 @@ function anony_app_render_orders( $status, $current_page = 1 ) {
 		} else {
 			$orders = $customer_orders;
 		}
+		$all_orders = array();
 		foreach ( $orders as $order ) {
-			echo '<div class="anony-grid-col-sm-6">';
-			echo '<div class="order-container">';
-			echo '<div class="order-row">';
-			echo '<div class="order-number">' . esc_html__( 'Order number:', 'woocommerce' ) . ' #' . esc_html( $order->get_order_number() ) . '</div>';
-			echo '<div class="order-status"><span class="status-indicator ' . esc_html( $status ) . '"></span> ' . esc_html( wc_get_order_status_name( $order->get_status() ) ) . '</div>';
-			echo '</div>';
-			echo '<div class="order-row">';
-			echo '<div class="order-date">' . esc_html( wc_format_datetime( $order->get_date_created() ) ) . '</div>';
-			echo '</div>';
-			echo '<div class="order-row">';
-			echo '<div class="order-total-label">' . esc_html__( 'Total:', 'woocommerce' ) . '</div>';
-            // phpcs:disable
-			echo '<div class="order-total-value">' . $money_icon . ' ' . wp_kses_post( $order->get_formatted_order_total() ) . '</div>';
-            // phpcs:enable
-			echo '</div>';
-			$actions = wc_get_account_orders_actions( $order );
-
-			if ( ! empty( $actions ) ) {
-				echo '<div class="anony-flex anony-flex-end anony-order-actions">';
-				foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-					echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-button' . esc_attr( $wp_button_class ) . ' button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
-				}
-				echo '</div>';
-			}
-
-			echo '</div>';
-			echo '</div>';
-
+			$all_orders[ 'wc-' . $order->get_status() ][] = $order;
 		}
+
+		$tab_items = '';
+		$content   = '';
+
+		foreach ( $all_orders as $status => $status_orders ) {
+			$label      = trim( $statuses[ $status ], '()' );
+			$active     = ( 'wc-processing' === $status ) ? ' active-tab' : '';
+			$tab_items .= '<li><a class="order-tab-trigger' . $active . '" href="#" data-target="' . esc_attr( $status ) . '-tab">' . esc_attr( $label ) . '</a></li>';
+
+			//phpcs:disable
+			$content .= '<div class="order-tab-content' . $active . '" id="' . esc_attr( $status ) . '-tab">';
+			//phpcs:enable
+			$content .= anony_render_status_orders( $status_orders, $status );
+			$content .= '</div>';
+		}
+
+		$tabs  = '<ul id="anony-orders-page-tabs" class="horizontal-fancy-tabs">';
+		$tabs .= $tab_items;
+		$tabs .= '</ul>';
+
+		$output = $tabs . '<div id="anony-orders-content" class="anony-grid-row anony-grid-col">' . $content . '</div>';
+
+		echo wp_kses_post( $output );
+
+		do_action( 'woocommerce_before_account_orders', $has_orders );
 
 		do_action( 'woocommerce_before_account_orders_pagination' );
 
@@ -188,6 +235,11 @@ function anony_custom_app_orders( $atts ) {
 	$wp_button_class = wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '';
 	$has_orders      = false;
 
+	$statuses = array(
+		'wc-processing' => esc_html__( 'Processing', 'woocommerce' ),
+		'wc-completed'  => esc_html__( 'Completed', 'woocommerce' ),
+		'wc-cancelled'  => esc_html__( 'Cancel', 'woocommerce' ),
+	);
 	if ( 'yes' === $atts['has_orders'] ) {
 		$has_orders = true;
 	}
@@ -196,40 +248,9 @@ function anony_custom_app_orders( $atts ) {
 		wc_print_notice( esc_html__( 'No order has been made yet.', 'woocommerce' ) . ' <a class="woocommerce-Button button' . esc_attr( $wp_button_class ) . '" href="' . esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ) . '">' . esc_html__( 'Browse products', 'woocommerce' ) . '</a>', 'notice' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
 		return ob_get_clean();
 	}
-	$statuses  = array(
-		'wc-processing' => esc_html__( 'Processing', 'woocommerce' ),
-		'wc-completed'  => esc_html__( 'Completed', 'woocommerce' ),
-		'wc-cancelled'  => esc_html__( 'Cancel', 'woocommerce' ),
-	);
-	$tab_items = '';
-	$content   = '';
-	foreach ( $statuses as $status => $label ) {
-		$label      = trim( $label, '()' );
-		$active     = ( 'wc-processing' === $status ) ? ' active-tab' : '';
-		$tab_items .= '<li><a class="order-tab-trigger' . $active . '" href="#" data-target="' . esc_attr( $status ) . '-tab">' . esc_attr( $label ) . '</a></li>';
-
-		ob_start();
-        //phpcs:disable
-		echo '<div class="order-tab-content' . $active . '" id="' . esc_attr( $status ) . '-tab">';
-        //phpcs:enable
-		anony_app_render_orders( $status, absint( $atts['current_page'] ) );
-		echo '</div>';
-		$content .= ob_get_clean();
-	}
-	$tabs  = '<ul id="anony-orders-page-tabs" class="horizontal-fancy-tabs">';
-	$tabs .= $tab_items;
-	$tabs .= '</ul>';
 	ob_start();
-	do_action( 'woocommerce_before_account_orders', $has_orders );
-	$before_orders = ob_get_clean();
-
-	ob_start();
-	do_action( 'woocommerce_after_account_orders', $has_orders );
-	$after_orders = ob_get_clean();
-
-	$output = $before_orders . $tabs . '<div id="anony-orders-content" class="anony-grid-row anony-grid-col">' . $content . '</div>' . $after_orders;
-
-	return $output;
+	anony_app_render_orders( $statuses, $has_orders, $atts['current_page'] );
+	return ob_get_clean();
 }
 add_action(
 	'wp_footer',
