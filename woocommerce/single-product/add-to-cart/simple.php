7078c92bff12ec6902a6c56a6b11aa19fc53e7bb
makiomar <mo7amed.maki@gmail.com>
2024-01-03 16:58:48 +0200

    Deactivate some hooks if add to cart is in the footer

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -23,12 +23,18 @@ if ( ! $product->is_purchasable() ) {
 	return;
 }
 if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+	//phpcs:disable
 	echo wc_get_stock_html( $product ); // WPCS: XSS ok.
+	//phpcs:enable.
 }
 
 if ( $product->is_in_stock() ) : ?>
 
-	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+	<?php
+	if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+		do_action( 'woocommerce_before_add_to_cart_form' );
+	}
+	?>
 
 	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
@@ -40,7 +46,9 @@ if ( $product->is_in_stock() ) : ?>
 			array(
 				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
 				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+				//phpcs:disable
 				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				//phpcs:enable.
 			)
 		);
 
@@ -52,6 +60,10 @@ if ( $product->is_in_stock() ) : ?>
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
 
-	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+	<?php
+	if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+		do_action( 'woocommerce_after_add_to_cart_form' );
+	}
+	?>
 
 <?php endif; ?>
--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -23,19 +23,35 @@ $attribute_keys  = array_keys( $attributes );
 $variations_json = wp_json_encode( $available_variations );
 $variations_attr = function_exists( 'wc_esc_json' ) ? wc_esc_json( $variations_json ) : _wp_specialchars( $variations_json, ENT_QUOTES, 'UTF-8', true );
 
-do_action( 'woocommerce_before_add_to_cart_form' ); ?>
-
+if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+	do_action( 'woocommerce_before_add_to_cart_form' );
+}
+//phpcs:disable
+?>
 <form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo $variations_attr; // WPCS: XSS ok. ?>">
-	<?php do_action( 'woocommerce_before_variations_form' ); ?>
+	<?php
+	//phpcs:enable.
+	if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+		do_action( 'woocommerce_before_variations_form' );
+	}
+	?>
 
-	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
+	<?php if ( empty( $available_variations ) && false !== $available_variations && ! defined( 'FOOTER_ADD_TO_CART' ) ) : ?>
 		<p class="stock out-of-stock"><?php echo esc_html( apply_filters( 'woocommerce_out_of_stock_message', __( 'This product is currently out of stock and unavailable.', 'woocommerce' ) ) ); ?></p>
 	<?php else : ?>
-		<table class="variations" cellspacing="0" role="presentation">
+		<table class="variations" cellspacing="0" role="presentation" style="display:<?php echo defined( 'FOOTER_ADD_TO_CART' ) ? 'none' : 'block'; ?>">
 			<tbody>
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
 					<tr>
-						<th class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></th>
+						<th class="label">
+							<label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>">
+								<?php
+								//phpcs:disable
+								echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok.
+								//phpcs:enable.
+								?>
+							</label>
+						</th>
 						<td class="value">
 							<?php
 								wc_dropdown_variation_attribute_options(
@@ -52,34 +68,48 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 				<?php endforeach; ?>
 			</tbody>
 		</table>
-		<?php do_action( 'woocommerce_after_variations_table' ); ?>
+		<?php
+		if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+			do_action( 'woocommerce_after_variations_table' );
+		}
+		?>
 
-		<div class="single_variation_wrap">
+		<div class="single_variation_wrap anony-grid-row">
 			<?php
-				/**
-				 * Hook: woocommerce_before_single_variation.
-				 */
+			/**
+			 * Hook: woocommerce_before_single_variation.
+			 */
+			if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
 				do_action( 'woocommerce_before_single_variation' );
+			}
 
-				/**
-				 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
-				 *
-				 * @since 2.4.0
-				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
-				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
-				 */
-				do_action( 'woocommerce_single_variation' );
+			/**
+			 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
+			 *
+			 * @since 2.4.0
+			 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
+			 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
+			 */
+			do_action( 'woocommerce_single_variation' );
 
-				/**
-				 * Hook: woocommerce_after_single_variation.
-				 */
+			/**
+			 * Hook: woocommerce_after_single_variation.
+			 */
+			if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
 				do_action( 'woocommerce_after_single_variation' );
+			}
 			?>
 		</div>
 	<?php endif; ?>
 
-	<?php do_action( 'woocommerce_after_variations_form' ); ?>
+	<?php
+	if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+		do_action( 'woocommerce_after_variations_form' );
+	}
+	?>
 </form>
 
 <?php
-do_action( 'woocommerce_after_add_to_cart_form' );
+if ( ! defined( 'FOOTER_ADD_TO_CART' ) ) {
+	do_action( 'woocommerce_after_add_to_cart_form' );
+}
