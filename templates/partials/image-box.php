e297788559be07cc428c0e8350425d6073a7de16
makiomar <mo7amed.maki@gmail.com>
2024-04-23 14:09:20 +0200

    lazyload inegrate

--- templates/partials/image-box.php
+++ templates/partials/image-box.php
@@ -19,9 +19,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div class="anony-grid-col<?php echo esc_html( $classes ); ?>">
 		<?php
 		if ( $image_url && ! empty( $image_url ) ) {
-			if ( class_exists( 'ANONY_Options_Model' ) ) {
+			if ( class_exists( 'ANONY_Options_Model' ) && class_exists( 'Anony_Flash_Wp' ) ) {
 				$anofl_options = ANONY_Options_Model::get_instance( 'Anofl_Options' );
-				if ( '1' === $anofl_options->add_missing_image_dimensions && '1' === $anofl_options->lazyload_images ) {
+				if ( '1' === $anofl_options->add_missing_image_dimensions && '1' === $anofl_options->lazyload_images && '1' !== $anofl_options->debug_mode ) {
 					?>
 					<img loading="lazy" data-src="<?php echo esc_url( $image_url ); ?>" src="data:image/svg+xml,%3Csvg%20xmlns=\'http://www.w3.org/2000/svg\'%20viewBox=\'0%200%20225%20225\'%3E%3C/svg%3E" alt="<?php echo esc_attr( $image_box_title ); ?>">
 					<?php
