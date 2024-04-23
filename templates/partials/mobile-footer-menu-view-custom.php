ef66b5db1e507a3546f2c00ae061525280191b8d
makiomar <mo7amed.maki@gmail.com>
2024-04-23 13:57:54 +0200

    Custom footer

---
+++ templates/partials/mobile-footer-menu-view-custom.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Mobile footer menu two
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+do_action( 'anony_custom_mobile_footer' );
