04dd5d930f125f137162e38d0193af80d78f6a48
makiomar <mo7amed.maki@gmail.com>
2024-05-11 15:01:41 +0300

    Term listings edits

--- templates/listings/term/term-listing-one.php
+++ templates/listings/term/term-listing-one.php
@@ -15,27 +15,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php
 	foreach ( $terms as $term_id => $name ) {
 		?>
-			<div class="anony-grid-col anony-grid-col-sm-<?php echo esc_attr( $desktop_columns ); ?> anony-grid-col-slg-<?php echo esc_attr( $mobile_columns ); ?> anony-circle-image anony-inline-flex flex-h-center anony-align-flex-start">
+			<div class="anony-grid-col <?php echo esc_attr( $class_prefix ); ?>-sm-<?php echo esc_attr( $desktop_columns ); ?> <?php echo esc_attr( $class_prefix ); ?>-slg-<?php echo esc_attr( $mobile_columns ); ?> <?php echo esc_html( $layout ); ?> anony-inline-flex flex-h-center flex-v-center anony-align-flex-start">
 				<a href="<?php echo esc_url( get_term_link( $term_id ) ); ?>" title="<?php echo esc_attr( $name ); ?>" class="anony-grid-row flex-h-center anony-flex-column anony-align-flex-start">
 					<?php
-					$image_id = get_term_meta( $term_id, $atts['image_id_meta_key'], true );
+					$image_id = get_term_meta( $term_id, $image_id_meta_key, true );
 
 					if ( ! empty( $image_id ) && is_numeric( $image_id ) ) {
-						$image_url = wp_get_attachment_image_url( absint( $image_id ), 'thumbnail' );
+						$image_url = wp_get_attachment_image_url( absint( $image_id ), $thumb_size );
 						?>
-						<img style="width:100px;height:100px" src="<?php echo esc_url( $image_url ); ?>"/>
+						<img style="width:<?php echo esc_html( $width ); ?>;height:<?php echo esc_html( $height ); ?>" src="<?php echo esc_url( $image_url ); ?>"/>
 						<?php
 					} else {
 						?>
-						<img style="width:100px;height:100px" src="<?php echo esc_url( ANONY_THEME_URI . '/images/placeholder_150x150.webp' ); ?>"/>
+						<img style="width:<?php echo esc_html( $width ); ?>;height:<?php echo esc_html( $height ); ?>" src="<?php echo esc_url( ANONY_THEME_URI . '/images/placeholder_150x150.webp' ); ?>"/>
 						<?php
 					}
 					?>
+					<?php if ( 'yes' === $show_title ) : ?>
 					<strong style="text-align:<?php echo esc_attr( $name_align ); ?>;font-size:<?php echo esc_attr( $font_size ); ?>"><?php echo esc_html( $name ); ?></strong>
+					<?php endif; ?>
 				</a>
 			</div>
 			<?php
-
 	}
 	?>
 </div>
