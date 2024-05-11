efa4ca9e5dba24d1adebba967d07e4e9e556a207
makiomar <mo7amed.maki@gmail.com>
2024-05-11 14:52:52 +0300

    Implement one column footer widget

--- templates/footer-view.php
+++ templates/footer-view.php
@@ -16,34 +16,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 $anony_options = ANONY_Options_Model::get_instance();
 ?>
 <footer id="anony-footer" class="anony-grid-col">
-<?php if ( '1' === $anony_options->enable_footer_top ) { ?>
-	<div id="anony-top-footer" class="anony-grid-row">
-		<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'top-footer-widget-1' ); ?></div>
-		<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'top-footer-widget-2' ); ?></div>
-		<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'top-footer-widget-3' ); ?></div>
-	</div>
-	<?php } ?>
-	<div id="anony-main-footer" class="anony-grid-row">
-		<div class="anony-grid-col anony-grid-col-sm-2">
-			<?php anony_dynamic_sidebar( 'footer-widget-1' ); ?>
+	<?php
+	if ( '1' === $anony_options->only_one_col_footer ) {
+		?>
+		<div id="anony-footer-one-col">
+			<?php anony_dynamic_sidebar( 'footer-one-col' ); ?>
 		</div>
-		<div class="anony-grid-col anony-grid-col-sm-2">
-		<?php anony_dynamic_sidebar( 'footer-widget-2' ); ?>
+		<?php
+	} else {
+		if ( '1' === $anony_options->enable_footer_top ) {
+			?>
+			<div id="anony-top-footer" class="anony-grid-row">
+				<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'top-footer-widget-1' ); ?></div>
+				<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'top-footer-widget-2' ); ?></div>
+				<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'top-footer-widget-3' ); ?></div>
+			</div>
+			<?php
+		}
+		?>
+		<div id="anony-main-footer" class="anony-grid-row">
+			<div class="anony-grid-col anony-grid-col-sm-2">
+				<?php anony_dynamic_sidebar( 'footer-widget-1' ); ?>
+			</div>
+			<div class="anony-grid-col anony-grid-col-sm-2">
+			<?php anony_dynamic_sidebar( 'footer-widget-2' ); ?>
+			</div>
+			<div class="anony-grid-col anony-grid-col-sm-5">
+			<?php anony_dynamic_sidebar( 'footer-widget-3' ); ?>
+			</div>
+			<div class="anony-grid-col anony-grid-col-sm-3">
+			<?php anony_dynamic_sidebar( 'footer-widget-4' ); ?>
+			</div>
 		</div>
-		<div class="anony-grid-col anony-grid-col-sm-5">
-		<?php anony_dynamic_sidebar( 'footer-widget-3' ); ?>
-		</div>
-		<div class="anony-grid-col anony-grid-col-sm-3">
-		<?php anony_dynamic_sidebar( 'footer-widget-4' ); ?>
-		</div>
-	</div>
-	<?php if ( '1' === $anony_options->enable_footer_bottom ) { ?>
-	<div id="anony-bottom-footer" class="anony-grid-row">
-		<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'bottom-footer-widget-1' ); ?></div>
-		<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'bottom-footer-widget-2' ); ?></div>
-		<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'bottom-footer-widget-3' ); ?></div>
-	</div>
-	<?php } ?>
+		<?php
+		if ( '1' === $anony_options->enable_footer_bottom ) {
+			?>
+			<div id="anony-bottom-footer" class="anony-grid-row">
+				<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'bottom-footer-widget-1' ); ?></div>
+				<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'bottom-footer-widget-2' ); ?></div>
+				<div class="anony-grid-col anony-grid-col-sm-4"><?php anony_dynamic_sidebar( 'bottom-footer-widget-3' ); ?></div>
+			</div>
+			<?php
+		}
+	}
+	?>
 	<?php
 	if ( wp_is_mobile() && '1' === $anony_options->enable_mobile_footer_sticky ) {
 		switch ( ANONY_FOOTER_STICKY_MENU_STYLE ) {
