6a235678fc24fa0cabca3f304ee13814a8ff06ae
makiomar <mo7amed.maki@gmail.com>
2024-05-15 10:26:31 +0300

    Markup edits

--- templates/listings/term/term-listing-one.php
+++ templates/listings/term/term-listing-one.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php
 	foreach ( $terms as $term_id => $name ) {
 		?>
-			<div class="anony-grid-col <?php echo esc_attr( $class_prefix ); ?>-sm-<?php echo esc_attr( $desktop_columns ); ?> <?php echo esc_attr( $class_prefix ); ?>-slg-<?php echo esc_attr( $mobile_columns ); ?> <?php echo esc_html( $layout ); ?> anony-inline-flex flex-h-center flex-v-center anony-align-flex-start">
+			<div height="<?php echo esc_html( $height ); ?>" class="anony-grid-col <?php echo esc_attr( $class_prefix ); ?>-sm-<?php echo esc_attr( $desktop_columns ); ?> <?php echo esc_attr( $class_prefix ); ?>-slg-<?php echo esc_attr( $mobile_columns ); ?> <?php echo esc_html( $layout ); ?> anony-inline-flex flex-h-center flex-v-center anony-align-flex-start">
 				<a href="<?php echo esc_url( get_term_link( $term_id ) ); ?>" title="<?php echo esc_attr( $name ); ?>" class="anony-grid-row flex-h-center anony-flex-column anony-align-flex-start">
 					<?php
 					$image_id = get_term_meta( $term_id, $image_id_meta_key, true );
@@ -23,11 +23,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 					if ( ! empty( $image_id ) && is_numeric( $image_id ) ) {
 						$image_url = wp_get_attachment_image_url( absint( $image_id ), $thumb_size );
 						?>
-						<img style="width:<?php echo esc_html( $width ); ?>;height:<?php echo esc_html( $height ); ?>" src="<?php echo esc_url( $image_url ); ?>"/>
+						<img width="<?php echo esc_html( $width ); ?>" height="<?php echo esc_html( $height ); ?>" width="<?php echo esc_html( $width ); ?>" height="<?php echo esc_html( $height ); ?>" style="width:<?php echo esc_html( $width ); ?>;height:<?php echo esc_html( $height ); ?>" src="<?php echo esc_url( $image_url ); ?>"/>
 						<?php
 					} else {
 						?>
-						<img style="width:<?php echo esc_html( $width ); ?>;height:<?php echo esc_html( $height ); ?>" src="<?php echo esc_url( ANONY_THEME_URI . '/images/placeholder_150x150.webp' ); ?>"/>
+						<img width="<?php echo esc_html( $width ); ?>" height="<?php echo esc_html( $height ); ?>" style="width:<?php echo esc_html( $width ); ?>;height:<?php echo esc_html( $height ); ?>" src="<?php echo esc_url( ANONY_THEME_URI . '/images/placeholder_150x150.webp' ); ?>"/>
 						<?php
 					}
 					?>
