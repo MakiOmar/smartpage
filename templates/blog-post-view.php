de5e5d936cfdf22d903c947d31c715e79cbd26aa
makiomar <mo7amed.maki@gmail.com>
2024-05-20 13:51:51 +0300

    Access array items directly without explode

--- templates/blog-post-view.php
+++ templates/blog-post-view.php
@@ -1,16 +1,27 @@
 <?php
+/**
+ * Blog post template
+ *
+ * PHP version 7.3 Or Later
+ *
+ * @package  SmartPage
+ * @author   Makiomar <info@makior.com>
+ * @license  https://makiomar.com SmartPage Licence
+ * @link     https://makiomar.com
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed direct.ly
+	exit; // Exit if accessed directly.
 }
 ?>
-<div id="post-<?php echo $id; ?>" class="anony-post-wrapper anony-grid-col-max-480-12 anony-grid-col-av-12 anony-grid-col-md-6 anony-grid-col">
+<div id="post-<?php echo esc_attr( $p['id'] ); ?>" class="anony-post-wrapper anony-grid-col-max-480-12 anony-grid-col-av-12 anony-grid-col-md-6 anony-grid-col">
 	 
 	<div class="anony-post-contents anony-blog-post anony-grid-col">
   
 			<div class="anony-post-info white-bg">
 	   
 			<?php
-			if ( $thumb && $thumb_exists ) {
+			if ( $p['thumb'] && $p['thumb_exists'] ) {
 
 				include locate_template( 'templates/post-layout/post-with-thumb.php', false, false );
 
@@ -25,19 +36,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 			 
 				<div class="anony-author-avatar">
 				 
-					<?php echo $gravatar; ?>
+					<?php echo wp_kses_post( $p['gravatar'] ); ?>
 					 
 				</div>
 				 
 				<div class="author-name">
 				 
-					<span><?php echo $author; ?></span>
+					<span><?php echo esc_html( $p['author'] ); ?></span>
 					 
 				</div>
 				 
 				<div>
 				 
-					<a class="button anony-button" href="<?php echo $permalink; ?>"><?php echo $read_more; ?></a>
+					<a class="button anony-button" href="<?php echo esc_url( $p['permalink'] ); ?>"><?php echo wp_kses_post( $p['read_more'] ); ?></a>
 					 
 				</div>
 				 
