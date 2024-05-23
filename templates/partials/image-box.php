16c95dd6b576176ad52a4d3e6ae18c2e843c7658
makiomar <mo7amed.maki@gmail.com>
2024-05-23 17:24:39 +0300

    Partial structure

--- templates/partials/image-box.php
+++ templates/partials/image-box.php
@@ -15,35 +15,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div class="anony-image-box-wrapper">
-	<div class="anony-grid-row anony-inline-flex">
-		<div class="anony-grid-col<?php echo esc_html( $classes ); ?>">
+	<div class="anony-grid-row anony-flex<?php echo esc_attr( $classes ); ?>">
+		<div class="anony-grid-col<?php echo 'vertical' !== $layout ? ' anony-grid-col-4' : ''; ?>">
 		<?php
 		if ( $image_url && ! empty( $image_url ) ) {
-			if ( class_exists( 'ANONY_Options_Model' ) && class_exists( 'Anony_Flash_Wp' ) ) {
-				$anofl_options = ANONY_Options_Model::get_instance( 'Anofl_Options' );
-				if ( '1' === $anofl_options->add_missing_image_dimensions && '1' === $anofl_options->lazyload_images && '1' !== $anofl_options->debug_mode ) {
-					?>
-					<img loading="lazy" data-src="<?php echo esc_url( $image_url ); ?>" src="data:image/svg+xml,%3Csvg%20xmlns=\'http://www.w3.org/2000/svg\'%20viewBox=\'0%200%20225%20225\'%3E%3C/svg%3E" alt="<?php echo esc_attr( $image_box_title ); ?>">
-					<?php
-				} else {
-					?>
-					<img src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_box_title ); ?>"/>
-					<?php
-				}
-			} else {
-				?>
-				<img src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_box_title ); ?>"/>
-				<?php
-			}
+			?>
+			<a href="<?php echo esc_url( $image_box_link ); ?>" title="<?php echo esc_attr( wp_strip_all_tags( $image_box_title ) ); ?>"><img style="width:<?php echo esc_html( $image_width ); ?>px" src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $image_box_title ); ?>"/></a>
+			<?php
 		}
 		?>
 		</div>
-		<div class="anony-grid-col anony-grid-col-8 anony-inline-flex flex-v-center anony-flex-column">
+		<div class="anony-grid-col<?php echo 'vertical' !== $layout ? ' anony-grid-col-8 ' : ''; ?>anony-inline-flex flex-v-center anony-flex-column">
 			<?php if ( $image_box_title && ! empty( $image_box_title ) ) { ?>
-			<div><strong><?php echo esc_html( $image_box_title ); ?></strong></div>
+			<div><strong><?php echo wp_kses_post( $image_box_title ); ?></strong></div>
 			<?php } ?>
 			<?php if ( $image_box_description && ! empty( $image_box_description ) ) { ?>
-			<div><?php echo esc_html( $image_box_description ); ?></div>
+			<div><?php echo wp_kses_post( $image_box_description ); ?></div>
 			<?php } ?>
 		</div>
 	</div>
