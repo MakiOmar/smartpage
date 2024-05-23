487b5cfe4993222f39b4bedcc8676f0903a5d7bd
makiomar <mo7amed.maki@gmail.com>
2024-05-23 19:52:03 +0300

    Arrange shop structure

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -18,7 +18,8 @@
 defined( 'ABSPATH' ) || exit;
 
 get_header( 'shop' );
-
+$anony_options = ANONY_Options_Model::get_instance();
+$content_width = 'no-sidebar' !== $anony_options->sidebar ? 'anony-grid-col-sm-9-5 ' : '';
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -30,97 +31,106 @@ get_header( 'shop' );
 do_action( 'woocommerce_before_main_content' );
 
 ?>
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+<main class="anony-content">
+	<header class="woocommerce-products-header">
+		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+			<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+		<?php endif; ?>
 
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
-<div class="anony-grid">
-	<div class="anony-grid-row anony-grid-col">
-		
-		<div class="anony-grid-col-sm-9-5">
+		<?php
+		/**
+		 * Hook: woocommerce_archive_description.
+		 *
+		 * @hooked woocommerce_taxonomy_archive_description - 10
+		 * @hooked woocommerce_product_archive_description - 10
+		 */
+		do_action( 'woocommerce_archive_description' );
+		?>
+	</header>
+	<div class="anony-grid">
+		<div class="anony-grid-row anony-grid-col">
 			
-			<div class="anony-grid-col">
+			<div class="
+				<?php
+				//phpcs:disable
+				echo $content_width;
+				//phpcs:enable
+				?>
+				anony-grid-col">
 				
-				<div class="anony-grid-container">
-					<?php
-					if ( woocommerce_product_loop() ) {
+				<div class="anony-grid-col">
+					
+					<div class="anony-grid-container">
+						<?php
+						if ( woocommerce_product_loop() ) {
 
-						/**
-						 * Hook: woocommerce_before_shop_loop.
-						 *
-						 * @hooked woocommerce_output_all_notices - 10
-						 * @hooked woocommerce_result_count - 20
-						 * @hooked woocommerce_catalog_ordering - 30
-						 */
-						do_action( 'woocommerce_before_shop_loop' );
+							/**
+							 * Hook: woocommerce_before_shop_loop.
+							 *
+							 * @hooked woocommerce_output_all_notices - 10
+							 * @hooked woocommerce_result_count - 20
+							 * @hooked woocommerce_catalog_ordering - 30
+							 */
+							do_action( 'woocommerce_before_shop_loop' );
 
-						woocommerce_product_loop_start();
+							woocommerce_product_loop_start();
 
-						if ( wc_get_loop_prop( 'total' ) ) {
-							while ( have_posts() ) {
-								the_post();
+							if ( wc_get_loop_prop( 'total' ) ) {
+								while ( have_posts() ) {
+									the_post();
 
-								/**
-								 * Hook: woocommerce_shop_loop.
-								 */
-								do_action( 'woocommerce_shop_loop' );
+									/**
+									 * Hook: woocommerce_shop_loop.
+									 */
+									do_action( 'woocommerce_shop_loop' );
 
-								wc_get_template_part( 'content', 'product' );
+									wc_get_template_part( 'content', 'product' );
+								}
 							}
+
+							woocommerce_product_loop_end();
+
+							/**
+							 * Hook: woocommerce_after_shop_loop.
+							 *
+							 * @hooked woocommerce_pagination - 10
+							 */
+							do_action( 'woocommerce_after_shop_loop' );
+						} else {
+							/**
+							 * Hook: woocommerce_no_products_found.
+							 *
+							 * @hooked wc_no_products_found - 10
+							 */
+							do_action( 'woocommerce_no_products_found' );
 						}
 
-						woocommerce_product_loop_end();
-
 						/**
-						 * Hook: woocommerce_after_shop_loop.
+						 * Hook: woocommerce_after_main_content.
 						 *
-						 * @hooked woocommerce_pagination - 10
+						 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 						 */
-						do_action( 'woocommerce_after_shop_loop' );
-					} else {
-						/**
-						 * Hook: woocommerce_no_products_found.
-						 *
-						 * @hooked wc_no_products_found - 10
-						 */
-						do_action( 'woocommerce_no_products_found' );
-					}
-
-					/**
-					 * Hook: woocommerce_after_main_content.
-					 *
-					 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-					 */
-					do_action( 'woocommerce_after_main_content' );
-					?>
+						do_action( 'woocommerce_after_main_content' );
+						?>
+						
+					</div>
 					
 				</div>
 				
 			</div>
-			
+			<?php
+			if ( 'no-sidebar' === $anony_options->sidebar ) {
+				/**
+				 * Hook: woocommerce_sidebar.
+				 *
+				 * @hooked woocommerce_get_sidebar - 10
+				 */
+				do_action( 'woocommerce_sidebar' );
+			}
+			?>
 		</div>
-		<?php
-		/**
-		 * Hook: woocommerce_sidebar.
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
-		?>
+		
 	</div>
-	
-</div>
-
+</main>
 <?php
 get_footer( 'shop' );
