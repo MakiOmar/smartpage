4db3613b58e5f1d17733013ecdc1e82b4b83405a
makiomar <mo7amed.maki@gmail.com>
2024-07-16 14:19:30 +0300

    Sticky class

--- templates/header-mobile-view.php
+++ templates/header-mobile-view.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 require 'document-head.php';
 ?>
 
-<header id="anony-mobile-header" class="anony-grid-row anony-box-shadow">
+<header id="anony-mobile-header" class="anony-grid-row anony-box-shadow<?php echo esc_attr( $sticky_class ); ?>">
 	<?php do_action( 'anony_after_header_prepend' ); ?>
 	<div class="anony-grid-col anony-grid-col-4 anony-inline-flex flex-h-center flex-v-center">
 		<?php do_action( 'anony_mobile_header_first' ); ?>
