8bbf1d7991c279795870360e11864462ee6797d5
makiomar <mo7amed.maki@gmail.com>
2024-07-16 14:18:49 +0300

    Remove socials

--- header.php
+++ header.php
@@ -17,6 +17,8 @@ if ( ! defined( 'ANOENGINE' ) ) {
 } else {
 	$anony_options = ANONY_Options_Model::get_instance();
 
+	$sticky_class = 'sticky' === $anony_options->mobile_header_behavior ? ' anony-has-sticky' : '';
+
 	$language_atts = get_language_attributes();
 	$content_type  = get_bloginfo( 'html_type' );
 	$char_set      = get_bloginfo( 'charset' );
@@ -50,58 +52,6 @@ if ( ! defined( 'ANOENGINE' ) ) {
 				require locate_template( 'templates/header-one-view.php', false, false );
 				break;
 			default:
-				$languages_menu = anony_navigation( 'anony-languages-menu', '' );
-				$socials_follow = array(
-					array(
-						'icon'  => 'facebook',
-						'url'   => $anony_options->facebook,
-						'title' => __( 'Follow us on Facebook', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'twitter',
-						'url'   => $anony_options->twitter,
-						'title' => __( 'Follow us on Twitter', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'youtube',
-						'url'   => $anony_options->youtube,
-						'title' => __( 'Follow us on Youtube', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'pinterest',
-						'url'   => $anony_options->pinterest,
-						'title' => __( 'Follow us on Pinterest', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'linkedin',
-						'url'   => $anony_options->linkedin,
-						'title' => __( 'Follow us on Linkedin', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'instagram',
-						'url'   => $anony_options->instagram,
-						'title' => __( 'Follow us on Instagram', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'tumblr',
-						'url'   => $anony_options->tumblr,
-						'title' => __( 'Follow us on Tumblr', 'smartpage' ),
-					),
-
-					array(
-						'icon'  => 'rss',
-						'url'   => $anony_options->rss,
-						'title' => __( 'Follow us with RSS feed', 'smartpage' ),
-					),
-				);
-				$phone = $anony_options->phone;
-				$email = $anony_options->email;
 				require locate_template( 'templates/header-view.php', false, false );
 		}
 		// phpcs:disable
